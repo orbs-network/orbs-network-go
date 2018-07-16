224fd89107cbaf80f44a0b7fbaf2a76c1cecd6b7
Kirill Maksimov <kirill@orbs.com>
2018-07-16 18:12:54 +0300

    Leveldb persistence (WIP)

--- services/blockstorage/adapter/leveldb_persistence.go
+++ services/blockstorage/adapter/leveldb_persistence.go
@@ -2,6 +2,10 @@ package adapter
 
 import (
 	"github.com/orbs-network/orbs-spec/types/go/protocol"
+	"github.com/syndtr/goleveldb/leveldb"
+	"fmt"
+	"github.com/syndtr/goleveldb/leveldb/util"
+	"strconv"
 )
 
 type Config interface {
@@ -12,6 +16,7 @@ type levelDbBlockPersistence struct {
 	blockWritten chan bool
 	blockPairs   []*protocol.BlockPairContainer
 	config       Config
+	db *leveldb.DB
 }
 
 type config struct {
@@ -27,17 +32,61 @@ func NewLevelDbBlockPersistenceConfig(name string) Config {
 }
 
 func NewLevelDbBlockPersistence(config Config) BlockPersistence {
+	db, err := leveldb.OpenFile("/tmp/db", nil)
+
+	if err != nil{
+		fmt.Println("Could not instantiate leveldb", err)
+		panic("Could not instantiate leveldb")
+	}
+
 	return &levelDbBlockPersistence{
 		config:       config,
 		blockWritten: make(chan bool, 10),
+		db: db,
 	}
 }
 
 func (bp *levelDbBlockPersistence) WriteBlock(blockPair *protocol.BlockPairContainer) {
+	key := "transaction-block-header-" + strconv.FormatUint(uint64(blockPair.TransactionsBlock.Header.BlockHeight()), 10)
+	value := blockPair.TransactionsBlock.Header.Raw()
+
+	fmt.Printf("Writing key %v, value %v\n", key, value)
+	bp.db.Put([]byte(key), value, nil)
+
 	bp.blockPairs = append(bp.blockPairs, blockPair)
 	bp.blockWritten <- true
 }
 
 func (bp *levelDbBlockPersistence) ReadAllBlocks() []*protocol.BlockPairContainer {
-	return bp.blockPairs
+	var results []*protocol.BlockPairContainer
+
+	iter := bp.db.NewIterator(util.BytesPrefix([]byte("transaction-block-header-")), nil)
+
+	for iter.Next()  {
+		// Remember that the contents of the returned slice should not be modified, and
+		// only valid until the next call to Next.
+		key := string(iter.Key())
+		value := iter.Value()
+
+		fmt.Printf("Retrieving key %v, value %v\n", key, value)
+
+		transactionsBlock := &protocol.TransactionsBlockContainer{
+			Header: protocol.TransactionsBlockHeaderReader(value),
+		}
+
+		fmt.Println("Height, timestamp", transactionsBlock.Header.BlockHeight(), transactionsBlock.Header.Timestamp())
+
+		resultsBlock := &protocol.ResultsBlockContainer{}
+
+		container := &protocol.BlockPairContainer{
+			TransactionsBlock: transactionsBlock,
+			ResultsBlock: resultsBlock,
+		}
+
+		results = append(results, container)
+	}
+	iter.Release()
+	_ = iter.Error()
+
+	return results
 }
--- services/blockstorage/adapter/leveldb_persistence_test.go
+++ services/blockstorage/adapter/leveldb_persistence_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 	"github.com/orbs-network/orbs-spec/types/go/protocol"
 	"github.com/orbs-network/orbs-spec/types/go/primitives"
+	"fmt"
 )
 
 func TestLevelDbPersistence(t *testing.T) {
@@ -49,6 +50,9 @@ var _ = Describe("LevelDb persistence", func() {
 			db.WriteBlock(container1)
 
 			allBlocks := db.ReadAllBlocks()
+
+			fmt.Println(allBlocks[0].TransactionsBlock.Header.String())
+			fmt.Println(allBlocks[1].TransactionsBlock.Header.String())
 
 			compareContainers(container0, allBlocks[0])
 			compareContainers(container1, allBlocks[1])
