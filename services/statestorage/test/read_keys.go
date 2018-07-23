e33b64027e0d10c298491728c3c631b244035796
Noam Berg <noamberg@yahoo.com>
2018-07-23 14:32:50 +0300

    tests for commitStateDiff

--- services/statestorage/test/read_keys.go
+++ services/statestorage/test/read_keys.go
@@ -8,13 +8,16 @@ import (
 	. "github.com/onsi/gomega"
 	"github.com/orbs-network/orbs-spec/types/go/protocol"
 	"github.com/orbs-network/orbs-network-go/services/statestorage/adapter"
+	"bytes"
+	"fmt"
 )
 
+//TODO a case where we pass a set of keys to ReadKeys(), and at least one key has no matching value
 var _ = Describe("Reading a Key", func() {
 	When("not providing a contract name", func() {
 		It("Returns an error", func() {
 			d := newStateStorageDriver()
-			_, err := d.readKey("", "someKey")
+			_, err := d.readSingleKey("", "someKey")
 			Expect(err).To(MatchError("missing contract name"))
 		})
 	})
@@ -22,12 +25,12 @@ var _ = Describe("Reading a Key", func() {
 	When("key doesn't exist", func() {
 		It("Returns no Value", func() {
 			d := newStateStorageDriver()
-			_, err := d.readKey("fooContract", "someKey")
+			_, err := d.readSingleKey("fooContract", "someKey")
 			Expect(err).To(MatchError("no value found for input key(s)"))
 		})
 	})
 
-	When ( "State has only One Contract", func() {
+	When("State has only One Contract", func() {
 		When("key exist", func() {
 			It("Returns a Value", func() {
 				value := []byte("bar")
@@ -38,17 +41,15 @@ var _ = Describe("Reading a Key", func() {
 				d.write(contract, key, value)
 				d.write(contract, "someOtherKey", value)
 
-				output, err := d.readKey(contract, key)
+				output, err := d.readSingleKey(contract, key)
 
 				Expect(err).ToNot(HaveOccurred())
-				Expect(output).To(HaveLen(1))
-				Expect(string(output[0].Key())).To(Equal(key))
-				Expect(output[0].Value()).To(Equal(value))
+				Expect(output).To(Equal(value))
 			})
 		})
 	})
 
-	When ( "State has multiple Contracts", func() {
+	When("State has multiple Contracts", func() {
 		When("same key exist in two contracts", func() {
 			It("Returns a different Value", func() {
 				key := "foo"
@@ -58,33 +59,81 @@ var _ = Describe("Reading a Key", func() {
 				d.write("contract1", key, v1)
 				d.write("contract2", key, v2)
 
-				output, err := d.readKey("contract1", key)
+				output, err := d.readSingleKey("contract1", key)
 				Expect(err).ToNot(HaveOccurred())
-				Expect(string(output[0].Key())).To(Equal(key))
-				Expect(output[0].Value()).To(Equal(v1))
+				Expect(output).To(Equal(v1))
 
-				output2, err2 := d.readKey("contract2", key)
+				output2, err2 := d.readSingleKey("contract2", key)
 				Expect(err2).ToNot(HaveOccurred())
-				Expect(string(output2[0].Key())).To(Equal(key))
-				Expect(output2[0].Value()).To(Equal(v2))
+				Expect(output2).To(Equal(v2))
 			})
 		})
+	})
+
+})
+
+var _ = Describe("Commit a State Diff", func() {
+
+	It("persists the state into storage", func() {
+		d := newStateStorageDriver()
+
+		v1 := []byte("v1")
+		v2 := []byte("v2")
+		v3 := []byte("v3")
+
+		input := &services.CommitStateDiffInput{ContractStateDiffs:
+		[]*protocol.ContractStateDiff{(&protocol.ContractStateDiffBuilder{
+			ContractName: primitives.ContractName("contract1"),
+			StateDiffs: []*protocol.StateRecordBuilder{
+				{Key: []byte("key1"), Value: v1},
+				{Key: []byte("key2"), Value: v2},
+			},
+		}).Build(),
+			(&protocol.ContractStateDiffBuilder{
+				ContractName: primitives.ContractName("contract2"),
+				StateDiffs: []*protocol.StateRecordBuilder{
+					{Key: []byte("key1"), Value: v3},
+				},
+			}).Build(),
+		},
+		}
+
+		d.service.CommitStateDiff(input)
+
+		output, err := d.readSingleKey("contract1", "key1")
+		Expect(err).ToNot(HaveOccurred())
+		Expect(output).To(Equal(v1))
+		output2, err := d.readSingleKey("contract1", "key2")
+		Expect(err).ToNot(HaveOccurred())
+		Expect(output2).To(Equal(v2))
+		output3, err := d.readSingleKey("contract2", "key1")
+		Expect(err).ToNot(HaveOccurred())
+		Expect(output3).To(Equal(v3))
+
 	})
 })
 
 type driver struct {
-	service services.StateStorage
+	service     services.StateStorage
 	persistence adapter.StatePersistence
 }
 
-func (d *driver) readKey(contract string, key string) ([]*protocol.StateRecord, error) {
+func (d *driver) readSingleKey(contract string, key string) ([]byte, error) {
 	out, err := d.service.ReadKeys(&services.ReadKeysInput{ContractName: primitives.ContractName(contract), Keys: []primitives.Ripmd160Sha256{[]byte(key)}})
 
 	if err != nil {
 		return nil, err
 	}
 
-	return out.StateRecords, nil
+	if l := len(out.StateRecords); l != 1 {
+		panic(fmt.Sprintf("expected exactly one element in array. found %v", l))
+	}
+
+	if actual, expected := out.StateRecords[0].Key(), []byte(key); !bytes.Equal(actual, expected) {
+		panic(fmt.Sprintf("expected output key %s to match input key %s", actual, expected))
+	}
+
+	return out.StateRecords[0].Value(), nil
 }
 
 func (d *driver) write(contract string, key string, value []byte) {
@@ -94,7 +143,5 @@ func (d *driver) write(contract string, key string, value []byte) {
 func newStateStorageDriver() *driver {
 	p := adapter.NewInMemoryStatePersistence(&struct{}{})
 
-	return &driver {persistence: p, service: statestorage.NewStateStorage(p)}
+	return &driver{persistence: p, service: statestorage.NewStateStorage(p)}
 }
-
-
