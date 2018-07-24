75ec20bc08b48cd73f9741003ae65ba8eef8ccd1
Kirill Maksimov <kirill@orbs.com>
2018-07-24 12:38:44 +0300

    Add json logger

--- instrumentation/reporting_test.go
+++ instrumentation/reporting_test.go
@@ -3,6 +3,11 @@ package instrumentation
 import (
 	"testing"
 	"go.uber.org/zap"
+	"time"
+	"encoding/json"
+	"fmt"
+	"go.uber.org/zap/zapcore"
+	"runtime"
 )
 
 const (
@@ -17,12 +22,61 @@ type BasicLogger interface {
 	Info(flow string, message string, params... zap.Field)
 }
 
+type jsonLogger struct {
+
+}
+
 type basicLogger struct {
 	node string
 	vchain int
 	service string
 
 	logger *zap.Logger
+	jsonLogger *jsonLogger
+}
+
+func getCaller() (function string, source string) {
+	fpcs := make([]uintptr, 1)
+
+	// skip 4 levels to get to the caller of logger function
+	n := runtime.Callers(4, fpcs)
+	if n == 0 {
+		return "n/a", "n/a"
+	}
+
+	fun := runtime.FuncForPC(fpcs[0]-1)
+	if fun == nil {
+		return "n/a", "n/a"
+	}
+
+	file, line := fun.FileLine(fpcs[0]-1)
+	return fun.Name(), fmt.Sprintf("%s:%d", file, line)
+}
+
+func (j *jsonLogger) Info(message string, params... zap.Field) {
+	logLine := make(map[string]interface{})
+
+	logLine["level"] = "info"
+	logLine["timestamp"] = float64(time.Now().UTC().UnixNano()) / 1000000000
+	logLine["message"] = message
+
+	function, source := getCaller()
+	logLine["function"] = function
+	logLine["source"] = source
+
+	for _, param := range params {
+		switch param.Type {
+		case zapcore.StringType:
+			logLine[param.Key] = param.String
+		case zapcore.Int64Type:
+			logLine[param.Key] = param.Integer
+		}
+
+	}
+
+	logLineAsJson, _ := json.Marshal(logLine)
+
+	fmt.Println(string(logLineAsJson))
 }
 
 func (b *basicLogger) Info(flow string, message string, params... zap.Field){
@@ -37,11 +91,12 @@ func (b *basicLogger) Info(flow string, message string, params... zap.Field){
 	zapParams = append(zapParams, params...)
 
 	b.logger.Info(message, zapParams...)
+	b.jsonLogger.Info(message, zapParams...)
 }
 
 func getLogger(node string, vchain int, service string) BasicLogger {
 	zapLogger, _ := zap.NewProduction()
-	logger := &basicLogger{node, vchain, service, zapLogger}
+	logger := &basicLogger{node, vchain, service, zapLogger, &jsonLogger{}}
 
 	return logger
 }
