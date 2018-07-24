7cd8451f3e6d482e34d65b04daca74b0e6374286
Kirill Maksimov <kirill@orbs.com>
2018-07-24 18:16:22 +0300

    Add test for meter

--- instrumentation/basic_logger_test.go
+++ instrumentation/basic_logger_test.go
@@ -1,34 +1,110 @@
 package instrumentation
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	. "github.com/onsi/gomega"
+	"io"
+	"io/ioutil"
+	"os"
 	"testing"
 	"time"
 )
 
 const (
-	TransactionFlow = "TransactionFlow"
+	TransactionFlow     = "TransactionFlow"
+	TransactionAccepted = "Transaction accepted"
 )
 
-const (
-	TransactionAccepted  = "Transaction accepted"
-	TransactionProcessed = "Transaction processed"
-)
+func readString(reader io.Reader) string {
+	result, err := ioutil.ReadAll(reader)
+	fmt.Println(err)
+	return string(result)
+}
+
+func captureStdout(f func(writer io.Writer)) string {
+	r, w, _ := os.Pipe()
+
+	f(w)
+
+	w.Close()
+
+	var buf bytes.Buffer
+	io.Copy(&buf, r)
+	return buf.String()
+}
+
+func parseStdout(input string) map[string]interface{} {
+	jsonMap := make(map[string]interface{})
+	_ = json.Unmarshal([]byte(input), &jsonMap)
+	return jsonMap
+}
 
 func TestReport(t *testing.T) {
-	serviceLogger := GetLogger(Node("node1"), Service("public-api"))
-	serviceLogger.Info("Service initialized")
+	RegisterTestingT(t)
 
-	txId := String("txId", "1234567")
+	stdout := captureStdout(func(writer io.Writer) {
+		serviceLogger := GetLogger(Node("node1"), Service("public-api")).WithOutput(writer)
+		serviceLogger.Info("Service initialized")
+	})
 
-	txFlowLogger := serviceLogger.For(String("flow", TransactionFlow))
-	txFlowLogger.Info(TransactionAccepted, txId, Bytes("payload", []byte{1, 2, 3, 99}))
+	fmt.Println(stdout)
+	jsonMap := parseStdout(stdout)
 
-	prefixedLogger := txFlowLogger.For(String("one-more-prefix", "one-more-value"))
+	Expect(jsonMap["level"]).To(Equal("info"))
+	Expect(jsonMap["node"]).To(Equal("node1"))
+	Expect(jsonMap["service"]).To(Equal("public-api"))
+	Expect(jsonMap["function"]).To(Equal("github.com/orbs-network/orbs-network-go/instrumentation.TestReport.func1"))
+	Expect(jsonMap["message"]).To(Equal("Service initialized"))
+	Expect(jsonMap["source"]).NotTo(BeEmpty())
+	Expect(jsonMap["timestamp"]).NotTo(BeNil())
 
-	prefixedLogger.Info(TransactionProcessed, txId)
+	stdout = captureStdout(func(writer io.Writer) {
+		serviceLogger := GetLogger(Node("node1"), Service("public-api")).WithOutput(writer)
+		txId := String("txId", "1234567")
+		txFlowLogger := serviceLogger.For(String("flow", TransactionFlow))
+		txFlowLogger.Info(TransactionAccepted, txId, Bytes("payload", []byte{1, 2, 3, 99}))
+	})
 
-	meter := txFlowLogger.Meter("tx-process-time", txId)
-	defer meter.Done()
+	fmt.Println(stdout)
+	jsonMap = parseStdout(stdout)
 
-	time.Sleep(1 * time.Millisecond)
+	Expect(jsonMap["level"]).To(Equal("info"))
+	Expect(jsonMap["node"]).To(Equal("node1"))
+	Expect(jsonMap["service"]).To(Equal("public-api"))
+	Expect(jsonMap["function"]).To(Equal("github.com/orbs-network/orbs-network-go/instrumentation.TestReport.func2"))
+	Expect(jsonMap["message"]).To(Equal(TransactionAccepted))
+	Expect(jsonMap["source"]).NotTo(BeEmpty())
+	Expect(jsonMap["timestamp"]).NotTo(BeNil())
+	Expect(jsonMap["txId"]).To(Equal("1234567"))
+	Expect(jsonMap["flow"]).To(Equal(TransactionFlow))
+	Expect(jsonMap["payload"]).To(Equal("MlZmV0E="))
+
+	stdout = captureStdout(func(writer io.Writer) {
+		serviceLogger := GetLogger(Node("node1"), Service("public-api")).WithOutput(writer)
+		txId := String("txId", "1234567")
+		txFlowLogger := serviceLogger.For(String("flow", TransactionFlow))
+		meter := txFlowLogger.Meter("tx-process-time", txId)
+		defer meter.Done()
+
+		time.Sleep(1 * time.Millisecond)
+	})
+
+	fmt.Println(stdout)
+
+	jsonMap = parseStdout(stdout)
+
+	Expect(jsonMap["level"]).To(Equal("metric"))
+	Expect(jsonMap["node"]).To(Equal("node1"))
+	Expect(jsonMap["service"]).To(Equal("public-api"))
+	Expect(jsonMap["function"]).To(Equal("github.com/orbs-network/orbs-network-go/instrumentation.TestReport.func3"))
+	Expect(jsonMap["message"]).To(Equal("Metric recorded"))
+	Expect(jsonMap["source"]).NotTo(BeEmpty())
+	Expect(jsonMap["timestamp"]).NotTo(BeNil())
+	Expect(jsonMap["metric"]).To(Equal("public-api-TransactionFlow-tx-process-time"))
+	//FIXME pass txId as well
+	//Expect(jsonMap["txId"]).To(Equal("1234567"))
+	Expect(jsonMap["flow"]).To(Equal(TransactionFlow))
+	Expect(jsonMap["process-time"]).NotTo(BeNil())
 }
