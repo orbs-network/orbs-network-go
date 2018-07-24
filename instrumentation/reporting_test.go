7995ddba7848bfddcdd0f9699e04f7cab0a80389
Kirill Maksimov <kirill@orbs.com>
2018-07-24 13:14:06 +0300

    Remove zap

--- instrumentation/reporting_test.go
+++ instrumentation/reporting_test.go
@@ -2,12 +2,11 @@ package instrumentation
 
 import (
 	"testing"
-	"go.uber.org/zap"
+	"fmt"
+	"runtime"
 	"time"
 	"encoding/json"
-	"fmt"
-	"go.uber.org/zap/zapcore"
-	"runtime"
+	"github.com/orbs-network/orbs-network-go/crypto/base58"
 )
 
 const (
@@ -19,7 +18,7 @@ const (
 )
 
 type BasicLogger interface {
-	Info(flow string, message string, params... zap.Field)
+	Info(flow string, message string, params... *Field)
 }
 
 type jsonLogger struct {
@@ -31,15 +30,66 @@ type basicLogger struct {
 	vchain int
 	service string
 
-	logger *zap.Logger
 	jsonLogger *jsonLogger
+}
+
+type FieldType uint8
+
+const (
+	NoType = iota
+	StringType
+	IntType
+	UintType
+	BytesType
+)
+
+type Field struct {
+	Key string
+	Type FieldType
+
+	String string
+	Int int64
+	Uint uint64
+	Bytes []byte
+}
+
+func String(key string, value string) *Field {
+	return &Field{Key: key, String: value, Type: StringType}
+}
+
+func Int(key string, value int) *Field {
+	return &Field{Key: key, Int: int64(value), Type: IntType}
+}
+
+func Int32(key string, value int32) *Field {
+	return &Field{Key: key, Int: int64(value), Type: IntType}
+}
+
+func Int64(key string, value int64) *Field {
+	return &Field{Key: key, Int: int64(value), Type: IntType}
+}
+
+func Bytes(key string, value []byte) *Field {
+	return &Field{Key: key, Bytes: value, Type: BytesType}
+}
+
+func Uint(key string, value uint) *Field {
+	return &Field{Key: key, Uint: uint64(value), Type: UintType}
+}
+
+func Uint32(key string, value uint32) *Field {
+	return &Field{Key: key, Uint: uint64(value), Type: UintType}
+}
+
+func Uint64(key string, value uint64) *Field {
+	return &Field{Key: key, Uint: value, Type: UintType}
 }
 
 func getCaller() (function string, source string) {
 	fpcs := make([]uintptr, 1)
 
-	// skip 4 levels to get to the caller of logger function
-	n := runtime.Callers(4, fpcs)
+	// skip levels to get to the caller of logger function
+	n := runtime.Callers(5, fpcs)
 	if n == 0 {
 		return "n/a", "n/a"
 	}
@@ -53,10 +103,10 @@ func getCaller() (function string, source string) {
 	return fun.Name(), fmt.Sprintf("%s:%d", file, line)
 }
 
-func (j *jsonLogger) Info(message string, params... zap.Field) {
+func (j *jsonLogger) Log(level string, message string, params... *Field) {
 	logLine := make(map[string]interface{})
 
-	logLine["level"] = "info"
+	logLine["level"] = level
 	logLine["timestamp"] = float64(time.Now().UTC().UnixNano()) / 1000000000
 	logLine["message"] = message
 
@@ -66,12 +116,15 @@ func (j *jsonLogger) Info(message string, params... zap.Field) {
 
 	for _, param := range params {
 		switch param.Type {
-		case zapcore.StringType:
+		case StringType:
 			logLine[param.Key] = param.String
-		case zapcore.Int64Type:
-			logLine[param.Key] = param.Integer
+		case IntType:
+			logLine[param.Key] = param.Int
+		case UintType:
+			logLine[param.Key] = param.Uint
+		case BytesType:
+			logLine[param.Key] = base58.Encode(param.Bytes)
 		}
-
 	}
 
 	logLineAsJson, _ := json.Marshal(logLine)
@@ -79,24 +132,23 @@ func (j *jsonLogger) Info(message string, params... zap.Field) {
 	fmt.Println(string(logLineAsJson))
 }
 
-func (b *basicLogger) Info(flow string, message string, params... zap.Field){
-	defer b.logger.Sync()
+func (j *jsonLogger) Info(message string, params... *Field) {
+	j.Log("info", message, params...)
+}
 
-	zapParams := []zap.Field{
-		zap.String("node", b.node),
-		zap.Int("vchain", b.vchain),
-		zap.String("flow", TransactionFlow),
+func (b *basicLogger) Info(flow string, message string, params... *Field){
+	logLineParams := []*Field{
+		String("node", b.node),
+		Int("vchain", b.vchain),
+		String("flow", TransactionFlow),
 	}
 
-	zapParams = append(zapParams, params...)
-
-	b.logger.Info(message, zapParams...)
-	b.jsonLogger.Info(message, zapParams...)
+	logLineParams = append(logLineParams, params...)
+	b.jsonLogger.Info(message, logLineParams...)
 }
 
 func getLogger(node string, vchain int, service string) BasicLogger {
-	zapLogger, _ := zap.NewProduction()
-	logger := &basicLogger{node, vchain, service, zapLogger, &jsonLogger{}}
+	logger := &basicLogger{node, vchain, service, &jsonLogger{}}
 
 	return logger
 }
@@ -104,7 +156,5 @@ func getLogger(node string, vchain int, service string) BasicLogger {
 func TestReporint(t *testing.T) {
 	logger := getLogger("node1", 123, "public-api")
 
-	txId := "1234567"
-
-	logger.Info(TransactionFlow, TransactionAccepted, zap.String("txId", txId))
+	logger.Info(TransactionFlow, TransactionAccepted, String("txId", "1234567"), Bytes("payload", []byte{1, 2, 3, 99}))
 }
