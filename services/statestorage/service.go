ce34be6429fd8034266e213ba061ba1132146497
Noam Berg <noamberg@yahoo.com>
2018-08-08 17:04:24 +0300

    temp

--- services/statestorage/service.go
+++ services/statestorage/service.go
@@ -63,6 +63,9 @@ func (s *service) ReadKeys(input *services.ReadKeysInput) (*services.ReadKeysOut
 	if input.BlockHeight + primitives.BlockHeight(s.config.GetMaxStateHistory()) <= s.lastCommittedBlockHeader.BlockHeight() {
 		return nil, fmt.Errorf("unsupported block height: block %v too old. currently at %v. keeping %v back", input.BlockHeight, s.lastCommittedBlockHeader.BlockHeight(), primitives.BlockHeight(s.config.GetMaxStateHistory()))
 	}
+	
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
 
 	contractState, err := s.persistence.ReadState(input.BlockHeight, input.ContractName)
 	if err != nil  {
