963598421126dc8605443fb15333797e3c012f36
Tal Kol <talkol@gmail.com>
2018-08-15 01:44:28 +0300

    maybe failfast is the problem

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -xe
 
 go test ./...
-go test ./test/acceptance -count 100 -failfast | grep -A 15 -- "--- FAIL:"
+go test ./test/acceptance -count 100 | grep -A 15 -- "--- FAIL:"
 test ${PIPESTATUS[0]} -eq 0
