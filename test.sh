a50ebc565a0dff6d601ccf31e4c8aca166f1f3ac
Tal Kol <talkol@gmail.com>
2018-08-15 01:40:18 +0300

    i hate bash

--- test.sh
+++ test.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -xe
 
 go test ./...
-go test ./test/acceptance -count 100 -failfast | grep -A 15 -- "--- FAIL:" ; test ${PIPESTATUS[0]} -eq 0
+go test ./test/acceptance -count 100 -failfast | grep -A 15 -- "--- FAIL:"
+test ${PIPESTATUS[0]} -eq 0
