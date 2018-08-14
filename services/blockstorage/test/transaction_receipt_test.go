27530dd01eb90362095026de0a6c9928297f2584
Kirill Maksimov <kirill@orbs.com>
2018-08-14 15:41:22 +0300

    Remove gingko from transaction_receipt_test.go

--- services/blockstorage/test/transaction_receipt_test.go
+++ services/blockstorage/test/transaction_receipt_test.go
@@ -1,82 +1,65 @@
 package test
 
 import (
-	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 	"github.com/orbs-network/orbs-network-go/crypto/digest"
 	"github.com/orbs-network/orbs-network-go/test/builders"
 	"github.com/orbs-network/orbs-spec/types/go/primitives"
 	"github.com/orbs-network/orbs-spec/types/go/services"
+	"github.com/stretchr/testify/require"
+	"testing"
 	"time"
 )
 
-var _ = Describe("Block Storage", func() {
-	When("fetching transaction receipts", func() {
-		Context("and the transaction was not found", func() {
-			It("returns an empty receipt along with the last committed block height and timestamp", func() {
-				driver := NewDriver()
-				driver.expectCommitStateDiff()
+func TestReturnTransactionReceiptIfTransactionNotFound(t *testing.T) {
+	driver := NewDriver()
+	driver.t = t
+	driver.expectCommitStateDiff()
 
-				block := builders.BlockPair().WithTimestampBloomFilter().Build()
-				driver.commitBlock(block)
+	block := builders.BlockPair().WithTimestampBloomFilter().Build()
+	driver.commitBlock(block)
 
-				out, err := driver.blockStorage.GetTransactionReceipt(&services.GetTransactionReceiptInput{
-					Txhash:               []byte("will-not-be-found"),
-					TransactionTimestamp: primitives.TimestampNano(time.Now().UnixNano()),
-				})
-
-				Expect(err).ToNot(HaveOccurred())
-				Expect(out.TransactionReceipt).To(BeNil())
-				Expect(out.BlockHeight).To(BeEquivalentTo(1))
-				Expect(out.BlockTimestamp).To(Equal(block.ResultsBlock.Header.Timestamp()))
-			})
-
-			XIt("while the transaction timestamp is in the future (and too far ahead to be in the grace)", func() {
-
-			})
-		})
-
-		Context("and the transaction is found", func() {
-			It("while the transaction timestamp is within the grace", func() {
-				driver := NewDriver()
-				driver.expectCommitStateDiff()
-
-				block := builders.BlockPair().WithTransactions(10).WithReceiptsForTransactions().WithTimestampBloomFilter().WithTimestampNow().Build()
-				driver.commitBlock(block)
-
-				// it will be similar data transactions, but with different time stamps (and hashes..)
-				block2 := builders.BlockPair().WithTransactions(10).WithReceiptsForTransactions().WithTimestampBloomFilter().WithTimestampNow().Build()
-				driver.commitBlock(block2)
-
-				// taking a transaction at 'random' (they were created at random)
-				t := block.TransactionsBlock.SignedTransactions[3].Transaction()
-				txHash := digest.CalcTxHash(t)
-
-				// the block timestamp is just a couple of nanos ahead of the transactions, which is inside the grace
-				out, err := driver.blockStorage.GetTransactionReceipt(&services.GetTransactionReceiptInput{
-					Txhash:               txHash,
-					TransactionTimestamp: t.Timestamp(),
-				})
-
-				Expect(err).ToNot(HaveOccurred())
-				Expect(out.TransactionReceipt).ToNot(BeNil())
-				Expect(out.TransactionReceipt.Txhash()).To(Equal(txHash))
-				Expect(out.BlockHeight).To(BeEquivalentTo(1))
-				Expect(out.BlockTimestamp).To(Equal(block.ResultsBlock.Header.Timestamp()))
-			})
-
-			XIt("while the transaction timestamp is outside the grace (regular)", func() {
-
-			})
-
-			XIt("while the transaction timestamp is at the expire window", func() {
-
-			})
-
-			XIt("while the transaction timestamp is at the expire window and within the grace", func() {
-
-			})
-		})
+	out, err := driver.blockStorage.GetTransactionReceipt(&services.GetTransactionReceiptInput{
+		Txhash:               []byte("will-not-be-found"),
+		TransactionTimestamp: primitives.TimestampNano(time.Now().UnixNano()),
 	})
 
-})
+	require.NoError(t, err)
+	require.Nil(t, out.TransactionReceipt)
+	require.EqualValues(t, out.BlockHeight, 1)
+	require.EqualValues(t, out.BlockTimestamp, block.ResultsBlock.Header.Timestamp())
+}
+
+// TODO return transaction receipt while the transaction timestamp is in the future (and too far ahead to be in the grace
+
+func TestReturnTransactionReceipt(t *testing.T) {
+	driver := NewDriver()
+	driver.t = t
+	driver.expectCommitStateDiff()
+
+	block := builders.BlockPair().WithTransactions(10).WithReceiptsForTransactions().WithTimestampBloomFilter().WithTimestampNow().Build()
+	driver.commitBlock(block)
+
+	// it will be similar data transactions, but with different time stamps (and hashes..)
+	block2 := builders.BlockPair().WithTransactions(10).WithReceiptsForTransactions().WithTimestampBloomFilter().WithTimestampNow().Build()
+	driver.commitBlock(block2)
+
+	// taking a transaction at 'random' (they were created at random)
+	tx := block.TransactionsBlock.SignedTransactions[3].Transaction()
+	txHash := digest.CalcTxHash(tx)
+
+	// the block timestamp is just a couple of nanos ahead of the transactions, which is inside the grace
+	out, err := driver.blockStorage.GetTransactionReceipt(&services.GetTransactionReceiptInput{
+		Txhash:               txHash,
+		TransactionTimestamp: tx.Timestamp(),
+	})
+
+	require.NoError(t, err)
+	require.NotNil(t, out.TransactionReceipt)
+	require.EqualValues(t, out.TransactionReceipt.Txhash(), txHash)
+	require.EqualValues(t, out.BlockHeight, 1)
+	require.EqualValues(t, out.BlockTimestamp, block.ResultsBlock.Header.Timestamp())
+}
+
+// TODO return transaction receipt while the transaction timestamp is outside the grace (regular)
+// TODO return transaction receipt while the transaction timestamp is at the expire window
+// TODO return transaction receipt while the transaction timestamp is at the expire window and within the grace
