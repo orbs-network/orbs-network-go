9e9ff19692fd1cdfd4e3643b9c7464939778b97e
Ido Zilberberg <idoz@orbs.com>
2018-08-19 17:38:24 +0300

    Re-added empty test for lean-helix-go (to allow manul to recognize it)

--- test/acceptance/leanhelix_noop_test.go
+++ test/acceptance/leanhelix_noop_test.go
@@ -1,1 +1,16 @@
 package acceptance
+
+import (
+	"fmt"
+	"testing"
+	"github.com/orbs-network/lean-helix-go/go/leanhelix"
+)
+
+// This is just to allow importing lean-helix-go as submodule (it must be used someplace).
+// Once that lib is used in real tests, can delete this file
+func TestLeanHelixNoOp(t *testing.T) {
+
+	s := leanhelix.NewLeanHelix()
+	fmt.Println(s)
+
+}
