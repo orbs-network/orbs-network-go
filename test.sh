e663d1acce9c8a071ebe0223cf8c7387268eaa85
Kirill Maksimov <kirill@orbs.com>
2018-08-19 13:57:43 +0300

    Fix exit codes in test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,13 @@
 #!/bin/bash -x
 
 go test -timeout 5s ./...
-go test ./test/acceptance -count 100 > test.out
+export EXIT_CODE=$?
 
+if [ $EXIT_CODE != 0 ]; then
+  exit $EXIT_CODE
+fi
+
+go test ./test/acceptance -count 100 > test.out
 export EXIT_CODE=$?
 
 cat test.out | grep -A 15 -- "--- FAIL:"
