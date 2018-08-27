7b594d8895cb5d2334811f6d496c1192833dc8d4
Ron <ron@orbs.com>
2018-08-27 17:52:49 +0300

    extract BlockTracker from InMemoryStatePersistence to the test harness

--- test/harness/services/statestorage/adapter/memory_persistence.go
+++ test/harness/services/statestorage/adapter/memory_persistence.go
@@ -2,15 +2,56 @@ package adapter
 
 import (
 	"github.com/orbs-network/orbs-network-go/services/statestorage/adapter"
+	"github.com/orbs-network/orbs-network-go/synchronization"
 	"github.com/orbs-network/orbs-spec/types/go/primitives"
+	"github.com/orbs-network/orbs-spec/types/go/protocol"
+	"time"
 )
 
 type InMemoryStatePersistence interface {
 	adapter.StatePersistence
-	Dump() string // TODO expose iterators/getters in StatePersistence and move Dump() to a wrapper struct in this file
+	Dump() string
 	WaitUntilCommittedBlockOfHeight(height primitives.BlockHeight) error
 }
 
+type TestStatePersistence struct {
+	inner                *adapter.InMemoryStatePersistence
+	blockTrackerForTests *synchronization.BlockTracker
+}
+
 func NewInMemoryStatePersistence() InMemoryStatePersistence {
-	return adapter.NewInMemoryStatePersistence()
+	return &TestStatePersistence{
+		inner:                adapter.NewInMemoryStatePersistence(),
+		blockTrackerForTests: synchronization.NewBlockTracker(0, 64000, time.Duration(1*time.Hour)),
+	}
+}
+
+func (t *TestStatePersistence) WriteState(height primitives.BlockHeight, contractStateDiffs []*protocol.ContractStateDiff) error {
+	err := t.inner.WriteState(height, contractStateDiffs)
+	if err != nil {
+		return err
+	}
+
+	t.blockTrackerForTests.IncrementHeight()
+	return nil
+}
+
+func (t *TestStatePersistence) ReadState(height primitives.BlockHeight, contract primitives.ContractName) (map[string]*protocol.StateRecord, error) {
+	return t.inner.ReadState(height, contract)
+}
+
+func (t *TestStatePersistence) WriteMerkleRoot(height primitives.BlockHeight, sha256 primitives.MerkleSha256) error {
+	return t.inner.WriteMerkleRoot(height, sha256)
+}
+
+func (t *TestStatePersistence) ReadMerkleRoot(height primitives.BlockHeight) (primitives.MerkleSha256, error) {
+	return t.inner.ReadMerkleRoot(height)
+}
+
+func (t *TestStatePersistence) Dump() string {
+	return t.inner.Dump()
+}
+
+func (t *TestStatePersistence) WaitUntilCommittedBlockOfHeight(height primitives.BlockHeight) error {
+	return t.blockTrackerForTests.WaitForBlock(height)
 }
