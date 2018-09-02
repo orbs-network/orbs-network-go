fddb6c410a5eff139a0dfb4683fb11dd77a1a262
Kirill Maksimov <kirill@orbs.com>
2018-09-02 15:00:23 +0300

    Refactor block sync unit test

---
+++ services/blockstorage/block_sync_mocks_test.go
@@ -0,0 +1,33 @@
+package blockstorage
+
+import (
+	"github.com/orbs-network/go-mock"
+	"github.com/orbs-network/orbs-spec/types/go/primitives"
+	"github.com/orbs-network/orbs-spec/types/go/protocol"
+	"github.com/orbs-network/orbs-spec/types/go/services"
+)
+
+type blockSyncStorageMock struct {
+	mock.Mock
+}
+
+func (s *blockSyncStorageMock) GetBlocks(first primitives.BlockHeight, last primitives.BlockHeight) (blocks []*protocol.BlockPairContainer, firstAvailableBlockHeight primitives.BlockHeight, lastAvailableBlockHeight primitives.BlockHeight) {
+	panic("not mocked")
+}
+
+func (s *blockSyncStorageMock) LastCommittedBlockHeight() primitives.BlockHeight {
+	ret := s.Called()
+	return ret.Get(0).(primitives.BlockHeight)
+}
+
+func (s *blockSyncStorageMock) CommitBlock(input *services.CommitBlockInput) (*services.CommitBlockOutput, error) {
+	panic("not mocked")
+}
+
+func (s *blockSyncStorageMock) ValidateBlockForCommit(input *services.ValidateBlockForCommitInput) (*services.ValidateBlockForCommitOutput, error) {
+	panic("not mocked")
+}
+
+func (s *blockSyncStorageMock) UpdateConsensusAlgosAboutLatestCommittedBlock() {
+	s.Called()
+}
--- services/blockstorage/block_sync_test.go
+++ services/blockstorage/block_sync_test.go
@@ -5,40 +5,19 @@ import (
 	"github.com/orbs-network/orbs-network-go/config"
 	"github.com/orbs-network/orbs-network-go/instrumentation/log"
 	"github.com/orbs-network/orbs-spec/types/go/primitives"
-	"github.com/orbs-network/orbs-spec/types/go/protocol"
 	"github.com/orbs-network/orbs-spec/types/go/protocol/gossipmessages"
-	"github.com/orbs-network/orbs-spec/types/go/services"
 	"github.com/orbs-network/orbs-spec/types/go/services/gossiptopics"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
-type blockSyncStorageMock struct {
-	mock.Mock
+type blockSyncHarness struct {
+	blockSync *BlockSync
+	gossip    *gossiptopics.MockBlockSync
+	storage   *blockSyncStorageMock
 }
 
-func (s *blockSyncStorageMock) GetBlocks(first primitives.BlockHeight, last primitives.BlockHeight) (blocks []*protocol.BlockPairContainer, firstAvailableBlockHeight primitives.BlockHeight, lastAvailableBlockHeight primitives.BlockHeight) {
-	panic("not mocked")
-}
-
-func (s *blockSyncStorageMock) LastCommittedBlockHeight() primitives.BlockHeight {
-	ret := s.Called()
-	return ret.Get(0).(primitives.BlockHeight)
-}
-
-func (s *blockSyncStorageMock) CommitBlock(input *services.CommitBlockInput) (*services.CommitBlockOutput, error) {
-	panic("not mocked")
-}
-
-func (s *blockSyncStorageMock) ValidateBlockForCommit(input *services.ValidateBlockForCommitInput) (*services.ValidateBlockForCommitOutput, error) {
-	panic("not mocked")
-}
-
-func (s *blockSyncStorageMock) UpdateConsensusAlgosAboutLatestCommittedBlock() {
-	s.Called()
-}
-
-func TestTransitionFromSyncStartToCollectAvailabilityResponses(t *testing.T) {
+func newBlockSyncHarness() *blockSyncHarness {
 	cfg := config.EmptyConfig()
 	gossip := &gossiptopics.MockBlockSync{}
 	storage := &blockSyncStorageMock{}
@@ -51,20 +30,34 @@ func TestTransitionFromSyncStartToCollectAvailabilityResponses(t *testing.T) {
 		events:    nil,
 	}
 
+	return &blockSyncHarness{
+		blockSync: blockSync,
+		gossip:    gossip,
+		storage:   storage,
+	}
+}
+
+func (h *blockSyncHarness) verifyMocks(t *testing.T) {
+	ok, err := mock.VerifyMocks(h.storage, h.gossip)
+	require.NoError(t, err)
+	require.True(t, ok)
+
+}
+
+func TestTransitionFromSyncStartToCollectAvailabilityResponses(t *testing.T) {
+	harness := newBlockSyncHarness()
+
 	var event interface{}
-	var blockAvailabilityResponses []*gossipmessages.BlockAvailabilityResponseMessage
+	var responses []*gossipmessages.BlockAvailabilityResponseMessage
 
-	storage.When("UpdateConsensusAlgosAboutLatestCommittedBlock").Return().Times(1)
-	storage.When("LastCommittedBlockHeight").Return(primitives.BlockHeight(10)).Times(1)
+	harness.storage.When("UpdateConsensusAlgosAboutLatestCommittedBlock").Return().Times(1)
+	harness.storage.When("LastCommittedBlockHeight").Return(primitives.BlockHeight(10)).Times(1)
+	harness.gossip.When("BroadcastBlockAvailabilityRequest", mock.Any).Return(nil, nil).Times(1)
 
-	gossip.When("BroadcastBlockAvailabilityRequest", mock.Any).Return(nil, nil).Times(1)
-
-	newState, responses := blockSync.transitionState(BLOCK_SYNC_STATE_START_SYNC, event, blockAvailabilityResponses)
+	newState, responses := harness.blockSync.transitionState(BLOCK_SYNC_STATE_START_SYNC, event, responses)
 
 	require.Equal(t, BLOCK_SYNC_PETITIONER_COLLECTING_AVAILABILITY_RESPONSES, newState)
 	require.Empty(t, responses, "no responses were sent yet")
 
-	ok, err := mock.VerifyMocks(storage, gossip)
-	require.NoError(t, err)
-	require.True(t, ok)
+	harness.verifyMocks(t)
 }
