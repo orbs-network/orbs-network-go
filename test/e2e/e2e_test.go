2e72f639d01a077de38f3a5b9299b6f7b352023f
Kirill Maksimov <kirill@orbs.com>
2018-09-02 13:47:29 +0300

    Remove gomega from e2e

--- test/e2e/e2e_test.go
+++ test/e2e/e2e_test.go
@@ -4,11 +4,11 @@ import (
 	"bytes"
 	"fmt"
 	. "github.com/onsi/ginkgo"
-	. "github.com/onsi/gomega"
 	"github.com/orbs-network/membuffers/go"
 	"github.com/orbs-network/orbs-network-go/bootstrap"
 	"github.com/orbs-network/orbs-network-go/config"
 	"github.com/orbs-network/orbs-network-go/instrumentation/log"
+	"github.com/orbs-network/orbs-network-go/test"
 	"github.com/orbs-network/orbs-network-go/test/builders"
 	"github.com/orbs-network/orbs-network-go/test/crypto/keys"
 	gossipAdapter "github.com/orbs-network/orbs-network-go/test/harness/services/gossip/adapter"
@@ -16,6 +16,7 @@ import (
 	"github.com/orbs-network/orbs-spec/types/go/protocol/client"
 	"github.com/orbs-network/orbs-spec/types/go/protocol/consensus"
 	"github.com/orbs-network/orbs-spec/types/go/services"
+	"github.com/stretchr/testify/require"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -32,7 +33,6 @@ func TestE2E(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Skipping E2E tests in short mode")
 	}
-	RegisterFailHandler(Fail)
 	RunSpecs(t, "E2E Suite")
 }
 
@@ -50,96 +50,92 @@ func getConfig() E2EConfig {
 	}
 }
 
-var _ = Describe("The Orbs Network", func() {
-	It("accepts a transaction and reflects the state change after it is committed", func(done Done) {
-		var nodes []bootstrap.Node
+func TestOrbsNetworkAcceptsTransactionAndCommitsIt(t *testing.T) {
+	var nodes []bootstrap.Node
 
-		// TODO: kill me - why do we need this override?
-		if getConfig().Bootstrap {
-			gossipTransport := gossipAdapter.NewTamperingTransport()
+	// TODO: kill me - why do we need this override?
+	if getConfig().Bootstrap {
+		gossipTransport := gossipAdapter.NewTamperingTransport()
 
-			federationNodes := make(map[string]config.FederationNode)
-			leaderKeyPair := keys.Ed25519KeyPairForTests(0)
-			for i := 0; i < 3; i++ {
-				nodeKeyPair := keys.Ed25519KeyPairForTests(i)
-				federationNodes[nodeKeyPair.PublicKey().KeyForMap()] = config.NewHardCodedFederationNode(nodeKeyPair.PublicKey())
-			}
-
-			logger := log.GetLogger().WithOutput(log.NewOutput(os.Stdout).WithFormatter(log.NewHumanReadableFormatter()))
-
-			for i := 0; i < 3; i++ {
-				nodeKeyPair := keys.Ed25519KeyPairForTests(i)
-				node := bootstrap.NewNode(
-					fmt.Sprintf(":%d", 8080+i),
-					nodeKeyPair.PublicKey(),
-					nodeKeyPair.PrivateKey(),
-					federationNodes,
-					leaderKeyPair.PublicKey(),
-					consensus.CONSENSUS_ALGO_TYPE_BENCHMARK_CONSENSUS,
-					logger,
-					gossipTransport,
-				)
-
-				nodes = append(nodes, node)
-			}
-
-			// To let node start up properly, otherwise in Docker we get connection refused
-			time.Sleep(100 * time.Millisecond)
+		federationNodes := make(map[string]config.FederationNode)
+		leaderKeyPair := keys.Ed25519KeyPairForTests(0)
+		for i := 0; i < 3; i++ {
+			nodeKeyPair := keys.Ed25519KeyPairForTests(i)
+			federationNodes[nodeKeyPair.PublicKey().KeyForMap()] = config.NewHardCodedFederationNode(nodeKeyPair.PublicKey())
 		}
 
-		tx := builders.TransferTransaction().WithAmount(17).Builder()
+		logger := log.GetLogger().WithOutput(log.NewOutput(os.Stdout).WithFormatter(log.NewHumanReadableFormatter()))
 
-		_ = sendTransaction(tx)
+		for i := 0; i < 3; i++ {
+			nodeKeyPair := keys.Ed25519KeyPairForTests(i)
+			node := bootstrap.NewNode(
+				fmt.Sprintf(":%d", 8080+i),
+				nodeKeyPair.PublicKey(),
+				nodeKeyPair.PrivateKey(),
+				federationNodes,
+				leaderKeyPair.PublicKey(),
+				consensus.CONSENSUS_ALGO_TYPE_BENCHMARK_CONSENSUS,
+				logger,
+				gossipTransport,
+			)
 
-		m := &protocol.TransactionBuilder{
-			ContractName: "BenchmarkToken",
-			MethodName:   "getBalance",
+			nodes = append(nodes, node)
 		}
 
-		Eventually(func() uint64 {
-			outputArgsIterator := builders.ClientCallMethodResponseOutputArgumentsParse(callMethod(m).ClientResponse)
-			if outputArgsIterator.HasNext() {
-				return outputArgsIterator.NextArguments().Uint64Value()
-			} else {
-				return 0
-			}
-		}).Should(BeEquivalentTo(17))
+		// To let node start up properly, otherwise in Docker we get connection refused
+		time.Sleep(100 * time.Millisecond)
+	}
 
-		if getConfig().Bootstrap {
-			for _, node := range nodes {
-				node.GracefulShutdown(1 * time.Second)
-			}
+	tx := builders.TransferTransaction().WithAmount(17).Builder()
+
+	_ = sendTransaction(t, tx)
+
+	m := &protocol.TransactionBuilder{
+		ContractName: "BenchmarkToken",
+		MethodName:   "getBalance",
+	}
+
+	test.Eventually(func() bool {
+		outputArgsIterator := builders.ClientCallMethodResponseOutputArgumentsParse(callMethod(t, m).ClientResponse)
+		if outputArgsIterator.HasNext() {
+			return outputArgsIterator.NextArguments().Uint64Value() == 17
+		} else {
+			return false
 		}
+	})
 
-		close(done)
-	}, 10)
-})
+	if getConfig().Bootstrap {
+		for _, node := range nodes {
+			node.GracefulShutdown(1 * time.Second)
+		}
+	}
+}
 
-func sendTransaction(txBuilder *protocol.SignedTransactionBuilder) *services.SendTransactionOutput {
+func sendTransaction(t *testing.T, txBuilder *protocol.SignedTransactionBuilder) *services.SendTransactionOutput {
 	input := (&client.SendTransactionRequestBuilder{
 		SignedTransaction: txBuilder,
 	}).Build()
 
-	return &services.SendTransactionOutput{ClientResponse: client.SendTransactionResponseReader(httpPost(input, "send-transaction"))}
+	return &services.SendTransactionOutput{ClientResponse: client.SendTransactionResponseReader(httpPost(t, input, "send-transaction"))}
 }
 
-func callMethod(txBuilder *protocol.TransactionBuilder) *services.CallMethodOutput {
+func callMethod(t *testing.T, txBuilder *protocol.TransactionBuilder) *services.CallMethodOutput {
 	input := (&client.CallMethodRequestBuilder{
 		Transaction: txBuilder,
 	}).Build()
 
-	return &services.CallMethodOutput{ClientResponse: client.CallMethodResponseReader(httpPost(input, "call-method"))}
+	return &services.CallMethodOutput{ClientResponse: client.CallMethodResponseReader(httpPost(t, input, "call-method"))}
 
 }
 
-func httpPost(input membuffers.Message, method string) []byte {
+func httpPost(t *testing.T, input membuffers.Message, method string) []byte {
 	res, err := http.Post(getConfig().ApiEndpoint+method, "application/octet-stream", bytes.NewReader(input.Raw()))
-	Expect(err).ToNot(HaveOccurred())
-	Expect(res.StatusCode).To(Equal(http.StatusOK))
+	require.NoError(t, err)
+	require.Equal(t, res.StatusCode, http.StatusOK)
 
 	bytes, err := ioutil.ReadAll(res.Body)
 	defer res.Body.Close()
-	Expect(err).ToNot(HaveOccurred())
+	require.NoError(t, err)
 
 	return bytes
 }
