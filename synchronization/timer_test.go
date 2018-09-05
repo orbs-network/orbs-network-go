6b9c7af4d112eb7f6081fafd48e9b9cbe8ed5e8c
Jonathan Levison <jonathanl@orbs.com>
2018-09-05 09:07:57 +0300

    made tests faster, added testify

--- synchronization/timer_test.go
+++ synchronization/timer_test.go
@@ -2,6 +2,7 @@ package synchronization_test
 
 import (
 	"github.com/orbs-network/orbs-network-go/synchronization"
+	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
 )
@@ -9,26 +10,27 @@ import (
 func TestTimer_Reset(t *testing.T) {
 	start := time.Now()
 
-	timer := synchronization.NewTimer(10 * time.Millisecond)
-	time.Sleep(20 * time.Millisecond)
-	timer.Reset(10 * time.Millisecond)
+	// replacing synchronization to time (using the standard timer) will break the test and show the issue
+	timer := synchronization.NewTimer(5 * time.Millisecond)
+	time.Sleep(7 * time.Millisecond)
+	timer.Reset(2 * time.Millisecond)
 
 	// See issue: https://github.com/golang/go/issues/11513 , this timer should solve it, the reset above should 'reset' the channel as well
 	<-timer.C
 
 	runtime := time.Since(start).Seconds()
 
-	if runtime < 0.03 {
-		t.Errorf("took ~%v milliseconds, should be ~30 milliseconds\n", runtime*1000)
+	if runtime < 0.009 {
+		t.Errorf("took ~%v milliseconds, should be ~9 milliseconds\n", runtime*1000)
 	}
 }
 
 func TestTimer_Stop(t *testing.T) {
-	timer := synchronization.NewTimer(10 * time.Millisecond)
-	time.Sleep(20 * time.Millisecond)
+	timer := synchronization.NewTimer(2 * time.Millisecond)
+	time.Sleep(3 * time.Millisecond)
 	timer.Stop()
 	releaseC := make(chan bool, 1)
-	time.AfterFunc(10*time.Millisecond, func() { releaseC <- true })
+	time.AfterFunc(1*time.Millisecond, func() { releaseC <- true })
 
 	select {
 	case <-timer.C:
@@ -39,22 +41,18 @@ func TestTimer_Stop(t *testing.T) {
 
 func TestTimer_GetTimer(t *testing.T) {
 	x := synchronization.NewTimer(time.Second)
-	if x == nil {
-		t.Error("cannot create new timer")
-	}
+	require.NotNil(t, x, "cannot create new timer")
 
 	timer := x.GetTimer()
-	if timer == nil {
-		t.Error("timer inner object was not created, something is really wrong")
-	}
+	require.NotNil(t, timer, "timer inner object was not created, something is really wrong")
 }
 
 func TestValidTimerStopOp(t *testing.T) {
 	// testing that a timer stopped before it was expired does not later ping
-	timer := synchronization.NewTimer(5 * time.Millisecond)
+	timer := synchronization.NewTimer(1 * time.Millisecond)
 	timer.Stop()
 	releaseC := make(chan bool, 1)
-	time.AfterFunc(10*time.Millisecond, func() { releaseC <- true })
+	time.AfterFunc(2*time.Millisecond, func() { releaseC <- true })
 
 	select {
 	case <-timer.C:
@@ -67,13 +65,13 @@ func TestValidTimerResetOp(t *testing.T) {
 	// testing that a timer reset before it was expired does not ping too early
 	start := time.Now()
 
-	timer := synchronization.NewTimer(5 * time.Millisecond)
-	timer.Reset(10 * time.Millisecond)
+	timer := synchronization.NewTimer(2 * time.Millisecond)
+	timer.Reset(3 * time.Millisecond)
 	<-timer.C
 
 	runtime := time.Since(start).Seconds()
 
-	if runtime < 0.01 {
-		t.Errorf("took ~%v milliseconds, should be ~10 milliseconds\n", runtime*1000)
+	if runtime < 0.003 {
+		t.Errorf("took ~%v milliseconds, should be ~3 milliseconds\n", runtime*1000)
 	}
 }
