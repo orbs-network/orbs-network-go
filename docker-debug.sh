7485b6ca2b9698bd39b26eb91f5bc779c433780a
Kirill Maksimov <kirill@orbs.com>
2018-09-09 15:02:25 +0300

    Refactor docker debug mode

--- docker-debug.sh
+++ docker-debug.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -xe
 
 rm -rf logs
-docker-compose -f docker-compose.debug.yml up --build --abort-on-container-exit --exit-code-from orbs-e2e
+docker-compose -f docker-compose.debug.yml up --abort-on-container-exit --exit-code-from orbs-e2e
