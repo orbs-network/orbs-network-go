a46f711f462e38a6a0b4e060eead530b2df3b7dd
Kirill Maksimov <kirill@orbs.com>
2018-09-12 12:19:35 +0300

    Fix build for binaries

--- build-binaries.sh
+++ build-binaries.sh
@@ -4,7 +4,7 @@ CGO_ENABLED=0 go build -o orbs-node -a -ldflags '-extldflags "-static"' main.go
 
 CGO_ENABLED=0 go test -a -ldflags '-extldflags "-static"' -c ./test/e2e
 
-if [ "$SKIP_DEVTOOLS" != "" ]; then
+if [ "$SKIP_DEVTOOLS" == "" ]; then
     CGO_ENABLED=0 go build -o orbs-json-client -a -ldflags '-extldflags "-static"' devtools/jsonapi/main/main.go
 
     CGO_ENABLED=0 go build -o sambusac -a -ldflags '-extldflags "-static"' devtools/sambusac/main/main.go
