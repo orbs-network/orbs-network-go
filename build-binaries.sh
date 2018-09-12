b644fb4fb0df10dacbce989896820a8ef625d469
Kirill Maksimov <kirill@orbs.com>
2018-09-12 12:25:04 +0300

    Measure time for static compilation

--- build-binaries.sh
+++ build-binaries.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -x
 
-CGO_ENABLED=0 go build -o orbs-node -a -ldflags '-extldflags "-static"' main.go
+CGO_ENABLED=0 time go build -o orbs-node -a -ldflags '-extldflags "-static"' main.go
 
-CGO_ENABLED=0 go test -a -ldflags '-extldflags "-static"' -c ./test/e2e
+CGO_ENABLED=0 time go test -a -ldflags '-extldflags "-static"' -c ./test/e2e
 
 if [ "$SKIP_DEVTOOLS" == "" ]; then
-    CGO_ENABLED=0 go build -o orbs-json-client -a -ldflags '-extldflags "-static"' devtools/jsonapi/main/main.go
+    CGO_ENABLED=0 time go build -o orbs-json-client -a -ldflags '-extldflags "-static"' devtools/jsonapi/main/main.go
 
-    CGO_ENABLED=0 go build -o sambusac -a -ldflags '-extldflags "-static"' devtools/sambusac/main/main.go
+    CGO_ENABLED=0 time go build -o sambusac -a -ldflags '-extldflags "-static"' devtools/sambusac/main/main.go
 fi
