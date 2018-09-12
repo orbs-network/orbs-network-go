1dd97ebc45f3c2cae0c45ee63d5d5e6bc184dcf6
Ido Zilberberg <idoz@orbs.com>
2018-09-12 19:41:59 +0300

    Added single test

--- test.sh
+++ test.sh
@@ -37,10 +37,15 @@ run_all_tests () {
     return $rc
 }
 
+run_specific_test() {
+    time go test ./services/blockstorage/test -v -run TestSyncCompletePetitionerSyncFlow ${SERIAL} -count ${COUNT} -timeout ${MULTIPLE_RUNS_TIMEOUT} -failfast 2&>1 >> ${OUT}
+
+}
 run_component_tests () {
 
     # "blockstorage/test" "consensusalgo/benchmarkconsensus/test" "consensuscontext/test" "gossip/adapter" "processor/native/test" "publicapi/test" "statestorage/test" "transactionpool/test" "virtualmachine/test"
-    for comp in "blockstorage/test" "consensusalgo/benchmarkconsensus/test" "consensuscontext/test" "gossip/adapter" "processor/native/test" "publicapi/test" "statestorage/test" "transactionpool/test" "virtualmachine/test"
+#    for comp in "blockstorage/test" "consensusalgo/benchmarkconsensus/test" "consensuscontext/test" "gossip/adapter" "processor/native/test" "publicapi/test" "statestorage/test" "transactionpool/test" "virtualmachine/test"
+    for comp in "blockstorage/test"
     do
         echo "=====> $(get_date) Starting to run component tests under ${comp} ${COUNT} times <=====" | tee -a ${OUT}
         time go test ./services/${comp} -v ${SERIAL} -count ${COUNT} -timeout ${MULTIPLE_RUNS_TIMEOUT} -failfast 2&>1 >> ${OUT}
@@ -72,11 +77,12 @@ echo "@@@@@ STARTING TO RUN TESTS (output: ${OUT}) @@@@@\n" | tee ${OUT}
 #run_all_tests
 #check_exit_code_and_report
 
-run_component_tests
+#run_component_tests
+run_specific_test
 check_exit_code_and_report
 
-run_acceptance_tests
-check_exit_code_and_report
+#run_acceptance_tests
+#check_exit_code_and_report
 
 echo "\n@@@@@ FINISHED RUNNING ALL TESTS (output: ${OUT}) @@@@@" | tee -a ${OUT}
 echo "\n\n@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n" | tee -a ${OUT}
