9f65bd187d65e93a698488c9f2df85132be8c874
Kirill Maksimov <kirill@orbs.com>
2018-09-13 17:31:10 +0300

    Reduce amount of logs generated

--- test.common.sh
+++ test.common.sh
@@ -8,8 +8,8 @@ check_exit_code_and_report () {
     export EXIT_CODE=$?
 
     if [ $EXIT_CODE != 0 ]; then
-        cat test.out | grep -B 150 -A 15 -- "FAIL:"
-        cat test.out | grep -B 150 -A 150 -- "timed out"
+        grep -B 150 -A 15 -- "FAIL:" test.out
+        grep -B 150 -A 150 -- "test timed out" test.out
 
         exit $EXIT_CODE
     fi
