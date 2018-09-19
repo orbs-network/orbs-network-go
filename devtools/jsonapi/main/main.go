0ba9c9374c81648401452c8339cd241e4deb8edd
itamar Arjuan <itamar.arjuan@gmail.com>
2018-09-19 13:00:40 +0300

    adding some spacing in help message

--- devtools/jsonapi/main/main.go
+++ devtools/jsonapi/main/main.go
@@ -14,14 +14,19 @@ func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Welcome to gamma-cli")
 		fmt.Println("Example usage:")
+		fmt.Println("")
 		fmt.Println("$ gamma-cli start")
 		fmt.Println("  Start gamma-server with 3 Orbs virtual nodes")
+		fmt.Println("")
 		fmt.Println("$ gamma-cli deploy MyContractName path/to/some/contract.go")
 		fmt.Println("  Deploy your contract code onto the running blockchain on your local machine")
+		fmt.Println("")
 		fmt.Println("$ gamma-cli run send path/to/operation.json")
 		fmt.Println("  Perform a contract method which mutates state")
+		fmt.Println("")
 		fmt.Println("$ gamma-cli run call path/to/operation.json")
 		fmt.Println("  Perform a contract method which reads from state")
+		fmt.Println("")
 		os.Exit(1)
 	}
 
