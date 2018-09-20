108ca29a44ea98a28e6d3a651eec57d0ae18ebed
itamar Arjuan <itamar.arjuan@gmail.com>
2018-09-20 14:50:32 +0300

    removed a silly old git ignore rule.

---
+++ devtools/gamma-server/main/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"flag"
+	"github.com/orbs-network/orbs-network-go/devtools/jsonapi"
+	"strconv"
+)
+
+func main() {
+	port := flag.Int("port", 8080, "The port to bind the gamma-server to")
+	flag.Parse()
+
+	var serverAddress = ":" + strconv.Itoa(*port)
+
+	// TODO Remove the blocking boolean flag from here and make it an environment variable. (Shouldn't concern our users who will see how we bootstrap this)
+	jsonapi.StartGammaServer(serverAddress, true)
+}
