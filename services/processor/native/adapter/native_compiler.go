01731beb8965fb722730460f20db88074914b785
Tal Kol <talkol@gmail.com>
2018-09-25 20:43:20 +0300

    Gave more time for warm up compilation time

--- services/processor/native/adapter/native_compiler.go
+++ services/processor/native/adapter/native_compiler.go
@@ -20,7 +20,8 @@ import (
 const SOURCE_CODE_PATH = "native-src"
 const SHARED_OBJECT_PATH = "native-bin"
 const GC_CACHE_PATH = "native-cache"
-const MAX_COMPILATION_TIME = 5 * time.Second // TODO: maybe move to config or maybe have caller provide via context
+const MAX_COMPILATION_TIME = 5 * time.Second          // TODO: maybe move to config or maybe have caller provide via context
+const MAX_WARM_UP_COMPILATION_TIME = 15 * time.Second // TODO: maybe move to config or maybe have caller provide via context
 
 type Config interface {
 	ProcessorArtifactPath() string
@@ -44,7 +45,7 @@ func NewNativeCompiler(config Config, reporting log.BasicLogger) Compiler {
 }
 
 func (c *nativeCompiler) warmUpCompilationCache() {
-	ctx, cancel := context.WithTimeout(context.Background(), MAX_COMPILATION_TIME)
+	ctx, cancel := context.WithTimeout(context.Background(), MAX_WARM_UP_COMPILATION_TIME)
 	defer cancel()
 
 	_, err := c.Compile(ctx, string(contracts.SourceCodeForNop()))
