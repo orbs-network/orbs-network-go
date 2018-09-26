9f3dc31b59cca08ea1a6c96ea34c21ae4d09b8b4
Kirill Maksimov <kirill@orbs.com>
2018-09-26 13:17:15 +0300

    Refactor multiple transactions tests

--- test/e2e/multiple_transaction_test.go
+++ test/e2e/multiple_transaction_test.go
@@ -4,21 +4,19 @@ import (
 	"github.com/orbs-network/orbs-network-go/test"
 	"github.com/orbs-network/orbs-network-go/test/builders"
 	"github.com/orbs-network/orbs-network-go/test/crypto/keys"
+	"github.com/orbs-network/orbs-spec/types/go/primitives"
 	"github.com/orbs-network/orbs-spec/types/go/protocol"
 	"github.com/stretchr/testify/require"
 	"math/rand"
 	"testing"
 )
 
-func sendAmount(t *testing.T, h *harness, amount uint64) {
-	signerKeyPair := keys.Ed25519KeyPairForTests(5)
-	targetAddress := builders.AddressForEd25519SignerForTests(6)
+func sendAmount(t *testing.T, h *harness, amount uint64, signerKeyPair *keys.Ed25519KeyPair, targetAddress primitives.Ripmd160Sha256) {
 	transfer := builders.TransferTransaction().WithEd25519Signer(signerKeyPair).WithAmountAndTargetAddress(amount, targetAddress).Builder()
 	response, err := h.sendTransaction(t, transfer)
 	require.NoError(t, err, "transaction for amount %d should not return error", amount)
 	require.Equal(t, protocol.TRANSACTION_STATUS_COMMITTED, response.TransactionStatus(), "transaction for amount %d should be successfully committed", amount)
 	require.Equal(t, protocol.EXECUTION_RESULT_SUCCESS, response.TransactionReceipt().ExecutionResult(), "transaction for amount %d should execute successfully", amount)
-
 }
 
 func TestNetworkCommitsMultipleTransactions(t *testing.T) {
@@ -32,7 +30,9 @@ func TestNetworkCommitsMultipleTransactions(t *testing.T) {
 	// send 3 transactions with total of 70
 	amounts := []uint64{15, 22, 33}
 	for _, amount := range amounts {
-		sendAmount(t, h, amount)
+		signerKeyPair := keys.Ed25519KeyPairForTests(5)
+		targetAddress := builders.AddressForEd25519SignerForTests(6)
+		sendAmount(t, h, amount, signerKeyPair, targetAddress)
 	}
 
 	// check balance
@@ -52,7 +52,7 @@ func TestNetworkCommitsMultipleTransactions(t *testing.T) {
 	require.True(t, ok, "getBalance should return total amount")
 }
 
-func TestMultipleTransactionsProcessingTime(t *testing.T) {
+func TestMultipleTransactionsInBatches(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Skipping E2E tests in short mode")
 	}
@@ -61,7 +61,7 @@ func TestMultipleTransactionsProcessingTime(t *testing.T) {
 	defer h.gracefulShutdown()
 
 	const numOfBrackets = 10
-	const txPerBracket = 1
+	const txPerBracket = 10
 
 	amounts := [numOfBrackets][txPerBracket]uint64{}
 	sum := uint64(0)
@@ -74,20 +74,21 @@ func TestMultipleTransactionsProcessingTime(t *testing.T) {
 		}
 	}
 
-	sendAmount(t, h, 0)
+	signerKeyPair := keys.Ed25519KeyPairForTests(8)
+	targetAddress := builders.AddressForEd25519SignerForTests(9)
+
+	sendAmount(t, h, 0, signerKeyPair, targetAddress)
 
 	for _, bracket := range amounts {
 		for _, amount := range bracket {
-			go func() {
-				sendAmount(t, h, amount)
-			}()
+			go sendAmount(t, h, amount, signerKeyPair, targetAddress)
 		}
 	}
 
 	// check balance
 	ok := test.Eventually(test.EVENTUALLY_DOCKER_E2E_TIMEOUT, func() bool {
-		signerKeyPair := keys.Ed25519KeyPairForTests(6)
-		targetAddress := builders.AddressForEd25519SignerForTests(6)
+		signerKeyPair := keys.Ed25519KeyPairForTests(9)
+		targetAddress := builders.AddressForEd25519SignerForTests(9)
 		getBalance := builders.GetBalanceTransaction().WithEd25519Signer(signerKeyPair).WithTargetAddress(targetAddress).Builder().Transaction
 		response, err := h.callMethod(t, getBalance)
 		if err == nil && response.CallMethodResult() == protocol.EXECUTION_RESULT_RESERVED { // TODO: this is a bug, change to EXECUTION_RESULT_SUCCESS
