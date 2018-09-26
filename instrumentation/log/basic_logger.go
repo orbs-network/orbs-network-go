fa609257a401eec87fbbacfbe22b63e97d8db478
Shai Yallin <shai.yallin@gmail.com>
2018-09-26 12:07:10 +0300

    WIP

--- instrumentation/log/basic_logger.go
+++ instrumentation/log/basic_logger.go
@@ -1,11 +1,8 @@
 package log
 
 import (
-	"encoding/hex"
 	"fmt"
-	"github.com/orbs-network/orbs-spec/types/go/primitives"
 	"os"
-	"reflect"
 	"runtime"
 	"strings"
 	"time"
@@ -21,6 +18,7 @@ type BasicLogger interface {
 	Meter(name string, params ...*Field) BasicMeter
 	Prefixes() []*Field
 	WithOutput(writer ...Output) BasicLogger
+	WithFilter(filter *Field) BasicLogger
 }
 
 type basicLogger struct {
@@ -28,9 +26,8 @@ type basicLogger struct {
 	prefixes              []*Field
 	nestingLevel          int
 	sourceRootPrefixIndex int
+	filters               []*Field
 }
-
-type FieldType uint8
 
 const (
 	NoType = iota
@@ -46,106 +43,6 @@ const (
 	SourceType
 	StringArrayType
 )
-
-type Field struct {
-	Key  string
-	Type FieldType
-
-	String      string
-	StringArray []string
-	Int         int64
-	Uint        uint64
-	Bytes       []byte
-	Float       float64
-
-	Error error
-}
-
-func Node(value string) *Field {
-	return &Field{Key: "node", String: value, Type: NodeType}
-}
-
-func Service(value string) *Field {
-	return &Field{Key: "service", String: value, Type: ServiceType}
-}
-
-func Function(value string) *Field {
-	return &Field{Key: "function", String: value, Type: FunctionType}
-}
-
-func Source(value string) *Field {
-	return &Field{Key: "source", String: value, Type: SourceType}
-}
-
-func String(key string, value string) *Field {
-	return &Field{Key: key, String: value, Type: StringType}
-}
-
-func Stringable(key string, value fmt.Stringer) *Field {
-	return &Field{Key: key, String: value.String(), Type: StringType}
-}
-
-func StringableSlice(key string, values interface{}) *Field {
-	var strings []string
-	switch reflect.TypeOf(values).Kind() {
-	case reflect.Slice:
-		s := reflect.ValueOf(values)
-
-		strings = make([]string, 0, s.Len())
-
-		for i := 0; i < s.Len(); i++ {
-			if stringer, ok := s.Index(i).Interface().(fmt.Stringer); ok {
-				strings = append(strings, stringer.String())
-			}
-		}
-	}
-
-	return &Field{Key: key, StringArray: strings, Type: StringArrayType}
-}
-
-func Int(key string, value int) *Field {
-	return &Field{Key: key, Int: int64(value), Type: IntType}
-}
-
-func Int32(key string, value int32) *Field {
-	return &Field{Key: key, Int: int64(value), Type: IntType}
-}
-
-func Int64(key string, value int64) *Field {
-	return &Field{Key: key, Int: int64(value), Type: IntType}
-}
-
-func Bytes(key string, value []byte) *Field {
-	return &Field{Key: key, Bytes: value, Type: BytesType}
-}
-
-func Uint(key string, value uint) *Field {
-	return &Field{Key: key, Uint: uint64(value), Type: UintType}
-}
-
-func Uint32(key string, value uint32) *Field {
-	return &Field{Key: key, Uint: uint64(value), Type: UintType}
-}
-
-func Uint64(key string, value uint64) *Field {
-	return &Field{Key: key, Uint: value, Type: UintType}
-}
-
-func Float32(key string, value float32) *Field {
-	return &Field{Key: key, Float: float64(value), Type: FloatType}
-}
-
-func Float64(key string, value float64) *Field {
-	return &Field{Key: key, Float: value, Type: FloatType}
-}
-
-func Error(value error) *Field {
-	return &Field{Key: "error", Error: value, Type: ErrorType}
-}
-
-func BlockHeight(value primitives.BlockHeight) *Field {
-	return &Field{Key: "block-height", String: value.String(), Type: StringType}
-}
 
 func GetLogger(params ...*Field) BasicLogger {
 	logger := &basicLogger{
@@ -198,7 +95,7 @@ func (b *basicLogger) Prefixes() []*Field {
 
 func (b *basicLogger) For(params ...*Field) BasicLogger {
 	prefixes := append(b.prefixes, params...)
-	return &basicLogger{prefixes: prefixes, nestingLevel: b.nestingLevel, outputs: b.outputs, sourceRootPrefixIndex: b.sourceRootPrefixIndex}
+	return &basicLogger{prefixes: prefixes, nestingLevel: b.nestingLevel, outputs: b.outputs, sourceRootPrefixIndex: b.sourceRootPrefixIndex, filters: b.filters}
 }
 
 func (b *basicLogger) Metric(metric string, params ...*Field) {
@@ -206,41 +103,15 @@ func (b *basicLogger) Metric(metric string, params ...*Field) {
 	b.Log("metric", "Metric recorded", metricParams...)
 }
 
-func (f *Field) Value() interface{} {
-	switch f.Type {
-	case NodeType:
-		return f.String
-	case ServiceType:
-		return f.String
-	case FunctionType:
-		return f.String
-	case SourceType:
-		return f.String
-	case StringType:
-		return f.String
-	case IntType:
-		return f.Int
-	case UintType:
-		return f.Uint
-	case BytesType:
-		return hex.EncodeToString(f.Bytes)
-	case FloatType:
-		return f.Float
-	case ErrorType:
-		if f.Error != nil {
-			return f.Error.Error()
-		} else {
-			return "<nil>"
+func (b *basicLogger) Log(level string, message string, params ...*Field) {
+	for _, p := range params {
+		for _, f := range b.filters {
+			if p.Equal(f) {
+				return
+			}
 		}
-		return f.Error.Error()
-	case StringArrayType:
-		return f.StringArray
 	}
 
-	return nil
-}
-
-func (b *basicLogger) Log(level string, message string, params ...*Field) {
 	function, source := b.getCaller(b.nestingLevel)
 
 	enrichmentParams := []*Field{
@@ -279,5 +150,10 @@ func (b *basicLogger) Meter(name string, params ...*Field) BasicMeter {
 
 func (b *basicLogger) WithOutput(writers ...Output) BasicLogger {
 	b.outputs = writers
+	return b
+}
+
+func (b *basicLogger) WithFilter(filter *Field) BasicLogger {
+	b.filters = append(b.filters, filter) // this is not thread safe, I know
 	return b
 }
