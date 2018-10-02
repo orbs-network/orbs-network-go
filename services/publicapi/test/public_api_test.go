e7d76a8967c6b972a149ae7b357b7afa6872ad49
Noam Berg <noamberg@yahoo.com>
2018-10-02 15:19:18 +0300

    handleerror uncomment + test

--- services/publicapi/test/public_api_test.go
+++ services/publicapi/test/public_api_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/orbs-network/go-mock"
 	"github.com/orbs-network/orbs-network-go/config"
+	"github.com/orbs-network/orbs-network-go/crypto/digest"
 	"github.com/orbs-network/orbs-network-go/instrumentation/log"
 	"github.com/orbs-network/orbs-network-go/services/publicapi"
 	"github.com/orbs-network/orbs-network-go/test"
@@ -19,30 +20,19 @@ import (
 	"time"
 )
 
-func newPublicApiConfig(txTimeout time.Duration) publicapi.Config {
-	cfg := config.EmptyConfig()
-	cfg.SetDuration(config.PUBLIC_API_SEND_TRANSACTION_TIMEOUT, txTimeout)
-
-	return cfg
-}
-
 func TestSendTransaction_AlreadyCommitted(t *testing.T) {
 	test.WithContext(func(ctx context.Context) {
-		logger := log.GetLogger().WithOutput(log.NewOutput(os.Stdout).WithFormatter(log.NewHumanReadableFormatter()))
-		txpMock := makeTxMock()
-		vmMock := &services.MockVirtualMachine{}
-		cfg := newPublicApiConfig(1 * time.Millisecond)
-		papi := publicapi.NewPublicApi(ctx, cfg, txpMock, vmMock, logger)
+		harness := newPublicApiHarness(ctx, 1*time.Millisecond)
 
 		blockTime := primitives.TimestampNano(time.Now().Nanosecond())
-		txpMock.When("AddNewTransaction", mock.Any).Return(&services.AddNewTransactionOutput{
+		harness.txpMock.When("AddNewTransaction", mock.Any).Return(&services.AddNewTransactionOutput{
 			TransactionStatus:  protocol.TRANSACTION_STATUS_DUPLICATE_TRANSACTION_ALREADY_COMMITTED,
 			TransactionReceipt: builders.TransactionReceipt().Build(),
 			BlockHeight:        1,
 			BlockTimestamp:     blockTime,
 		}).Times(1)
 
-		tx, err := papi.SendTransaction(&services.SendTransactionInput{
+		tx, err := harness.papi.SendTransaction(&services.SendTransactionInput{
 			ClientRequest: (&client.SendTransactionRequestBuilder{
 				SignedTransaction: builders.Transaction().Builder()}).Build(),
 		})
@@ -54,19 +44,15 @@ func TestSendTransaction_AlreadyCommitted(t *testing.T) {
 
 func TestSendTransaction_BlocksUntilTransactionCompletes(t *testing.T) {
 	test.WithContext(func(ctx context.Context) {
-		logger := log.GetLogger().WithOutput(log.NewOutput(os.Stdout).WithFormatter(log.NewHumanReadableFormatter()))
-		txpMock := makeTxMock()
-		vmMock := &services.MockVirtualMachine{}
-		cfg := newPublicApiConfig(1 * time.Second)
-		papi := publicapi.NewPublicApi(ctx, cfg, txpMock, vmMock, logger)
+		harness := newPublicApiHarness(ctx, 1*time.Second)
 
 		blockTime := primitives.TimestampNano(time.Now().Nanosecond())
 		txb := builders.Transaction().Builder()
-		txpMock.When("AddNewTransaction", mock.Any).Times(1).
+		harness.txpMock.When("AddNewTransaction", mock.Any).Times(1).
 			Call(func(input *services.AddNewTransactionInput) (*services.AddNewTransactionOutput, error) {
 				go func() {
 					time.Sleep(1 * time.Millisecond)
-					papi.HandleTransactionResults(&handlers.HandleTransactionResultsInput{
+					harness.papi.HandleTransactionResults(&handlers.HandleTransactionResultsInput{
 						TransactionReceipts: []*protocol.TransactionReceipt{builders.TransactionReceipt().WithTransaction(txb.Build().Transaction()).Build()},
 						BlockHeight:         2,
 						Timestamp:           blockTime,
@@ -75,7 +61,7 @@ func TestSendTransaction_BlocksUntilTransactionCompletes(t *testing.T) {
 				return &services.AddNewTransactionOutput{TransactionStatus: protocol.TRANSACTION_STATUS_PENDING}, nil
 			})
 
-		tx, err := papi.SendTransaction(&services.SendTransactionInput{
+		tx, err := harness.papi.SendTransaction(&services.SendTransactionInput{
 			ClientRequest: (&client.SendTransactionRequestBuilder{
 				SignedTransaction: txb,
 			}).Build(),
@@ -84,6 +70,64 @@ func TestSendTransaction_BlocksUntilTransactionCompletes(t *testing.T) {
 		require.NoError(t, err, "error happened when it should not")
 		require.NotNil(t, tx, "Send transaction returned nil instead of object")
 	})
+}
+
+func TestSendTransaction_BlocksUntilTransactionErrors(t *testing.T) {
+	test.WithContext(func(ctx context.Context) {
+		harness := newPublicApiHarness(ctx, 1*time.Second)
+
+		blockTime := primitives.TimestampNano(time.Now().Nanosecond())
+		txb := builders.Transaction().Builder()
+		txHash := digest.CalcTxHash(txb.Build().Transaction())
+
+		harness.txpMock.When("AddNewTransaction", mock.Any).Times(1).
+			Call(func(input *services.AddNewTransactionInput) (*services.AddNewTransactionOutput, error) {
+				go func() {
+					time.Sleep(1 * time.Millisecond)
+					harness.papi.HandleTransactionError(&handlers.HandleTransactionErrorInput{
+						Txhash:            txHash,
+						TransactionStatus: protocol.TRANSACTION_STATUS_REJECTED_TIMESTAMP_WINDOW_EXCEEDED,
+						BlockHeight:       8,
+						BlockTimestamp:    blockTime,
+					})
+				}()
+				return &services.AddNewTransactionOutput{TransactionStatus: protocol.TRANSACTION_STATUS_PENDING}, nil
+			})
+
+		tx, err := harness.papi.SendTransaction(&services.SendTransactionInput{
+			ClientRequest: (&client.SendTransactionRequestBuilder{
+				SignedTransaction: txb,
+			}).Build(),
+		})
+
+		require.NoError(t, err, "error happened when it should not")
+		require.NotNil(t, tx, "Send transaction returned nil instead of object")
+		require.Equal(t, protocol.TRANSACTION_STATUS_REJECTED_TIMESTAMP_WINDOW_EXCEEDED, tx.ClientResponse.TransactionStatus(), "got wrong status")
+	})
+}
+
+type harness struct {
+	papi    services.PublicApi
+	txpMock *services.MockTransactionPool
+}
+
+func newPublicApiHarness(ctx context.Context, txTimeout time.Duration) *harness {
+	logger := log.GetLogger().WithOutput(log.NewOutput(os.Stdout).WithFormatter(log.NewHumanReadableFormatter()))
+	cfg := newPublicApiConfig(txTimeout)
+	txpMock := makeTxMock()
+	vmMock := &services.MockVirtualMachine{}
+	papi := publicapi.NewPublicApi(ctx, cfg, txpMock, vmMock, logger)
+	return &harness{
+		papi:    papi,
+		txpMock: txpMock,
+	}
+}
+
+func newPublicApiConfig(txTimeout time.Duration) publicapi.Config {
+	cfg := config.EmptyConfig()
+	cfg.SetDuration(config.PUBLIC_API_SEND_TRANSACTION_TIMEOUT, txTimeout)
+
+	return cfg
 }
 
 func makeTxMock() *services.MockTransactionPool {
