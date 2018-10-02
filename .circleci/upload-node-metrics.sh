374e4ad4d7bf2739e6f12338777792d1d6f5b681
Kirill Maksimov <kirill@orbs.com>
2018-10-02 17:45:12 +0300

    Upload e2e logs to s3

--- .circleci/upload-node-metrics.sh
+++ .circleci/upload-node-metrics.sh
@@ -3,4 +3,4 @@
 export GIT_BRANCH=$(source ./docker-tag.sh)
 export GIT_COMMIT=$(git rev-parse HEAD)
 
-find logs -name 'node*.log' -exec aws s3 cp {} s3://orbs-network-logs-ci/logs/$(date +year=%Y/month=%m/day=%d/)/branch=$GIT_BRANCH/commit=$GIT_COMMIT/ \;
+find logs -name 'node*.log' -exec aws s3 cp {} s3://orbs-network-logs-ci/logs/$(date +year=%Y/month=%m/day=%d)/branch=$GIT_BRANCH/commit=$GIT_COMMIT/ \;
