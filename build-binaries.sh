8cb5a1d5a16a9bfbc6163157d2c817d8f2449c31
itamar Arjuan <itamar.arjuan@gmail.com>
2018-10-02 17:36:42 +0300

    fixing a wrong path in build-binaries

--- build-binaries.sh
+++ build-binaries.sh
@@ -5,7 +5,7 @@ time go build -o orbs-node -a main.go
 time go test -a -c ./test/e2e
 
 if [ "$SKIP_DEVTOOLS" == "" ]; then
-    time go build -o gamma-cli -a devtools/jsonapi/main/main.go
+    time go build -o gamma-cli -a devtools/gammacli/main/main.go
 
     time go build -o gamma-server -a devtools/gamma-server/main/main.go
 fi
