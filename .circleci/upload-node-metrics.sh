2b4ed1a6a9999017877741ede388629c66d1e740
Kirill Maksimov <kirill@orbs.com>
2018-10-03 14:45:20 +0300

    Refactor upload-node-metrics.sh

--- .circleci/upload-node-metrics.sh
+++ .circleci/upload-node-metrics.sh
@@ -4,6 +4,6 @@ export GIT_BRANCH=$(. ./docker-tag.sh)
 export GIT_COMMIT=$(git rev-parse HEAD)
 
 # it's way better to store metrics as one file query-wise
-cat logs/acceptance/* | sort > logs/acceptance.node.log
+find logs/acceptance -type f -exec cat {} + >> logs/acceptance.node.log
 
 find logs -name '*node*.log' -exec bzip2 {} \; -exec aws s3 cp {}.bz2 s3://orbs-network-logs-ci/logs/$(date +year=%Y/month=%m/day=%d)/branch=$GIT_BRANCH/commit=$GIT_COMMIT/ \;
