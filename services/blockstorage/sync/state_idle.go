7fdfed76f90da5a24ab534cacc0a31de61a1e60a
Jonathan Levison <jonathanl@orbs.com>
2018-10-07 09:01:06 +0300

    idle state stays idle on block commits

---
+++ services/blockstorage/sync/state_idle.go
@@ -0,0 +1,40 @@
+package sync
+
+import (
+	"github.com/orbs-network/orbs-network-go/synchronization"
+	"github.com/orbs-network/orbs-spec/types/go/primitives"
+	"github.com/orbs-network/orbs-spec/types/go/protocol"
+	"github.com/orbs-network/orbs-spec/types/go/protocol/gossipmessages"
+	"time"
+)
+
+type idleState struct {
+	noCommitTimeout time.Duration
+	noCommitTimer   *synchronization.Timer
+}
+
+func createIdleState(noCommitTimeout time.Duration) *idleState {
+	return &idleState{
+		noCommitTimeout: noCommitTimeout,
+		noCommitTimer:   synchronization.NewTimer(noCommitTimeout),
+	}
+}
+
+func (s *idleState) next() syncState {
+	select {
+	case <-s.noCommitTimer.C:
+		panic("implement me")
+	}
+}
+
+func (s *idleState) blockCommitted(blockHeight primitives.BlockHeight) {
+	s.noCommitTimer.Reset(s.noCommitTimeout)
+}
+
+func (s *idleState) gotAvailabilityResponse(message gossipmessages.BlockAvailabilityResponseMessage) {
+	return
+}
+
+func (s *idleState) gotBlocks(source primitives.Ed25519PublicKey, blocks []*protocol.BlockPairContainer) {
+	return
+}
---
+++ services/blockstorage/sync/state_idle_test.go
@@ -0,0 +1,24 @@
+package sync
+
+import (
+	"github.com/orbs-network/orbs-spec/types/go/primitives"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+func TestIdleStateStaysIdleOnCommit(t *testing.T) {
+	idle := createIdleState(3 * time.Millisecond)
+	var next syncState = nil
+	// in parallel, we will request to advance to the next state and commit blocks,
+	// while blocks are committed we should never advance
+	go func() { next = idle.next() }()
+	go func() {
+		for i := 11; i < 10000; i++ {
+			time.Sleep(1 * time.Millisecond)
+			idle.blockCommitted(primitives.BlockHeight(i))
+		}
+	}()
+	time.Sleep(10 * time.Millisecond)
+	require.Nil(t, next, "next state should not have happened yet")
+}
