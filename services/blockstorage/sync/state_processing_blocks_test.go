514ed64fdfc68c00242d7af66e9d6384c1860cef
Jonathan Levison <jonathanl@orbs.com>
2018-10-09 10:25:47 +0300

    commit failure test for processing

--- services/blockstorage/sync/state_processing_blocks_test.go
+++ services/blockstorage/sync/state_processing_blocks_test.go
@@ -78,3 +78,28 @@ func TestProcessingValidationFailure(t *testing.T) {
 
 	h.verifyMocks(t)
 }
+
+func TestProcessingCommitFailure(t *testing.T) {
+	h := newBlockSyncHarness()
+
+	message := builders.BlockSyncResponseInput().
+		WithFirstBlockHeight(10).
+		WithLastBlockHeight(20).
+		WithLastCommittedBlockHeight(20).
+		Build().Message
+
+	h.storage.When("ValidateBlockForCommit", mock.Any).Return(nil, nil).Times(6)
+	h.storage.When("CommitBlock", mock.Any).Call(func(input *services.CommitBlockInput) error {
+		if input.BlockPair.ResultsBlock.Header.BlockHeight().Equal(message.SignedChunkRange.FirstBlockHeight() + 5) {
+			return errors.New("failed to validate block #6")
+		}
+		return nil
+	}).Times(6)
+
+	processingState := h.sf.CreateProcessingBlocksState(message)
+	next := processingState.processState(h.ctx)
+
+	require.IsType(t, &collectingAvailabilityResponsesState{}, next, "next state after commit error should be collecting availability responses")
+
+	h.verifyMocks(t)
+}
