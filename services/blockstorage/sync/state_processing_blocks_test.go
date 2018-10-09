4d555224b687215e1aec1c2e7b3852766cf63b4a
Jonathan Levison <jonathanl@orbs.com>
2018-10-09 10:23:39 +0300

    validation error test for processing

--- services/blockstorage/sync/state_processing_blocks_test.go
+++ services/blockstorage/sync/state_processing_blocks_test.go
@@ -1,8 +1,10 @@
 package sync
 
 import (
+	"errors"
 	"github.com/orbs-network/go-mock"
 	"github.com/orbs-network/orbs-network-go/test/builders"
+	"github.com/orbs-network/orbs-spec/types/go/services"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -50,4 +52,29 @@ func TestProcessingWithNoBlocksReturnsToIdle(t *testing.T) {
 	next := processingState.processState(h.ctx)
 
 	require.IsType(t, &idleState{}, next, "commit initialized invalid should move to idle")
+}
+
+func TestProcessingValidationFailure(t *testing.T) {
+	h := newBlockSyncHarness()
+
+	message := builders.BlockSyncResponseInput().
+		WithFirstBlockHeight(10).
+		WithLastBlockHeight(20).
+		WithLastCommittedBlockHeight(20).
+		Build().Message
+
+	h.storage.When("ValidateBlockForCommit", mock.Any).Call(func(input *services.ValidateBlockForCommitInput) error {
+		if input.BlockPair.ResultsBlock.Header.BlockHeight().Equal(message.SignedChunkRange.FirstBlockHeight() + 5) {
+			return errors.New("failed to validate block #6")
+		}
+		return nil
+	}).Times(6)
+	h.storage.When("CommitBlock", mock.Any).Return(nil, nil).Times(5)
+
+	processingState := h.sf.CreateProcessingBlocksState(message)
+	next := processingState.processState(h.ctx)
+
+	require.IsType(t, &collectingAvailabilityResponsesState{}, next, "next state after validation error should be collecting availability responses")
+
+	h.verifyMocks(t)
 }
