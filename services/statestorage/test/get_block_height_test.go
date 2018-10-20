f6810e7924d2097971d66d9250145b15af63a4c8
Tal Kol <talkol@gmail.com>
2018-10-20 10:47:10 +0300

    Service statestorage - compiles and tests pass

--- services/statestorage/test/get_block_height_test.go
+++ services/statestorage/test/get_block_height_test.go
@@ -1,40 +1,48 @@
 package test
 
 import (
+	"context"
+	"github.com/orbs-network/orbs-network-go/test"
 	"github.com/orbs-network/orbs-network-go/test/builders"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestInitToZero(t *testing.T) {
-	d := newStateStorageDriver(1)
-	height, timestamp, err := d.getBlockHeightAndTimestamp()
+	test.WithContext(func(ctx context.Context) {
+		d := newStateStorageDriver(1)
+		height, timestamp, err := d.getBlockHeightAndTimestamp(ctx)
 
-	require.NoError(t, err, "unexpected error")
-	require.EqualValues(t, 0, height, "unexpected height")
-	require.EqualValues(t, 0, timestamp, "unexpected timestamp")
+		require.NoError(t, err, "unexpected error")
+		require.EqualValues(t, 0, height, "unexpected height")
+		require.EqualValues(t, 0, timestamp, "unexpected timestamp")
+	})
 }
 
 func TestReflectsSuccessfulCommit(t *testing.T) {
-	d := newStateStorageDriver(1)
-	heightBefore, _, err := d.getBlockHeightAndTimestamp()
-	d.service.CommitStateDiff(CommitStateDiff().WithBlockHeight(1).WithBlockTimestamp(6579).WithDiff(builders.ContractStateDiff().Build()).Build())
-	heightAfter, timestampAfter, err := d.getBlockHeightAndTimestamp()
+	test.WithContext(func(ctx context.Context) {
+		d := newStateStorageDriver(1)
+		heightBefore, _, err := d.getBlockHeightAndTimestamp(ctx)
+		d.service.CommitStateDiff(ctx, CommitStateDiff().WithBlockHeight(1).WithBlockTimestamp(6579).WithDiff(builders.ContractStateDiff().Build()).Build())
+		heightAfter, timestampAfter, err := d.getBlockHeightAndTimestamp(ctx)
 
-	require.NoError(t, err, "unexpected error")
-	require.EqualValues(t, heightBefore+1, heightAfter, "unexpected height")
-	require.EqualValues(t, 6579, timestampAfter, "unexpected timestamp")
+		require.NoError(t, err, "unexpected error")
+		require.EqualValues(t, heightBefore+1, heightAfter, "unexpected height")
+		require.EqualValues(t, 6579, timestampAfter, "unexpected timestamp")
+	})
 }
 
 func TestIgnoreFailedCommit(t *testing.T) {
-	d := newStateStorageDriver(1)
-	stateDiff := builders.ContractStateDiff().Build()
-	d.service.CommitStateDiff(CommitStateDiff().WithBlockHeight(1).WithDiff(stateDiff).Build())
-	d.service.CommitStateDiff(CommitStateDiff().WithBlockHeight(2).WithDiff(stateDiff).Build())
-	heightBefore, _, err := d.getBlockHeightAndTimestamp()
-	d.service.CommitStateDiff(CommitStateDiff().WithBlockHeight(1).WithDiff(stateDiff).Build())
-	heightAfter, _, err := d.getBlockHeightAndTimestamp()
+	test.WithContext(func(ctx context.Context) {
+		d := newStateStorageDriver(1)
+		stateDiff := builders.ContractStateDiff().Build()
+		d.service.CommitStateDiff(ctx, CommitStateDiff().WithBlockHeight(1).WithDiff(stateDiff).Build())
+		d.service.CommitStateDiff(ctx, CommitStateDiff().WithBlockHeight(2).WithDiff(stateDiff).Build())
+		heightBefore, _, err := d.getBlockHeightAndTimestamp(ctx)
+		d.service.CommitStateDiff(ctx, CommitStateDiff().WithBlockHeight(1).WithDiff(stateDiff).Build())
+		heightAfter, _, err := d.getBlockHeightAndTimestamp(ctx)
 
-	require.NoError(t, err, "unexpected error")
-	require.EqualValues(t, heightBefore, heightAfter, "unexpected height")
+		require.NoError(t, err, "unexpected error")
+		require.EqualValues(t, heightBefore, heightAfter, "unexpected height")
+	})
 }
