bcfc93860d3b2b71aba8e228f73596a365997e8b
Jonathan Levison <jonathanl@orbs.com>
2018-10-31 16:55:36 +0200

    added supervied into the main loop

--- synchronization/trigger.go
+++ synchronization/trigger.go
@@ -2,6 +2,7 @@ package synchronization
 
 import (
 	"context"
+	"github.com/orbs-network/orbs-network-go/synchronization/supervised"
 	"sync"
 	"sync/atomic"
 	"time"
@@ -9,7 +10,6 @@ import (
 
 type Trigger interface {
 	TimesTriggered() uint64
-	IsRunning() bool
 	Stop()
 }
 
@@ -17,33 +17,32 @@ type Telemetry struct {
 	timesTriggered uint64
 }
 
+// the trigger is coupled with supervized package, this feels okay for now
 type periodicalTrigger struct {
 	d       time.Duration
 	f       func()
 	s       func()
+	logger  supervised.Errorer
+	cancel  context.CancelFunc
 	ticker  *time.Ticker
 	metrics *Telemetry
-	running bool
-	stop    chan struct{}
 	wgSync  sync.WaitGroup
 }
 
-func NewPeriodicalTrigger(ctx context.Context, interval time.Duration, trigger func(), onStop func()) Trigger {
+func NewPeriodicalTrigger(ctx context.Context, interval time.Duration, logger supervised.Errorer, trigger func(), onStop func()) Trigger {
+	subCtx, cancel := context.WithCancel(ctx)
 	t := &periodicalTrigger{
 		ticker:  nil,
 		d:       interval,
 		f:       trigger,
 		s:       onStop,
+		cancel:  cancel,
+		logger:  logger,
 		metrics: &Telemetry{},
-		stop:    make(chan struct{}),
-		running: false,
 	}
-	t.run(ctx)
-	return t
-}
 
-func (t *periodicalTrigger) IsRunning() bool {
-	return t.running
+	t.run(subCtx)
+	return t
 }
 
 func (t *periodicalTrigger) TimesTriggered() uint64 {
@@ -51,45 +50,30 @@ func (t *periodicalTrigger) TimesTriggered() uint64 {
 }
 
 func (t *periodicalTrigger) run(ctx context.Context) {
-	if t.running {
-		return
-	}
-	t.running = true
 	t.ticker = time.NewTicker(t.d)
-	t.wgSync.Add(1)
 	go func() {
-		for {
-			select {
-			case <-t.ticker.C:
-				t.f()
-				atomic.AddUint64(&t.metrics.timesTriggered, 1)
-			case <-t.stop:
-				t.ticker.Stop()
-				t.running = false
-				t.wgSync.Done()
-				if t.s != nil {
-					go t.s()
+		supervised.LongLived(ctx, t.logger, func() {
+			t.wgSync.Add(1)
+			defer t.wgSync.Done()
+			for {
+				select {
+				case <-t.ticker.C:
+					t.f()
+					atomic.AddUint64(&t.metrics.timesTriggered, 1)
+				case <-ctx.Done():
+					t.ticker.Stop()
+					if t.s != nil {
+						go t.s()
+					}
+					return
 				}
-				return
-			case <-ctx.Done():
-				t.ticker.Stop()
-				t.running = false
-				t.wgSync.Done()
-				if t.s != nil {
-					go t.s()
-				}
-				return
 			}
-		}
+		})
 	}()
 }
 
 func (t *periodicalTrigger) Stop() {
-	if !t.running {
-		return
-	}
-
-	t.stop <- struct{}{}
-	// we set running to false only once the gofunc terminates and we will block (possibly, for a few nanosecs) until that channel is processed
+	t.cancel()
+	// we want ticker stop to process before we return
 	t.wgSync.Wait()
 }
--- synchronization/trigger_test.go
+++ synchronization/trigger_test.go
@@ -2,12 +2,31 @@ package synchronization_test
 
 import (
 	"context"
+	"github.com/orbs-network/orbs-network-go/instrumentation/log"
 	"github.com/orbs-network/orbs-network-go/synchronization"
 	"github.com/stretchr/testify/require"
 	"sync/atomic"
 	"testing"
 	"time"
 )
+
+type report struct {
+	message string
+	fields  []*log.Field
+}
+
+type collector struct {
+	errors chan report
+}
+
+func (c *collector) Error(message string, fields ...*log.Field) {
+	c.errors <- report{message, fields}
+}
+
+func mockLogger() *collector {
+	c := &collector{errors: make(chan report)}
+	return c
+}
 
 func getExpected(startTime, endTime time.Time, tickTime time.Duration) uint32 {
 	duration := endTime.Sub(startTime)
@@ -16,10 +35,11 @@ func getExpected(startTime, endTime time.Time, tickTime time.Duration) uint32 {
 }
 
 func TestPeriodicalTriggerStartsOk(t *testing.T) {
+	logger := mockLogger()
 	var x uint32
 	start := time.Now()
 	tickTime := 5 * time.Millisecond
-	p := synchronization.NewPeriodicalTrigger(context.Background(), tickTime, func() { atomic.AddUint32(&x, 1) }, nil)
+	p := synchronization.NewPeriodicalTrigger(context.Background(), tickTime, logger, func() { atomic.AddUint32(&x, 1) }, nil)
 	time.Sleep(time.Millisecond * 30)
 	expected := getExpected(start, time.Now(), tickTime)
 	require.True(t, expected/2 < atomic.LoadUint32(&x), "expected more than %d ticks, but got %d", expected/2, atomic.LoadUint32(&x))
@@ -27,10 +47,11 @@ func TestPeriodicalTriggerStartsOk(t *testing.T) {
 }
 
 func TestTriggerInternalMetrics(t *testing.T) {
+	logger := mockLogger()
 	var x uint32
 	start := time.Now()
 	tickTime := 5 * time.Millisecond
-	p := synchronization.NewPeriodicalTrigger(context.Background(), tickTime, func() { atomic.AddUint32(&x, 1) }, nil)
+	p := synchronization.NewPeriodicalTrigger(context.Background(), tickTime, logger, func() { atomic.AddUint32(&x, 1) }, nil)
 	time.Sleep(time.Millisecond * 30)
 	expected := getExpected(start, time.Now(), tickTime)
 	require.True(t, expected/2 < atomic.LoadUint32(&x), "expected more than %d ticks, but got %d", expected/2, atomic.LoadUint32(&x))
@@ -39,16 +60,18 @@ func TestTriggerInternalMetrics(t *testing.T) {
 }
 
 func TestPeriodicalTrigger_Stop(t *testing.T) {
+	logger := mockLogger()
 	x := 0
-	p := synchronization.NewPeriodicalTrigger(context.Background(), time.Millisecond*2, func() { x++ }, nil)
+	p := synchronization.NewPeriodicalTrigger(context.Background(), time.Millisecond*2, logger, func() { x++ }, nil)
 	p.Stop()
 	time.Sleep(3 * time.Millisecond)
 	require.Equal(t, 0, x, "expected no ticks")
 }
 
 func TestPeriodicalTrigger_StopAfterTrigger(t *testing.T) {
+	logger := mockLogger()
 	x := 0
-	p := synchronization.NewPeriodicalTrigger(context.Background(), time.Millisecond, func() { x++ }, nil)
+	p := synchronization.NewPeriodicalTrigger(context.Background(), time.Millisecond, logger, func() { x++ }, nil)
 	time.Sleep(time.Microsecond * 1100)
 	p.Stop()
 	xValueOnStop := x
@@ -57,18 +80,20 @@ func TestPeriodicalTrigger_StopAfterTrigger(t *testing.T) {
 }
 
 func TestPeriodicalTriggerStopOnContextCancel(t *testing.T) {
+	logger := mockLogger()
 	ctx, cancel := context.WithCancel(context.Background())
 	x := 0
-	synchronization.NewPeriodicalTrigger(ctx, time.Millisecond*2, func() { x++ }, nil)
+	synchronization.NewPeriodicalTrigger(ctx, time.Millisecond*2, logger, func() { x++ }, nil)
 	cancel()
 	time.Sleep(3 * time.Millisecond)
 	require.Equal(t, 0, x, "expected no ticks")
 }
 
 func TestPeriodicalTriggerStopWorksWhenContextIsCancelled(t *testing.T) {
+	logger := mockLogger()
 	ctx, cancel := context.WithCancel(context.Background())
 	x := 0
-	p := synchronization.NewPeriodicalTrigger(ctx, time.Millisecond*2, func() { x++ }, nil)
+	p := synchronization.NewPeriodicalTrigger(ctx, time.Millisecond*2, logger, func() { x++ }, nil)
 	cancel()
 	time.Sleep(3 * time.Millisecond)
 	require.Equal(t, 0, x, "expected no ticks")
@@ -77,17 +102,45 @@ func TestPeriodicalTriggerStopWorksWhenContextIsCancelled(t *testing.T) {
 }
 
 func TestPeriodicalTriggerStopOnContextCancelWithStopAction(t *testing.T) {
+	logger := mockLogger()
 	ctx, cancel := context.WithCancel(context.Background())
 	x := 0
-	synchronization.NewPeriodicalTrigger(ctx, time.Millisecond*2, func() { x++ }, func() { x = 20 })
+	synchronization.NewPeriodicalTrigger(ctx, time.Millisecond*2, logger, func() { x++ }, func() { x = 20 })
 	cancel()
 	time.Sleep(time.Millisecond) // yield
 	require.Equal(t, 20, x, "expected x to have the stop value")
 }
 
 func TestPeriodicalTriggerRunsOnStopAction(t *testing.T) {
+	logger := mockLogger()
+	latch := make(chan struct{})
 	x := 0
-	p := synchronization.NewPeriodicalTrigger(context.Background(), time.Second, func() { x++ }, func() { x = 20 })
+	p := synchronization.NewPeriodicalTrigger(context.Background(),
+		time.Second,
+		logger,
+		func() { x++ },
+		func() {
+			x = 20
+			latch <- struct{}{}
+		})
 	p.Stop()
+	<-latch // wait for stop to happen...
 	require.Equal(t, 20, x, "expected x to have the stop value")
+}
+
+func TestPeriodicalTriggerKeepsGoingOnPanic(t *testing.T) {
+	t.Skip("LongLived is broken, try again when its fixed")
+	logger := mockLogger()
+	x := 0
+	p := synchronization.NewPeriodicalTrigger(context.Background(),
+		time.Millisecond,
+		logger,
+		func() {
+			x++
+			panic("we should not see this other than the logs")
+		},
+		nil)
+	time.Sleep(5 * time.Millisecond)
+	p.Stop()
+	require.True(t, x > 1, "expected trigger to have ticked more than once (even though it panics) %d", x)
 }
