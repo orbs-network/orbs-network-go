ad85b5e02941a08316511b218c777cbcb4d9d383
Tal Kol <talkol@gmail.com>
2018-11-04 14:10:53 +0200

    Removed defaultConfig and turned it into productionConfig to avoid another default

--- config/system_factory_presets.go
+++ config/system_factory_presets.go
@@ -7,19 +7,25 @@ import (
 	"time"
 )
 
-// the default config is identical to ForProduction
 // all other configs are variations from the production one
-func defaultConfig() mutableNodeConfig {
+func defaultProductionConfig() mutableNodeConfig {
 	cfg := emptyConfig()
 
-	cfg.SetActiveConsensusAlgo(consensus.CONSENSUS_ALGO_TYPE_BENCHMARK_CONSENSUS)
-	cfg.SetUint32(CONSENSUS_CONTEXT_PERCENTAGE_OF_NODES_REQUIRED_FOR_CONSENSUS, 100)
 	cfg.SetUint32(VIRTUAL_CHAIN_ID, 42)
+	cfg.SetUint32(GOSSIP_LISTEN_PORT, 4400)
+	cfg.SetActiveConsensusAlgo(consensus.CONSENSUS_ALGO_TYPE_BENCHMARK_CONSENSUS)
+	cfg.SetDuration(BENCHMARK_CONSENSUS_RETRY_INTERVAL, 2*time.Second)
+	cfg.SetDuration(CONSENSUS_CONTEXT_MINIMAL_BLOCK_DELAY, 1*time.Second) // this is the time between empty blocks when no transactions, need to be large so we don't close infinite blocks on idle
+	cfg.SetUint32(CONSENSUS_CONTEXT_PERCENTAGE_OF_NODES_REQUIRED_FOR_CONSENSUS, 66)
+	cfg.SetUint32(CONSENSUS_CONTEXT_MINIMUM_TRANSACTION_IN_BLOCK, 10)
+	cfg.SetUint32(CONSENSUS_CONTEXT_MAXIMUM_TRANSACTION_IN_BLOCK, 100)
 	cfg.SetUint32(BLOCK_TRACKER_GRACE_DISTANCE, 3)
+	cfg.SetDuration(BLOCK_TRACKER_GRACE_TIMEOUT, 100*time.Millisecond)
 	cfg.SetUint32(BLOCK_SYNC_BATCH_SIZE, 10000)
 	cfg.SetDuration(BLOCK_SYNC_INTERVAL, 8*time.Second)
 	cfg.SetDuration(BLOCK_SYNC_COLLECT_RESPONSE_TIMEOUT, 3*time.Second)
 	cfg.SetDuration(BLOCK_SYNC_COLLECT_CHUNKS_TIMEOUT, 5*time.Second)
+	cfg.SetDuration(PUBLIC_API_SEND_TRANSACTION_TIMEOUT, 30*time.Second)
 	cfg.SetDuration(BLOCK_TRANSACTION_RECEIPT_QUERY_GRACE_START, 5*time.Second)
 	cfg.SetDuration(BLOCK_TRANSACTION_RECEIPT_QUERY_GRACE_END, 5*time.Second)
 	cfg.SetDuration(BLOCK_TRANSACTION_RECEIPT_QUERY_EXPIRATION_WINDOW, 3*time.Minute)
@@ -29,33 +35,22 @@ func defaultConfig() mutableNodeConfig {
 	cfg.SetDuration(TRANSACTION_POOL_FUTURE_TIMESTAMP_GRACE_TIMEOUT, 5*time.Second)
 	cfg.SetDuration(TRANSACTION_POOL_PENDING_POOL_CLEAR_EXPIRED_INTERVAL, 10*time.Second)
 	cfg.SetDuration(TRANSACTION_POOL_COMMITTED_POOL_CLEAR_EXPIRED_INTERVAL, 30*time.Second)
-	cfg.SetUint32(TRANSACTION_POOL_PROPAGATION_BATCH_SIZE, 5)
-	cfg.SetDuration(TRANSACTION_POOL_PROPAGATION_BATCHING_TIMEOUT, 10*time.Millisecond)
-	cfg.SetUint32(GOSSIP_LISTEN_PORT, 4400)
-	cfg.SetString(PROCESSOR_ARTIFACT_PATH, filepath.Join(GetProjectSourceTmpPath(), "processor-artifacts"))
+	cfg.SetUint32(TRANSACTION_POOL_PROPAGATION_BATCH_SIZE, 100)
+	cfg.SetDuration(TRANSACTION_POOL_PROPAGATION_BATCHING_TIMEOUT, 100*time.Millisecond)
+	cfg.SetDuration(GOSSIP_CONNECTION_KEEP_ALIVE_INTERVAL, 1*time.Second)
+	cfg.SetDuration(GOSSIP_NETWORK_TIMEOUT, 30*time.Second)
 	cfg.SetDuration(METRICS_REPORT_INTERVAL, 30*time.Second)
+	cfg.SetString(PROCESSOR_ARTIFACT_PATH, filepath.Join(GetProjectSourceTmpPath(), "processor-artifacts"))
 	return cfg
 }
 
 // config for a production node (either main net or test net)
 func ForProduction(processorArtifactPath string) mutableNodeConfig {
-	cfg := defaultConfig()
+	cfg := defaultProductionConfig()
 
-	cfg.SetDuration(BENCHMARK_CONSENSUS_RETRY_INTERVAL, 2*time.Second)
-	cfg.SetDuration(CONSENSUS_CONTEXT_MINIMAL_BLOCK_DELAY, 1*time.Second) // this is the time between empty blocks when no transactions, need to be large so we don't close infinite blocks on idle
-	cfg.SetUint32(CONSENSUS_CONTEXT_PERCENTAGE_OF_NODES_REQUIRED_FOR_CONSENSUS, 66)
-	cfg.SetDuration(BLOCK_TRACKER_GRACE_TIMEOUT, 100*time.Millisecond)
-	cfg.SetDuration(GOSSIP_CONNECTION_KEEP_ALIVE_INTERVAL, 1*time.Second)
-	cfg.SetDuration(GOSSIP_NETWORK_TIMEOUT, 30*time.Second)
-	cfg.SetDuration(PUBLIC_API_SEND_TRANSACTION_TIMEOUT, 30*time.Second)
-	cfg.SetUint32(CONSENSUS_CONTEXT_MINIMUM_TRANSACTION_IN_BLOCK, 10)
-	cfg.SetUint32(CONSENSUS_CONTEXT_MAXIMUM_TRANSACTION_IN_BLOCK, 100)
-	cfg.SetUint32(TRANSACTION_POOL_PROPAGATION_BATCH_SIZE, 100)
-	cfg.SetDuration(TRANSACTION_POOL_PROPAGATION_BATCHING_TIMEOUT, 100*time.Millisecond)
 	if processorArtifactPath != "" {
 		cfg.SetString(PROCESSOR_ARTIFACT_PATH, processorArtifactPath)
 	}
-
 	return cfg
 }
 
@@ -69,11 +64,12 @@ func ForAcceptanceTests(
 	activeConsensusAlgo consensus.ConsensusAlgoType,
 	maxTxPerBlock uint32,
 ) mutableNodeConfig {
-	cfg := defaultConfig()
+	cfg := defaultProductionConfig()
 	cfg.OverrideNodeSpecificValues(federationNodes, gossipPeers, 0, nodePublicKey, nodePrivateKey, constantConsensusLeader, activeConsensusAlgo)
 
 	cfg.SetDuration(BENCHMARK_CONSENSUS_RETRY_INTERVAL, 1*time.Millisecond)
 	cfg.SetDuration(CONSENSUS_CONTEXT_MINIMAL_BLOCK_DELAY, 10*time.Millisecond)
+	cfg.SetUint32(CONSENSUS_CONTEXT_PERCENTAGE_OF_NODES_REQUIRED_FOR_CONSENSUS, 100)
 	cfg.SetDuration(BLOCK_TRACKER_GRACE_TIMEOUT, 50*time.Millisecond)
 	cfg.SetDuration(PUBLIC_API_SEND_TRANSACTION_TIMEOUT, 300*time.Millisecond)
 	cfg.SetUint32(CONSENSUS_CONTEXT_MINIMUM_TRANSACTION_IN_BLOCK, 1)
@@ -96,15 +92,18 @@ func ForGamma(
 	constantConsensusLeader primitives.Ed25519PublicKey,
 	activeConsensusAlgo consensus.ConsensusAlgoType,
 ) mutableNodeConfig {
-	cfg := defaultConfig()
+	cfg := defaultProductionConfig()
 	cfg.OverrideNodeSpecificValues(federationNodes, gossipPeers, 0, nodePublicKey, nodePrivateKey, constantConsensusLeader, activeConsensusAlgo)
 
 	cfg.SetDuration(BENCHMARK_CONSENSUS_RETRY_INTERVAL, 1000*time.Millisecond)
 	cfg.SetDuration(CONSENSUS_CONTEXT_MINIMAL_BLOCK_DELAY, 500*time.Millisecond) // this is the time between empty blocks when no transactions, need to be large so we don't close infinite blocks on idle
+	cfg.SetUint32(CONSENSUS_CONTEXT_PERCENTAGE_OF_NODES_REQUIRED_FOR_CONSENSUS, 100)
 	cfg.SetDuration(BLOCK_TRACKER_GRACE_TIMEOUT, 100*time.Millisecond)
 	cfg.SetDuration(PUBLIC_API_SEND_TRANSACTION_TIMEOUT, 10*time.Second)
 	cfg.SetUint32(CONSENSUS_CONTEXT_MINIMUM_TRANSACTION_IN_BLOCK, 1)
 	cfg.SetUint32(CONSENSUS_CONTEXT_MAXIMUM_TRANSACTION_IN_BLOCK, 100)
+	cfg.SetUint32(TRANSACTION_POOL_PROPAGATION_BATCH_SIZE, 5)
+	cfg.SetDuration(TRANSACTION_POOL_PROPAGATION_BATCHING_TIMEOUT, 10*time.Millisecond)
 	cfg.SetUint32(BLOCK_SYNC_BATCH_SIZE, 5)
 	cfg.SetDuration(BLOCK_SYNC_INTERVAL, 2500*time.Millisecond)
 	cfg.SetDuration(BLOCK_SYNC_COLLECT_RESPONSE_TIMEOUT, 15*time.Millisecond)
