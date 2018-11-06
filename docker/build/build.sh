8adf8d26443c538f1b30df6105f6d0468ac2cbe8
Kirill Maksimov <kirill@orbs.com>
2018-11-06 13:22:57 +0200

    Introduce _bin directory for binaries

--- docker/build/build.sh
+++ docker/build/build.sh
@@ -8,11 +8,11 @@ docker run --name orbs_build orbs:build sleep 1
 
 export SRC=/go/src/github.com/orbs-network/orbs-network-go
 
-docker cp orbs_build:$SRC/orbs-node .
-docker cp orbs_build:$SRC/gamma-cli .
-docker cp orbs_build:$SRC/gamma-server .
+docker cp orbs_build:$SRC/_bin/orbs-node _bin
+docker cp orbs_build:$SRC/_bin/gamma-cli _bin
+docker cp orbs_build:$SRC/_bin/gamma-server _bin
 
-docker cp orbs_build:$SRC/e2e.test .
+docker cp orbs_build:$SRC/_bin/e2e.test _bin
 
 docker build -f ./docker/build/Dockerfile.export -t orbs:export .
 docker build -f ./docker/build/Dockerfile.gamma -t orbs:gamma-server .
