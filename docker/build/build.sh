32f8fe12893c88d64f615f90e160e2d2e4b2291d
Kirill Maksimov <kirill@orbs.com>
2018-11-06 13:33:31 +0200

    Fix docker build script

--- docker/build/build.sh
+++ docker/build/build.sh
@@ -8,11 +8,8 @@ docker run --name orbs_build orbs:build sleep 1
 
 export SRC=/go/src/github.com/orbs-network/orbs-network-go
 
-docker cp orbs_build:$SRC/_bin/orbs-node _bin
-docker cp orbs_build:$SRC/_bin/gamma-cli _bin
-docker cp orbs_build:$SRC/_bin/gamma-server _bin
-
-docker cp orbs_build:$SRC/_bin/e2e.test _bin
+rm -rf _bin
+docker cp orbs_build:$SRC/_bin .
 
 docker build -f ./docker/build/Dockerfile.export -t orbs:export .
 docker build -f ./docker/build/Dockerfile.gamma -t orbs:gamma-server .
