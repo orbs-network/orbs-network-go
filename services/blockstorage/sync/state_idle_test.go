c3de32077553e73ba4a2617657b71998aac159e4
danokoch <danokoch@gmail.com>
2018-11-11 13:33:34 +0200

    Renaming variables in IdleState tests

--- services/blockstorage/sync/state_idle_test.go
+++ services/blockstorage/sync/state_idle_test.go
@@ -15,50 +15,55 @@ func TestStateIdle_StaysIdleOnCommit(t *testing.T) {
 			return manualNoCommitTimer
 		})
 
-		idle := h.factory.CreateIdleState()
-		nextState := h.processStateInBackgroundAndWaitUntilFinished(ctx, idle, func() {
-			idle.blockCommitted(ctx)
+		state := h.factory.CreateIdleState()
+		nextState := h.processStateInBackgroundAndWaitUntilFinished(ctx, state, func() {
+			state.blockCommitted(ctx)
 			manualNoCommitTimer.ManualTick() // not required, added for completion (like in state_availability_requests_test)
 		})
 
 		require.IsType(t, &idleState{}, nextState, "nextState should still be idle")
-		require.True(t, nextState != idle, "processState state should be a different idle state (which was recreated so the timer starts from be beginning)")
+		require.True(t, nextState != state, "processState state should be a different idle state (which was recreated so the timer starts from be beginning)")
 	})
 }
 
 func TestStateIdle_MovesToCollectingAvailabilityResponsesOnNoCommitTimeout(t *testing.T) {
 	test.WithContext(func(ctx context.Context) {
 		h := newBlockSyncHarness()
-		idle := h.factory.CreateIdleState()
-		next := idle.processState(ctx)
-		require.IsType(t, &collectingAvailabilityResponsesState{}, next, "processState state should be collecting availability responses")
+
+		state := h.factory.CreateIdleState()
+		nextState := state.processState(ctx)
+
+		require.IsType(t, &collectingAvailabilityResponsesState{}, nextState, "processState state should be collecting availability responses")
 	})
 }
 
 func TestStateIdle_TerminatesOnContextTermination(t *testing.T) {
 	ctx, cancel := context.WithCancel(context.Background())
 	h := newBlockSyncHarness()
-	cancel()
-	idle := h.factory.CreateIdleState()
-	next := idle.processState(ctx)
 
-	require.Nil(t, next, "context termination should return a nil new state")
+	cancel()
+	state := h.factory.CreateIdleState()
+	nextState := state.processState(ctx)
+
+	require.Nil(t, nextState, "context termination should return a nil new state")
 }
 
 func TestStateIdle_DoesNotBlockOnNewBlockNotificationWhenChannelIsNotReady(t *testing.T) {
 	h := newBlockSyncHarness()
+
 	test.WithContextWithTimeout(h.config.noCommit/2, func(ctx context.Context) {
-		idle := h.factory.CreateIdleState()
-		idle.blockCommitted(ctx) // we did not call process, so channel is not ready, test only fails on timeout, if this blocks
+		state := h.factory.CreateIdleState()
+		state.blockCommitted(ctx) // we did not call process, so channel is not ready, test only fails on timeout, if this blocks
 	})
 }
 
 func TestStateIdle_NOP(t *testing.T) {
 	test.WithContext(func(ctx context.Context) {
 		h := newBlockSyncHarness()
-		idle := h.factory.CreateIdleState()
+
+		state := h.factory.CreateIdleState()
 		// these calls should do nothing, this is just a sanity that they do not panic and return nothing
-		idle.gotAvailabilityResponse(ctx, nil)
-		idle.gotBlocks(ctx, nil)
+		state.gotAvailabilityResponse(ctx, nil)
+		state.gotBlocks(ctx, nil)
 	})
 }
