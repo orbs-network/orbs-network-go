adc15a48ec87d1e43b0814802d53686e107e083c
Ido Zilberberg <idoz@orbs.com>
2018-11-11 11:37:37 +0200

    Added random committee chooser algo for RequestValidationCommittee

--- services/consensuscontext/committee.go
+++ services/consensuscontext/committee.go
@@ -2,35 +2,37 @@ package consensuscontext
 
 import (
 	"context"
+	"encoding/binary"
 	"fmt"
 	"github.com/orbs-network/orbs-network-go/config"
+	"github.com/orbs-network/orbs-network-go/crypto/hash"
 	"github.com/orbs-network/orbs-spec/types/go/primitives"
 	"github.com/orbs-network/orbs-spec/types/go/services"
 	"sort"
 )
 
 func (s *service) RequestOrderingCommittee(ctx context.Context, input *services.RequestCommitteeInput) (*services.RequestCommitteeOutput, error) {
-	return s.RequestOrderingCommittee(ctx, input)
+	return s.RequestValidationCommittee(ctx, input)
 }
 
-func toAscendingPublicKeys(nodes map[string]config.FederationNode) []string {
-	keys := make([]string, len(nodes))
+func toPublicKeys(nodes map[string]config.FederationNode) []primitives.Ed25519PublicKey {
+	keys := make([]primitives.Ed25519PublicKey, len(nodes))
 	i := 0
-	for key := range nodes {
-		keys[i] = key
+	for _, value := range nodes {
+		keys[i] = value.NodePublicKey()
 		i++
 	}
-	sort.Strings(keys)
-
 	return keys
 }
 
-// TODO Pending a different impl if necessary
 func (s *service) RequestValidationCommittee(ctx context.Context, input *services.RequestCommitteeInput) (*services.RequestCommitteeOutput, error) {
 	federationNodes := s.config.FederationNodes(uint64(input.BlockHeight))
-	federationNodesPublicKeys := toAscendingPublicKeys(federationNodes)
+	federationNodesPublicKeys := toPublicKeys(federationNodes)
 	committeeSize := calculateCommitteeSize(input.MaxCommitteeSize, s.config.ConsensusMinimumCommitteeSize(), uint32(len(federationNodesPublicKeys)))
-	indices := chooseRandomCommitteeIndices(committeeSize, input.RandomSeed)
+	indices, err := chooseRandomCommitteeIndices(committeeSize, input.RandomSeed, federationNodesPublicKeys)
+	if err != nil {
+		return nil, err
+	}
 
 	committeePublicKeys := make([]primitives.Ed25519PublicKey, len(indices))
 	for i, index := range indices {
@@ -61,11 +63,44 @@ func calculateCommitteeSize(requestedCommitteeSize uint32, minimumCommitteeSize 
 	return requestedCommitteeSize
 }
 
-// Smart algo!
-func chooseRandomCommitteeIndices(committeeSize uint32, randomSeed uint64) []uint32 {
+// See https://github.com/orbs-network/orbs-spec/issues/111
+func chooseRandomCommitteeIndices(committeeSize uint32, randomSeed uint64, nodes []primitives.Ed25519PublicKey) ([]uint32, error) {
+
+	type gradedIndex struct {
+		grade uint64
+		index uint32
+	}
+
+	seedBytes := []byte(fmt.Sprintf("%x", randomSeed))
+
+	grades := make([]*gradedIndex, len(nodes))
+
+	i := 0
+	for _, node := range nodes {
+
+		// Reputation per node is presently not implemented so it is constant
+		reputation := uint64(1)
+
+		hashInput := make([]byte, len(seedBytes)+len(node))
+		copy(hashInput, seedBytes)
+		copy(hashInput[len(seedBytes):], node)
+		nodeHash := hash.CalcSha256(hashInput)
+		nodeHash4LSB := nodeHash[len(nodeHash)-4:]
+		nodeHash4LSBInt := binary.LittleEndian.Uint32(nodeHash4LSB)
+		grades[i] = &gradedIndex{
+			grade: uint64(nodeHash4LSBInt) * reputation,
+			index: uint32(i),
+		}
+		i++
+	}
+	// Descending order
+	sort.Slice(grades, func(i, j int) bool {
+		return grades[i].grade > grades[j].grade
+	})
+
 	indices := make([]uint32, committeeSize)
 	for i := 0; i < int(committeeSize); i++ {
-		indices[i] = uint32(i)
+		indices[i] = grades[i].index
 	}
-	return indices
+	return indices, nil
 }
