329b55f2310ef75f032a2fced59c8c6cbd163601
Shai Yallin <shai.yallin@gmail.com>
2018-11-14 20:38:33 +0200

    test is stuck waiting on all nodes to reflect transactions

--- test/acceptance/quorum_size_test.go
+++ test/acceptance/quorum_size_test.go
@@ -2,6 +2,7 @@ package acceptance
 
 import (
 	"context"
+	"github.com/orbs-network/orbs-network-go/instrumentation/log"
 	"github.com/orbs-network/orbs-network-go/test"
 	"github.com/orbs-network/orbs-network-go/test/harness"
 	"github.com/orbs-network/orbs-spec/types/go/protocol/client"
@@ -10,23 +11,25 @@ import (
 )
 
 func TestNetworkStartedWithEnoughNodes_SucceedsClosingBlocks(t *testing.T) {
-	harness.Network(t).WithNumNodes(6).StartPartial(4, func(parent context.Context, network harness.TestNetworkDriver) {
-		ctx, cancel := context.WithTimeout(parent, 1*time.Second)
-		defer cancel()
+	harness.Network(t).
+		WithNumNodes(6).
+		WithLogFilters(log.ExcludeEntryPoint("BlockSync")).
+		StartPartial(4, func(parent context.Context, network harness.TestNetworkDriver) {
+			ctx, cancel := context.WithTimeout(parent, 1*time.Second)
+			defer cancel()
 
-		contract := network.GetBenchmarkTokenContract()
-		contract.DeployBenchmarkToken(ctx, 5)
+			contract := network.GetBenchmarkTokenContract()
+			contract.DeployBenchmarkToken(ctx, 5)
 
-		amount := uint64(23)
-		requireSuccess(t, contract.SendTransfer(ctx, 0, amount, 5, 6))
-	})
+			requireSuccess(t, contract.SendTransfer(ctx, 0, uint64(23), 5, 6))
+		})
 }
 
-func requireSuccess( t *testing.T, ch chan *client.SendTransactionResponse) {
+func requireSuccess(t *testing.T, ch chan *client.SendTransactionResponse) {
 	select {
 	case res := <-ch:
 		test.RequireSuccess(t, res, "transaction should be successfully committed and executed")
-	case <-time.After(1 * time.Second):
+	case <-time.After(2000 * time.Millisecond):
 		t.Fatalf("transaction did not succeed within timeout")
 	}
 }
