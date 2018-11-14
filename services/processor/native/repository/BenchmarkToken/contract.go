ca15ca137a7d7778820080ee563d52f7a11470f0
Tal Kol <talkol@gmail.com>
2018-11-14 09:16:05 +0200

    Example implementations of what we want the new contract api to look like

--- services/processor/native/repository/BenchmarkToken/contract.go
+++ services/processor/native/repository/BenchmarkToken/contract.go
@@ -3,98 +3,35 @@ package benchmarktoken
 import (
 	"fmt"
 	"github.com/orbs-network/orbs-contract-sdk/go/sdk"
+	"github.com/orbs-network/orbs-contract-sdk/go/sdk/address"
+	"github.com/orbs-network/orbs-contract-sdk/go/sdk/state"
 )
 
-var CONTRACT = sdk.ContractInfo{
-	Name:       "BenchmarkToken",
-	Permission: sdk.PERMISSION_SCOPE_SERVICE,
-	Methods: map[string]sdk.MethodInfo{
-		METHOD_INIT.Name:        METHOD_INIT,
-		METHOD_TRANSFER.Name:    METHOD_TRANSFER,
-		METHOD_GET_BALANCE.Name: METHOD_GET_BALANCE,
-	},
-	InitSingleton: newContract,
-}
-
-func newContract(base *sdk.BaseContract) sdk.ContractInstance {
-	return &contract{base}
-}
-
-type contract struct{ *sdk.BaseContract }
-
-///////////////////////////////////////////////////////////////////////////
+var EXPORTS = sdk.Export(transfer, getBalance)
 
 const TOTAL_SUPPLY = 10000
 
-///////////////////////////////////////////////////////////////////////////
-
-var METHOD_INIT = sdk.MethodInfo{
-	Name:           "_init",
-	External:       false,
-	Access:         sdk.ACCESS_SCOPE_READ_WRITE,
-	Implementation: (*contract)._init,
+func _init() {
+	ownerAddress := address.GetSignerAddress()
+	state.WriteUint64ByAddress(ownerAddress, TOTAL_SUPPLY)
 }
 
-func (c *contract) _init(ctx sdk.Context) error {
-	ownerAddress, err := c.Address.GetSignerAddress(ctx)
-	if err != nil {
-		return err
-	}
-	return c.State.WriteUint64ByAddress(ctx, ownerAddress, TOTAL_SUPPLY)
-}
-
-///////////////////////////////////////////////////////////////////////////
-
-var METHOD_TRANSFER = sdk.MethodInfo{
-	Name:           "transfer",
-	External:       true,
-	Access:         sdk.ACCESS_SCOPE_READ_WRITE,
-	Implementation: (*contract).transfer,
-}
-
-func (c *contract) transfer(ctx sdk.Context, amount uint64, targetAddress []byte) error {
+func transfer(amount uint64, targetAddress []byte) {
 	// sender
-	callerAddress, err := c.Address.GetCallerAddress(ctx)
-	if err != nil {
-		return err
-	}
-	callerBalance, err := c.State.ReadUint64ByAddress(ctx, callerAddress)
-	if err != nil {
-		return err
-	}
+	callerAddress := address.GetCallerAddress()
+	callerBalance := state.ReadUint64ByAddress(callerAddress)
 	if callerBalance < amount {
-		return fmt.Errorf("transfer of %d failed since balance is only %d", amount, callerBalance)
+		panic(fmt.Sprintf("transfer of %d failed since balance is only %d", amount, callerBalance))
 	}
-	err = c.State.WriteUint64ByAddress(ctx, callerAddress, callerBalance-amount)
-	if err != nil {
-		return err
-	}
+	state.WriteUint64ByAddress(callerAddress, callerBalance-amount)
 
 	// recipient
-	err = c.Address.ValidateAddress(ctx, targetAddress)
-	if err != nil {
-		return err
-	}
-	targetBalance, err := c.State.ReadUint64ByAddress(ctx, targetAddress)
-	if err != nil {
-		return err
-	}
-	return c.State.WriteUint64ByAddress(ctx, targetAddress, targetBalance+amount)
+	address.ValidateAddress(targetAddress)
+	targetBalance := state.ReadUint64ByAddress(targetAddress)
+	state.WriteUint64ByAddress(targetAddress, targetBalance+amount)
 }
 
-///////////////////////////////////////////////////////////////////////////
-
-var METHOD_GET_BALANCE = sdk.MethodInfo{
-	Name:           "getBalance",
-	External:       true,
-	Access:         sdk.ACCESS_SCOPE_READ_ONLY,
-	Implementation: (*contract).getBalance,
-}
-
-func (c *contract) getBalance(ctx sdk.Context, targetAddress []byte) (uint64, error) {
-	err := c.Address.ValidateAddress(ctx, targetAddress)
-	if err != nil {
-		return 0, err
-	}
-	return c.State.ReadUint64ByAddress(ctx, targetAddress)
+func getBalance(targetAddress []byte) uint64 {
+	address.ValidateAddress(targetAddress)
+	return state.ReadUint64ByAddress(targetAddress)
 }
--- test/contracts/nop.go
+++ test/contracts/nop.go
@@ -7,32 +7,9 @@ import (
 	"github.com/orbs-network/orbs-contract-sdk/go/sdk"
 )
 
-var CONTRACT = sdk.ContractInfo{
-	Name:       "_Nop",
-	Permission: sdk.PERMISSION_SCOPE_SERVICE,
-	Methods: map[string]sdk.MethodInfo{
-		METHOD_INIT.Name:  METHOD_INIT,
-	},
-	InitSingleton: newContract,
-}
+var EXPORTS = sdk.Export()
 
-func newContract(base *sdk.BaseContract) sdk.ContractInstance {
-	return &contract{base}
-}
-
-type contract struct{ *sdk.BaseContract }
-
-///////////////////////////////////////////////////////////////////////////
-
-var METHOD_INIT = sdk.MethodInfo{
-	Name:           "_init",
-	External:       false,
-	Access:         sdk.ACCESS_SCOPE_READ_WRITE,
-	Implementation: (*contract)._init,
-}
-
-func (c *contract) _init(ctx sdk.Context) error {
-	return nil
+func _init() {
 }
 `
 
