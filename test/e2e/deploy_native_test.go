17cc137b4f05a2e8ca186b2c815451986236952e
Tal Kol <talkol@gmail.com>
2018-11-26 16:18:55 +0200

    Make it easier to understand in IDE and logs which iteration is the one that fails

--- test/e2e/deploy_native_test.go
+++ test/e2e/deploy_native_test.go
@@ -17,69 +17,71 @@ func TestDeploymentOfNativeContract(t *testing.T) {
 		t.Skip("Skipping E2E tests in short mode")
 	}
 
-	lt := time.Now()
-	printTestTime(t, "started", &lt)
+	runMultipleTimes(t, func(t *testing.T) {
 
-	h := newHarness()
-	printTestTime(t, "new harness", &lt) // slow do to warm up compilation
+		h := newHarness()
+		lt := time.Now()
+		printTestTime(t, "started", &lt)
 
-	counterStart := uint64(time.Now().UnixNano())
+		counterStart := uint64(time.Now().UnixNano())
 
-	printTestTime(t, "send deploy - start", &lt)
-	response, err := h.deployNativeContract(fmt.Sprintf("CounterFrom%d", counterStart), []byte(contracts.NativeSourceCodeForCounter(counterStart)))
-	printTestTime(t, "send deploy - end", &lt)
+		printTestTime(t, "send deploy - start", &lt)
+		response, err := h.deployNativeContract(fmt.Sprintf("CounterFrom%d", counterStart), []byte(contracts.NativeSourceCodeForCounter(counterStart)))
+		printTestTime(t, "send deploy - end", &lt)
 
-	require.NoError(t, err, "deploy transaction should not return error")
-	test.RequireSuccess(t, response, "deploy transaction should be successfully committed and executed")
+		require.NoError(t, err, "deploy transaction should not return error")
+		test.RequireSuccess(t, response, "deploy transaction should be successfully committed and executed")
 
-	// check counter
-	ok := test.Eventually(test.EVENTUALLY_DOCKER_E2E_TIMEOUT, func() bool {
-		getCounter := builders.NonSignedTransaction().
-			WithMethod(primitives.ContractName(fmt.Sprintf("CounterFrom%d", counterStart)), "get")
+		// check counter
+		ok := test.Eventually(test.EVENTUALLY_DOCKER_E2E_TIMEOUT, func() bool {
+			getCounter := builders.NonSignedTransaction().
+				WithMethod(primitives.ContractName(fmt.Sprintf("CounterFrom%d", counterStart)), "get")
 
-		printTestTime(t, "call method - start", &lt)
-		response, err2 := h.callMethod(getCounter.Builder())
-		printTestTime(t, "call method - end", &lt)
+			printTestTime(t, "call method - start", &lt)
+			response, err2 := h.callMethod(getCounter.Builder())
+			printTestTime(t, "call method - end", &lt)
 
-		if err2 == nil && response.CallMethodResult() == protocol.EXECUTION_RESULT_SUCCESS {
-			outputArgsIterator := builders.ClientCallMethodResponseOutputArgumentsDecode(response)
-			if outputArgsIterator.HasNext() {
-				return outputArgsIterator.NextArguments().Uint64Value() == counterStart
+			if err2 == nil && response.CallMethodResult() == protocol.EXECUTION_RESULT_SUCCESS {
+				outputArgsIterator := builders.ClientCallMethodResponseOutputArgumentsDecode(response)
+				if outputArgsIterator.HasNext() {
+					return outputArgsIterator.NextArguments().Uint64Value() == counterStart
+				}
 			}
-		}
-		return false
-	})
-	require.True(t, ok, "get counter should return counter start")
+			return false
+		})
+		require.True(t, ok, "get counter should return counter start")
 
-	// transaction to add to the counter
-	amount := uint64(17)
-	add := builders.Transaction().
-		WithMethod(primitives.ContractName(fmt.Sprintf("CounterFrom%d", counterStart)), "add").
-		WithArgs(amount).
-		Builder()
+		// transaction to add to the counter
+		amount := uint64(17)
+		add := builders.Transaction().
+			WithMethod(primitives.ContractName(fmt.Sprintf("CounterFrom%d", counterStart)), "add").
+			WithArgs(amount).
+			Builder()
 
-	printTestTime(t, "send transaction - start", &lt)
-	response, err = h.sendTransaction(add)
-	printTestTime(t, "send transaction - end", &lt)
+		printTestTime(t, "send transaction - start", &lt)
+		response, err = h.sendTransaction(add)
+		printTestTime(t, "send transaction - end", &lt)
 
-	require.NoError(t, err, "add transaction should not return error")
-	test.RequireSuccess(t, response, "add transaction should be successfully committed and executed")
+		require.NoError(t, err, "add transaction should not return error")
+		test.RequireSuccess(t, response, "add transaction should be successfully committed and executed")
 
-	// check counter
-	ok = test.Eventually(test.EVENTUALLY_DOCKER_E2E_TIMEOUT, func() bool {
-		getCounter := builders.NonSignedTransaction().
-			WithMethod(primitives.ContractName(fmt.Sprintf("CounterFrom%d", counterStart)), "get")
+		// check counter
+		ok = test.Eventually(test.EVENTUALLY_DOCKER_E2E_TIMEOUT, func() bool {
+			getCounter := builders.NonSignedTransaction().
+				WithMethod(primitives.ContractName(fmt.Sprintf("CounterFrom%d", counterStart)), "get")
 
-		response, err := h.callMethod(getCounter.Builder())
-		if err == nil && response.CallMethodResult() == protocol.EXECUTION_RESULT_SUCCESS {
-			outputArgsIterator := builders.ClientCallMethodResponseOutputArgumentsDecode(response)
-			if outputArgsIterator.HasNext() {
-				return outputArgsIterator.NextArguments().Uint64Value() == counterStart+amount
+			response, err := h.callMethod(getCounter.Builder())
+			if err == nil && response.CallMethodResult() == protocol.EXECUTION_RESULT_SUCCESS {
+				outputArgsIterator := builders.ClientCallMethodResponseOutputArgumentsDecode(response)
+				if outputArgsIterator.HasNext() {
+					return outputArgsIterator.NextArguments().Uint64Value() == counterStart+amount
+				}
 			}
-		}
-		return false
-	})
+			return false
+		})
 
-	require.True(t, ok, "get counter should return counter start plus added value")
-	printTestTime(t, "done", &lt)
+		require.True(t, ok, "get counter should return counter start plus added value")
+		printTestTime(t, "done", &lt)
+
+	})
 }
--- test/e2e/main_test.go
+++ test/e2e/main_test.go
@@ -1,21 +1,13 @@
 package e2e
 
 import (
+	"fmt"
 	"os"
 	"testing"
+	"time"
 )
 
-func runOnce(m *testing.M) int {
-	return m.Run()
-}
-
-func runTwice(m *testing.M) int {
-	if exitCode := runOnce(m); exitCode != 0 {
-		return exitCode
-	}
-
-	return runOnce(m)
-}
+const TIMES_TO_RUN_EACH_TEST = 2
 
 func TestMain(m *testing.M) {
 	exitCode := 0
@@ -26,12 +18,12 @@ func TestMain(m *testing.M) {
 		cleanNativeProcessorCache()
 		n := newInProcessE2ENetwork()
 
-		exitCode = runTwice(m)
+		exitCode = m.Run()
 		n.gracefulShutdown()
 
 		cleanNativeProcessorCache()
 	} else {
-		exitCode = runOnce(m)
+		exitCode = m.Run()
 	}
 
 	os.Exit(exitCode)
@@ -40,4 +32,12 @@ func TestMain(m *testing.M) {
 func cleanNativeProcessorCache() {
 	_, dirToCleanup := getProcessorArtifactPath()
 	os.RemoveAll(dirToCleanup)
+}
+
+func runMultipleTimes(t *testing.T, f func(t *testing.T)) {
+	for i := 0; i < TIMES_TO_RUN_EACH_TEST; i++ {
+		name := fmt.Sprintf("%s_#%d", t.Name(), i+1)
+		t.Run(name, f)
+		time.Sleep(100 * time.Millisecond) // give async processes time to separate between iterations
+	}
 }
