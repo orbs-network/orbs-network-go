da954e0b0661ce8707da2ea5dc2c22c9b06c17f2
Shai Yallin <shai.yallin@gmail.com>
2018-11-27 21:04:08 +0200

    fixing gamma deploy native test to wait for add transaction

--- devtools/gammaserver/deploy_native_test.go
+++ devtools/gammaserver/deploy_native_test.go
@@ -11,6 +11,7 @@ import (
 	"time"
 )
 
+//TODO decide if we need this test - the Gamma e2e also covers deployment of native code, so what extra value does this test add?
 func TestNonLeaderDeploysNativeContract(t *testing.T) {
 	if testing.Short() {
 		t.Skip("Skipping compilation of contracts in short mode")
@@ -25,15 +26,15 @@ func TestNonLeaderDeploysNativeContract(t *testing.T) {
 		t.Log("deploying contract")
 
 		output := <-contract.SendDeployCounterContract(ctx, 1) // leader is nodeIndex 0, validator is nodeIndex 1
-
-		// wait for contract deployment take effect in node state
-		network.WaitForTransactionInState(ctx, output.TransactionReceipt().Txhash())
+		network.WaitForTransactionInState(ctx, output.TransactionReceipt().Txhash()) // wait for contract deployment take effect in node state
 
 		require.EqualValues(t, counterStart, <-contract.CallCounterGet(ctx, 0), "get counter after deploy")
 
 		t.Log("transacting with contract")
 
-		<-contract.SendCounterAdd(ctx, 1, 17)
+		output = <-contract.SendCounterAdd(ctx, 1, 17)
+		network.WaitForTransactionInState(ctx, output.TransactionReceipt().Txhash())
+
 		require.EqualValues(t, counterStart+17, <-contract.CallCounterGet(ctx, 0), "get counter after transaction")
 
 	})
