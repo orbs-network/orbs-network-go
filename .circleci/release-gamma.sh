b36c49dd27d3579c249fb381e8214706d74247b7
Kirill Maksimov <kirill@orbs.com>
2018-11-29 12:59:12 +0200

    Do not tag gamma image as latest

--- .circleci/release-gamma.sh
+++ .circleci/release-gamma.sh
@@ -3,7 +3,5 @@
 docker login -u $DOCKER_HUB_LOGIN -p $DOCKER_HUB_PASSWORD
 
 docker tag orbs:gamma-server orbsnetwork/gamma:$CIRCLE_TAG
-docker tag orbs:gamma-server orbsnetwork/gamma:latest
 
 docker push orbsnetwork/gamma:$CIRCLE_TAG
-docker push orbsnetwork/gamma:latest
