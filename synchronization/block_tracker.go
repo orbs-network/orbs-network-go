6887483d91691ec3ca47ffccd3a952bc8498de46
Ido Zilberberg <idoz@orbs.com>
2018-11-30 15:10:42 +0200

    Added log messages to BlockTracker

--- synchronization/block_tracker.go
+++ synchronization/block_tracker.go
@@ -3,10 +3,13 @@ package synchronization
 import (
 	"context"
 	"fmt"
+	"github.com/orbs-network/orbs-network-go/instrumentation/log"
 	"github.com/orbs-network/orbs-spec/types/go/primitives"
 	"github.com/pkg/errors"
 	"sync"
 )
+
+var LogTag = log.Service("block-tracker")
 
 type BlockTracker struct {
 	graceDistance uint16 // this is not primitives.BlockHeight on purpose, to indicate that grace distance should be small
@@ -14,12 +17,16 @@ type BlockTracker struct {
 	mutex         sync.RWMutex
 	currentHeight uint64 // this is not primitives.BlockHeight so as to avoid unnecessary casts
 	latch         chan struct{}
-
-	fireOnWait func() // used by unit test
+	logger        log.BasicLogger
+	fireOnWait    func() // used by unit test
 }
 
-func NewBlockTracker(startingHeight uint64, graceDist uint16) *BlockTracker {
+func NewBlockTracker(parent log.BasicLogger, startingHeight uint64, graceDist uint16) *BlockTracker {
+
+	logger := parent.WithTags(LogTag)
+
 	return &BlockTracker{
+		logger:        logger,
 		currentHeight: startingHeight,
 		graceDistance: graceDist,
 		latch:         make(chan struct{}),
@@ -46,9 +53,9 @@ func (t *BlockTracker) readAtomicHeightAndLatch() (uint64, chan struct{}) {
 // waits until we reach a block at the specified height, or until the context is closed
 // to wait until some timeout, pass a child context with a deadline
 func (t *BlockTracker) WaitForBlock(ctx context.Context, requestedHeight primitives.BlockHeight) error {
-
 	requestedHeightUint := uint64(requestedHeight)
 	currentHeight, currentLatch := t.readAtomicHeightAndLatch()
+	t.logger.Info("WaitForBlock() start", log.Uint64("current-height", currentHeight), log.Uint64("requestedHeight", requestedHeightUint))
 
 	if currentHeight >= requestedHeightUint { // requested block already committed
 		return nil
@@ -59,15 +66,20 @@ func (t *BlockTracker) WaitForBlock(ctx context.Context, requestedHeight primiti
 	}
 
 	for currentHeight < requestedHeightUint {
+		t.logger.Info("WaitForBlock() Before wait for block", log.Uint64("current-height", currentHeight), log.Uint64("requestedHeight", requestedHeightUint))
 		if t.fireOnWait != nil {
 			t.fireOnWait()
 		}
 		select {
 		case <-ctx.Done():
+			t.logger.Info("WaitForBlock() ctx.Done() called", log.Uint64("current-height", currentHeight), log.Uint64("requestedHeight", requestedHeightUint))
 			return errors.Wrap(ctx.Err(), fmt.Sprintf("aborted while waiting for block at height %v", requestedHeight))
 		case <-currentLatch:
+			t.logger.Info("WaitForBlock() Latch released", log.Uint64("current-height", currentHeight), log.Uint64("requestedHeight", requestedHeightUint))
 			currentHeight, currentLatch = t.readAtomicHeightAndLatch()
+			t.logger.Info("WaitForBlock() read new height", log.Uint64("current-height", currentHeight), log.Uint64("requestedHeight", requestedHeightUint))
 		}
 	}
+	t.logger.Info("WaitForBlock() return", log.Uint64("current-height", currentHeight), log.Uint64("requestedHeight", requestedHeightUint))
 	return nil
 }
