7cd613aa3237ca1c337549aa1e49b253c90fcdac
Tal Kol <talkol@gmail.com>
2018-12-03 22:29:41 +0200

    Made cpunoise less aggressive

--- test/cpunoise.go
+++ test/cpunoise.go
@@ -8,10 +8,10 @@ import (
 )
 
 var (
-	MIN_REST_DURATION      = 1 * time.Millisecond
+	MIN_REST_DURATION      = 2 * time.Millisecond
 	MAX_REST_DURATION      = 5 * time.Millisecond
-	MIN_BURST_DURATION     = 1 * time.Millisecond
-	MAX_BURST_DURATION     = 2 * time.Millisecond
+	MIN_BURST_DURATION     = 100 * time.Microsecond
+	MAX_BURST_DURATION     = 1 * time.Millisecond
 	MAX_ALLOWED_STARVATION = 10 * time.Millisecond
 )
 
