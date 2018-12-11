bc32dd9460bc2c2c5dbbb5db08daf708b8634547
Kirill Maksimov <kirill@orbs.com>
2018-12-11 13:36:42 +0200

    Refactor e2e stress test, disable rates

--- .circleci/e2e-testnet.sh
+++ .circleci/e2e-testnet.sh
@@ -2,7 +2,7 @@
 
 docker run -ti \
     -e API_ENDPOINT=http://us-east-1.global.nodes.staging.orbs-test.com/vchains/42/api/v1/ \
-    -e STRESS_TEST_NUMBER_OF_TRANSACTIONS=10000 \
+    -e STRESS_TEST_NUMBER_OF_TRANSACTIONS=100 \
     -e STRESS_TEST_FAILURE_RATE=20 \
     -e STRESS_TEST_TARGET_TPS=200 \
     -e STRESS_TEST=true \
