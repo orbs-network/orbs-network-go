998f311a6e13cd69094fe3a7f0bba4a599071584
Tal Kol <talkol@gmail.com>
2018-12-17 16:46:04 +0200

    Added support for Keccak256

--- crypto/hash/hash_test.go
+++ crypto/hash/hash_test.go
@@ -1,7 +1,7 @@
-package hash_test
+package hash
 
 import (
-	"github.com/orbs-network/orbs-network-go/crypto/hash"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
@@ -10,30 +10,35 @@ var someData = []byte("testing")
 const (
 	ExpectedSha256         = "cf80cd8aed482d5d1527d7dc72fceff84e6326592848447d2dc0b0e87dfc9a90"
 	ExpectedSha256Ripmd160 = "1acb19a469206161ed7e5ed9feb996a6e24be441"
+	ExpectedKeccak256      = "5f16f4c7f149ac4f9510d9cf8cf384038ad348b3bcdc01915f95de12df9d1b02"
 )
 
 func TestCalcSha256(t *testing.T) {
-	h := hash.CalcSha256(someData)
-	if h.String() != ExpectedSha256 {
-		t.Errorf("sha256 failed expected %s got %s", ExpectedSha256, h)
-	}
+	h := CalcSha256(someData)
+	require.Equal(t, SHA256_HASH_SIZE_BYTES, len(h))
+	require.Equal(t, ExpectedSha256, h.String(), "result should match")
 }
 
 func TestCalcRipemd160Sha256(t *testing.T) {
-	h := hash.CalcRipemd160Sha256(someData)
-	if h.String() != ExpectedSha256Ripmd160 {
-		t.Errorf("sha256ripmd160 failed expected %s got %s", ExpectedSha256Ripmd160, h)
-	}
+	h := CalcRipemd160Sha256(someData)
+	require.Equal(t, RIPMD160_HASH_SIZE_BYTES, len(h))
+	require.Equal(t, ExpectedSha256Ripmd160, h.String(), "result should match")
+}
+
+func TestCalcKeccak256(t *testing.T) {
+	h := CalcKeccak256(someData)
+	require.Equal(t, KECCAK256_HASH_SIZE_BYTES, len(h))
+	require.Equal(t, ExpectedKeccak256, h.String(), "result should match")
 }
 
 func BenchmarkCalcSha256(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		hash.CalcSha256(someData)
+		CalcSha256(someData)
 	}
 }
 
 func BenchmarkCalcRipemd160Sha256(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		hash.CalcRipemd160Sha256(someData)
+		CalcRipemd160Sha256(someData)
 	}
 }
