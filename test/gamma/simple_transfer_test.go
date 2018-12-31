48ed0dac15286a055c276007afff3c77934690ee
Shai Yallin <shai.yallin@gmail.com>
2018-12-31 14:14:12 +0200

    gamma e2e test now waits for tx pool to reach block 1 before starting

--- test/gamma/simple_transfer_test.go
+++ test/gamma/simple_transfer_test.go
@@ -2,34 +2,23 @@ package gamma
 
 import (
 	"github.com/orbs-network/orbs-client-sdk-go/gammacli/test"
-	testUtils "github.com/orbs-network/orbs-network-go/test"
 	"github.com/stretchr/testify/require"
 	"strings"
 	"testing"
-	"time"
 )
 
 func TestSimpleTransfer(t *testing.T) {
 	h := newHarness()
 	defer h.shutdown()
 
+	h.waitUntilTransactionPoolIsReady(t)
+
 	cli := test.GammaCliWithPort(h.port)
 
-	sendTxOutChan := make(chan string, 1)
+	out, err := cli.Run("send-tx", "transfer.json")
+	require.Contains(t, out, `"ExecutionResult": "SUCCESS"`)
 
-	// TODO remove Eventually loop once node can handle requests at block height 0
-	require.True(t, testUtils.Eventually(1*time.Second, func() bool {
-		out, err := cli.Run("send-tx", "transfer.json")
-		t.Log(out)
-		success := err == nil && strings.Contains(out, `"ExecutionResult": "SUCCESS"`)
-		if success {
-			sendTxOutChan <- out
-		}
-		return success
-	}), "transfer should eventually succeed")
-
-	sendTxOut := <-sendTxOutChan
-	txId := extractTxIdFromSendTxOutput(sendTxOut)
+	txId := extractTxIdFromSendTxOutput(out)
 	t.Log(txId)
 
 	sendTxOut, err := cli.Run("get-status", txId)
