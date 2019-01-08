4dcf89dd7b5f1ec73c0cff55ff6fb68c7c6ba55c
Jonathan Levison <jonathanl@orbs.com>
2019-01-08 13:55:50 +0200

    added decode and benchmarks

--- crypto/encoding/hex.go
+++ crypto/encoding/hex.go
@@ -3,10 +3,11 @@ package encoding
 import (
 	"encoding/hex"
 	"github.com/orbs-network/orbs-network-go/crypto/hash"
+	"github.com/pkg/errors"
 	"strings"
 )
 
-func EncodeHex(data []byte) string {
+func EncodeHex(data []byte) string { // EIP-55 'complaint', but using sha2 and not sha3
 	result := []byte(hex.EncodeToString(data))
 	hashed := hash.CalcSha256(data)
 
@@ -28,11 +29,26 @@ func EncodeHex(data []byte) string {
 
 // on decode error (eg. non hex character in str) returns zero_value, error
 // on checksum failure returns decoded_value, error (so users could warn about checksum but still use the decoded)
-// if all is lower or upper then the check is ignored (as the checksum was probably not taken into account)
+// if all is lower or upper then the checksum check is ignored (as the checksum was probably not taken into account)
 func DecodeHex(str string) ([]byte, error) {
 	if strings.HasPrefix(str, "0x") {
 		str = str[2:]
 	}
 
-	return hex.DecodeString(str)
+	data, err := hex.DecodeString(str)
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid hex string")
+	}
+
+	encoded := EncodeHex(data)
+	if encoded[2:] != str {
+		// checksum error, we will allow if the source is in uniform case (all lower/upper)
+		if strings.ToUpper(str) == str || strings.ToLower(str) == str {
+			return data, nil
+		} else {
+			return data, errors.New("invalid checksum")
+		}
+	}
+
+	return data, nil
 }
--- crypto/encoding/hex_test.go
+++ crypto/encoding/hex_test.go
@@ -25,3 +25,46 @@ func TestHexEncodeWithChecksum(t *testing.T) {
 		require.Equal(t, pair.checksumEncodedHex, encoded, "expected encoding with a specific result for each input")
 	}
 }
+
+func TestHexDecodeGoodChecksum(t *testing.T) {
+	for _, pair := range encodeStringTestTable {
+		rawData, err := hex.DecodeString(pair.sourceHex)
+		require.NoError(t, err, "failed to decode, human error most likely")
+		decoded, err := DecodeHex(pair.checksumEncodedHex)
+		require.NoError(t, err, "checksum should be valid")
+		require.Equal(t, rawData, decoded, "data should be decoded correctly")
+	}
+}
+
+func TestHexDecodeBadChecksum(t *testing.T) {
+	pair := encodeStringTestTable[0]
+	rawData, err := hex.DecodeString(pair.sourceHex)
+	require.NoError(t, err, "failed to decode, human error most likely")
+	wrongCheckSum := "de" + pair.checksumEncodedHex[4:]
+	decoded, err := DecodeHex(wrongCheckSum)
+	require.EqualError(t, err, "invalid checksum", "checksum should be invalid")
+	require.Equal(t, rawData, decoded, "data should be decoded correctly even though checksum is invalid")
+}
+
+func TestHexDecodeInvalidHex(t *testing.T) {
+	decoded, err := DecodeHex("0")
+	require.Error(t, err, "should not succeed with invalid hex")
+	require.Nil(t, decoded, "result should be nil")
+}
+
+func BenchmarkHexEncodeWithChecksum(b *testing.B) {
+	rawData, err := hex.DecodeString(encodeStringTestTable[0].sourceHex)
+	require.NoError(b, err, "failed to decode, human error most likely")
+	for i := 0; i < b.N; i++ {
+		EncodeHex(rawData)
+	}
+}
+
+func BenchmarkHexDecodeWithChecksum(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		_, err := DecodeHex(encodeStringTestTable[0].checksumEncodedHex)
+		if err != nil { // require/testify is very slow
+			b.Fail()
+		}
+	}
+}
