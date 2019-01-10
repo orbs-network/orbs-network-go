55544202f4c795ae7a5c92992e6782f7f9abe78f
Shai Yallin <shai.yallin@gmail.com>
2019-01-10 16:31:06 +0200

    adding junit reporter

--- docker/test/test.sh
+++ docker/test/test.sh
@@ -7,6 +7,9 @@ export GIT_COMMIT=$(git rev-parse HEAD)
 
 export SRC=/go/src/github.com/orbs-network/orbs-network-go
 
-docker run orbs:build sh $SRC/test.sh
+docker run orbs:build bash $SRC/test.sh
+
+mkdir -p /tmp/test-results/
+docker cp orbs_build:$SRC/report.xml /tmp/test-results/
 
 docker-compose -f ./docker/test/docker-compose.yml up --abort-on-container-exit --exit-code-from orbs-e2e
