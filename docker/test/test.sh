6135e30eee0ecc45626e1dfe86e09faae5b0ee9a
Kirill Maksimov <kirill@orbs.com>
2019-01-10 18:03:10 +0300

    Mount block persistence into e2e instead of creating separate Docker image

--- docker/test/test.sh
+++ docker/test/test.sh
@@ -5,4 +5,11 @@ rm -rf _logs
 export GIT_BRANCH=$(source ./docker/tag.sh)
 export GIT_COMMIT=$(git rev-parse HEAD)
 
+rm -rf _tmp/blocks
+mkdir -p _tmp/blocks/node{1..3}
+
+cp ./test/e2e/blocks _tmp/blocks/node1
+cp ./test/e2e/blocks _tmp/blocks/node2
+cp ./test/e2e/blocks _tmp/blocks/node3
+
 docker-compose -f ./docker/test/docker-compose.yml up --abort-on-container-exit --exit-code-from orbs-e2e
