2423f3ed1833ca30ead536e14b7176906f1bb3e2
Shai Yallin <shai.yallin@gmail.com>
2019-01-10 10:33:02 +0200

    unit tests now run after build container has been built

--- docker/test/test.sh
+++ docker/test/test.sh
@@ -5,4 +5,8 @@ rm -rf _logs
 export GIT_BRANCH=$(source ./docker/tag.sh)
 export GIT_COMMIT=$(git rev-parse HEAD)
 
+export SRC=/go/src/github.com/orbs-network/orbs-network-go
+
+docker run orbs:build sh $SRC/test.sh
+
 docker-compose -f ./docker/test/docker-compose.yml up --abort-on-container-exit --exit-code-from orbs-e2e
