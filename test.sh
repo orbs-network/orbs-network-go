246a589d9bbfcd3f594058b928135567e5d41e73
Shai Yallin <shai.yallin@gmail.com>
2019-01-11 12:43:15 +0200

    adding basedir so that report.xml file ends up in the expected place

--- test.sh
+++ test.sh
@@ -1,17 +1,19 @@
-#!/bin/sh -x
+#!/bin/bash -x
+
+BASEDIR=$(dirname "$0")
 
 ulimit -S -n 20000
 
-source ./test.common.sh
+source $BASEDIR/test.common.sh
 
-go test -timeout 7m ./... -failfast -v 2>&1 | tee >(go-junit-report > ./report.xml) > test.out
+go test -timeout 7m "$BASEDIR/..." -failfast -v 2>&1 | tee >(go-junit-report > "$BASEDIR/report.xml") > "$BASEDIR/test.out"
 check_exit_code_and_report
 
 # this test must run separately since zero parallel package tests are allowed concurrently
-source ./test.goroutine-leaks.sh
+source $BASEDIR/test.goroutine-leaks.sh
 
 # this test must run separately since zero parallel package tests are allowed concurrently
-source ./test.memory-leaks.sh
+source $BASEDIR/test.memory-leaks.sh
 
 # uncomment to run component tests
-# ./test.components.sh
+# $BASEDIR/test.components.sh
