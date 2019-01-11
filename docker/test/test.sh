5c68c960930961650047b8e2d2ddeb1e803d73a3
Shai Yallin <shai.yallin@gmail.com>
2019-01-11 15:51:15 +0200

    more attempts to get test results to circle ci

--- docker/test/test.sh
+++ docker/test/test.sh
@@ -8,9 +8,12 @@ export GIT_COMMIT=$(git rev-parse HEAD)
 export SRC=/go/src/github.com/orbs-network/orbs-network-go
 
 # run in-process tests (unit tests, component tests, acceptance tests, etc)
+[ "$(docker ps -a | grep orbs_test)" ] && docker rm -f orbs_test
 docker run --name orbs_test orbs:build bash $SRC/test.sh
-mkdir -p /tmp/test-results/
-docker cp orbs_test:$SRC/report.xml /tmp/test-results/
+rm -rf _out
+mkdir -p _out/fast
+docker cp orbs_test:$SRC/results.xml _out/fast
+docker cp orbs_test:$SRC/test.out _out/fast
 
 # run docker-reliant tests
 docker-compose -f ./docker/test/docker-compose.yml up --abort-on-container-exit --exit-code-from orbs-e2e
--- test.sh
+++ test.sh
@@ -1,19 +1,17 @@
 #!/bin/bash -x
 
-BASEDIR=$(dirname "$0")
-
 ulimit -S -n 20000
 
-source $BASEDIR/test.common.sh
+source ./test.common.sh
 
-go test -timeout 7m "$BASEDIR/..." -failfast -v 2>&1 | tee >(go-junit-report > "$BASEDIR/report.xml") > "$BASEDIR/test.out"
+go test -timeout 7m ./... -failfast -v 2>&1 | tee >(go-junit-report > ./results.xml) > ./test.out
 check_exit_code_and_report
 
 # this test must run separately since zero parallel package tests are allowed concurrently
-source $BASEDIR/test.goroutine-leaks.sh
+source ./test.goroutine-leaks.sh
 
 # this test must run separately since zero parallel package tests are allowed concurrently
-source $BASEDIR/test.memory-leaks.sh
+source ./test.memory-leaks.sh
 
 # uncomment to run component tests
-# $BASEDIR/test.components.sh
+# ./test.components.sh
