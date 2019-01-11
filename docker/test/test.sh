8a9094c38bb5955aec7181bb41c477e044e70d4a
Shai Yallin <shai.yallin@gmail.com>
2019-01-11 15:00:41 +0200

    running tests in a newly named container

--- docker/test/test.sh
+++ docker/test/test.sh
@@ -7,9 +7,10 @@ export GIT_COMMIT=$(git rev-parse HEAD)
 
 export SRC=/go/src/github.com/orbs-network/orbs-network-go
 
-docker run orbs:build bash $SRC/test.sh
-
+# run in-process tests (unit tests, component tests, acceptance tests, etc)
+docker run --name orbs_test orbs:build bash $SRC/test.sh
 mkdir -p /tmp/test-results/
-docker cp orbs_build:$SRC/report.xml /tmp/test-results/
+docker cp orbs_test:$SRC/report.xml /tmp/test-results/
 
+# run docker-reliant tests
 docker-compose -f ./docker/test/docker-compose.yml up --abort-on-container-exit --exit-code-from orbs-e2e
