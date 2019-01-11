063f7473de5d99b4f73a3a81df7ce5bf12f71d5b
Shai Yallin <shai.yallin@gmail.com>
2019-01-11 21:30:16 +0200

    testing whether a failed test fails the build

--- docker/test/test.sh
+++ docker/test/test.sh
@@ -10,10 +10,12 @@ export SRC=/go/src/github.com/orbs-network/orbs-network-go
 # run in-process tests (unit tests, component tests, acceptance tests, etc)
 [ "$(docker ps -a | grep orbs_test)" ] && docker rm -f orbs_test
 docker run --name orbs_test orbs:build bash $SRC/test.sh
+#TEST_EXIT_CODE=$?
 rm -rf _out
 mkdir -p _out/fast
 docker cp orbs_test:$SRC/results.xml _out/fast
 docker cp orbs_test:$SRC/test.out _out/fast
+#if [ "$TEST_EXIT_CODE" != 0 ] ; then exit "$TEST_EXIT_CODE" ; fi
 
 # run docker-reliant tests
 docker-compose -f ./docker/test/docker-compose.yml up --abort-on-container-exit --exit-code-from orbs-e2e
