340b8c0a46a6c4f131996547557a581e00b5eb42
Ido Zilberberg <idoz@orbs.com>
2019-01-13 16:24:52 +0200

    Added a 4th node to e2e tests on Docker and enabled Lean Helix

--- docker/test/test.sh
+++ docker/test/test.sh
@@ -6,10 +6,11 @@ export GIT_BRANCH=$(source ./docker/tag.sh)
 export GIT_COMMIT=$(git rev-parse HEAD)
 
 rm -rf _tmp/blocks
-mkdir -p _tmp/blocks/node{1..3}
+mkdir -p _tmp/blocks/node{1..4}
 
 cp ./test/e2e/blocks _tmp/blocks/node1
 cp ./test/e2e/blocks _tmp/blocks/node2
 cp ./test/e2e/blocks _tmp/blocks/node3
+cp ./test/e2e/blocks _tmp/blocks/node4
 
 docker-compose -f ./docker/test/docker-compose.yml up --abort-on-container-exit --exit-code-from orbs-e2e
