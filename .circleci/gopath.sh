f4f9a2c5c5f2b263c3e181dbdaebf735346ec3aa
Kirill Maksimov <kirill@orbs.com>
2019-01-14 10:32:03 +0200

    Install junit report

--- .circleci/gopath.sh
+++ .circleci/gopath.sh
@@ -2,3 +2,7 @@
 
 mkdir -p /go/src/github.com/orbs-network/
 mv ~/project/ /go/src/github.com/orbs-network/orbs-network-go
+
+mkdir -p /go/src/github.com/orbs-network/orbs-network-go/_out
+
+go get -u github.com/jstemmer/go-junit-report
