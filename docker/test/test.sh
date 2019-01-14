57fe790f74b218f6faba26bfccaf306d514b664b
Kirill Maksimov <kirill@orbs.com>
2019-01-14 11:23:43 +0200

    Remove docker tests, only run e2e with docker

--- docker/test/test.sh
+++ docker/test/test.sh
@@ -6,11 +6,6 @@ export GIT_BRANCH=$(source ./docker/tag.sh)
 export GIT_COMMIT=$(git rev-parse HEAD)
 export SRC=/go/src/github.com/orbs-network/orbs-network-go
 
-# run in-process tests (unit tests, component tests, acceptance tests, etc)
-rm -rf _out
-mkdir -p _out
-docker-compose -f ./docker/test/docker-compose.tests.yml up --abort-on-container-exit --exit-code-from orbs-tests
-
 # prepare persistent blocks for docker tests
 
 rm -rf _tmp/blocks
