7ce1aa89c3f9f2ca13fc9ea2d162969a06131ce0
Ido Zilberberg <idoz@orbs.com>
2019-01-17 15:05:15 +0200

    Fixed VerifyRandomSeed() to accommodate for nil MemberId() which is normal value from LH lib because it represents "master" memberId, aggregated from all random seeds

--- services/consensusalgo/leanhelixconsensus/key_manager.go
+++ services/consensusalgo/leanhelixconsensus/key_manager.go
@@ -1,6 +1,7 @@
 package leanhelixconsensus
 
 import (
+	"bytes"
 	"encoding/binary"
 	lhprimitives "github.com/orbs-network/lean-helix-go/spec/types/go/primitives"
 	lhprotocol "github.com/orbs-network/lean-helix-go/spec/types/go/protocol"
@@ -45,12 +46,26 @@ func (km *keyManager) VerifyConsensusMessage(blockHeight lhprimitives.BlockHeigh
 }
 
 func (km *keyManager) VerifyRandomSeed(blockHeight lhprimitives.BlockHeight, content []byte, sender *lhprotocol.SenderSignature) error {
+
+	// This is hack in v1 because BLS signatures / thresholds are not supported so master memberId is nil
+	// See https://tree.taiga.io/project/orbs-network/us/565
+	if len(sender.MemberId()) == 0 {
+		sig := km.AggregateRandomSeed(blockHeight, nil)
+		if bytes.Equal(sender.Signature(), sig) {
+			return nil
+		} else {
+			return errors.Errorf("Mismatch in signature on blockHeight %s", blockHeight)
+		}
+		return nil
+	}
+
 	if err := digest.VerifyNodeSignature(primitives.NodeAddress(sender.MemberId()), content, primitives.EcdsaSecp256K1Sig(sender.Signature())); err != nil {
 		return errors.Wrapf(err, "digest.VerifyNodeSignature() failed")
 	}
 	return nil
 }
 
+// This is hack in v1 - see https://tree.taiga.io/project/orbs-network/us/565
 func (km *keyManager) AggregateRandomSeed(blockHeight lhprimitives.BlockHeight, randomSeedShares []*lhprotocol.SenderSignature) lhprimitives.RandomSeedSignature {
 	heightAsByteArray := make([]byte, 8)
 	binary.LittleEndian.PutUint64(heightAsByteArray, uint64(blockHeight))
