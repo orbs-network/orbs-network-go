7f706600012b9704e5f85c0d53ed0043e7356ed8
Ido Zilberberg <idoz@orbs.com>
2019-01-19 21:11:27 +0200

    Set test to benchmark consensus only - TestNonLeaderPropagatesTransactionsToLeader

--- test/acceptance/simple_transfer_test.go
+++ test/acceptance/simple_transfer_test.go
@@ -32,64 +32,67 @@ func TestCommitTransactionWithLeanHelix(t *testing.T) {
 }
 
 func TestLeaderCommitsTransactionsAndSkipsInvalidOnes(t *testing.T) {
-	newHarness(t).Start(func(parent context.Context, network NetworkHarness) {
-		ctx, cancel := context.WithTimeout(parent, 1*time.Second)
-		defer cancel()
+	newHarness(t).
+		Start(func(parent context.Context, network NetworkHarness) {
+			ctx, cancel := context.WithTimeout(parent, 1*time.Second)
+			defer cancel()
 
-		contract := network.BenchmarkTokenContract()
-		contract.DeployBenchmarkToken(ctx, 5)
+			contract := network.BenchmarkTokenContract()
+			contract.DeployBenchmarkToken(ctx, 5)
 
-		// leader is nodeIndex 0, validator is nodeIndex 1
+			// leader is nodeIndex 0, validator is nodeIndex 1
 
-		_, txHash1 := contract.Transfer(ctx, 0, 17, 5, 6)
-		contract.InvalidTransfer(ctx, 0, 5, 6)
-		_, txHash2 := contract.Transfer(ctx, 0, 22, 5, 6)
+			_, txHash1 := contract.Transfer(ctx, 0, 17, 5, 6)
+			contract.InvalidTransfer(ctx, 0, 5, 6)
+			_, txHash2 := contract.Transfer(ctx, 0, 22, 5, 6)
 
-		t.Log("waiting for leader blocks")
+			t.Log("waiting for leader blocks")
 
-		network.WaitForTransactionInNodeState(ctx, txHash1, 0)
-		network.WaitForTransactionInNodeState(ctx, txHash2, 0)
-		require.EqualValues(t, benchmarktoken.TOTAL_SUPPLY-39, contract.GetBalance(ctx, 0, 5), "getBalance result on leader")
-		require.EqualValues(t, 39, contract.GetBalance(ctx, 0, 6), "getBalance result on leader")
+			network.WaitForTransactionInNodeState(ctx, txHash1, 0)
+			network.WaitForTransactionInNodeState(ctx, txHash2, 0)
+			require.EqualValues(t, benchmarktoken.TOTAL_SUPPLY-39, contract.GetBalance(ctx, 0, 5), "getBalance result on leader")
+			require.EqualValues(t, 39, contract.GetBalance(ctx, 0, 6), "getBalance result on leader")
 
-		t.Log("waiting for non leader blocks")
+			t.Log("waiting for non leader blocks")
 
-		network.WaitForTransactionInNodeState(ctx, txHash1, 1)
-		network.WaitForTransactionInNodeState(ctx, txHash2, 1)
-		require.EqualValues(t, benchmarktoken.TOTAL_SUPPLY-39, contract.GetBalance(ctx, 1, 5), "getBalance result on non leader")
-		require.EqualValues(t, 39, contract.GetBalance(ctx, 1, 6), "getBalance result on non leader")
-	})
+			network.WaitForTransactionInNodeState(ctx, txHash1, 1)
+			network.WaitForTransactionInNodeState(ctx, txHash2, 1)
+			require.EqualValues(t, benchmarktoken.TOTAL_SUPPLY-39, contract.GetBalance(ctx, 1, 5), "getBalance result on non leader")
+			require.EqualValues(t, 39, contract.GetBalance(ctx, 1, 6), "getBalance result on non leader")
+		})
 }
 
 func TestNonLeaderPropagatesTransactionsToLeader(t *testing.T) {
-	newHarness(t).Start(func(parent context.Context, network NetworkHarness) {
-		ctx, cancel := context.WithTimeout(parent, 1*time.Second)
-		defer cancel()
+	newHarness(t).
+		WithConsensusAlgos(consensus.CONSENSUS_ALGO_TYPE_BENCHMARK_CONSENSUS).
+		Start(func(parent context.Context, network NetworkHarness) {
+			ctx, cancel := context.WithTimeout(parent, 1*time.Second)
+			defer cancel()
 
-		contract := network.BenchmarkTokenContract()
-		contract.DeployBenchmarkToken(ctx, 5)
+			contract := network.BenchmarkTokenContract()
+			contract.DeployBenchmarkToken(ctx, 5)
 
-		// leader is nodeIndex 0, validator is nodeIndex 1
+			// leader is nodeIndex 0, validator is nodeIndex 1
 
-		pausedTxForwards := network.TransportTamperer().Pause(testkit.TransactionRelayMessage(gossipmessages.TRANSACTION_RELAY_FORWARDED_TRANSACTIONS))
-		txHash := contract.TransferInBackground(ctx, 1, 17, 5, 6)
+			pausedTxForwards := network.TransportTamperer().Pause(testkit.TransactionRelayMessage(gossipmessages.TRANSACTION_RELAY_FORWARDED_TRANSACTIONS))
+			txHash := contract.TransferInBackground(ctx, 1, 17, 5, 6)
 
-		if err := network.BlockPersistence(0).GetBlockTracker().WaitForBlock(ctx, 2); err != nil {
-			t.Errorf("failed waiting for block on node 0: %s", err)
-		}
-		if err := network.BlockPersistence(1).GetBlockTracker().WaitForBlock(ctx, 2); err != nil {
-			t.Errorf("failed waiting for block on node 1: %s", err)
-		}
+			if err := network.BlockPersistence(0).GetBlockTracker().WaitForBlock(ctx, 2); err != nil {
+				t.Errorf("failed waiting for block on node 0: %s", err)
+			}
+			if err := network.BlockPersistence(1).GetBlockTracker().WaitForBlock(ctx, 2); err != nil {
+				t.Errorf("failed waiting for block on node 1: %s", err)
+			}
 
-		requireBalanceInNodeEventually(ctx, t, contract, 0, 6, 0, "expected to read initial getBalance result on leader")
-		requireBalanceInNodeEventually(ctx, t, contract, 0, 6, 1, "expected to read initial getBalance result on non-leader")
+			requireBalanceInNodeEventually(ctx, t, contract, 0, 6, 0, "expected to read initial getBalance result on leader")
+			requireBalanceInNodeEventually(ctx, t, contract, 0, 6, 1, "expected to read initial getBalance result on non-leader")
 
-		pausedTxForwards.Release(ctx)
-		network.WaitForTransactionInNodeState(ctx, txHash, 0)
-		require.EqualValues(t, 17, contract.GetBalance(ctx, 0, 6), "eventual getBalance result on leader")
-		network.WaitForTransactionInNodeState(ctx, txHash, 1)
-		require.EqualValues(t, 17, contract.GetBalance(ctx, 1, 6), "eventual getBalance result on non leader")
-	})
+			pausedTxForwards.Release(ctx)
+			network.WaitForTransactionInNodeState(ctx, txHash, 0)
+			require.EqualValues(t, 17, contract.GetBalance(ctx, 0, 6), "eventual getBalance result on leader")
+			network.WaitForTransactionInNodeState(ctx, txHash, 1)
+			require.EqualValues(t, 17, contract.GetBalance(ctx, 1, 6), "eventual getBalance result on non leader")
+		})
 }
 
 func requireBalanceInNodeEventually(ctx context.Context, t *testing.T, contract callcontract.BenchmarkTokenClient, expectedBalance uint64, forAddressIndex int, nodeIndex int, msgAndArguments ...interface{}) {
