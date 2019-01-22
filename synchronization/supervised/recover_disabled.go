239daf4734573cb339628627a544a5497efd4b01
Shai Yallin <shai.yallin@gmail.com>
2019-01-22 15:25:22 +0200

    when norecover tag is on, exit the system forcefully so that we can discover dying goroutines

--- synchronization/supervised/recover_disabled.go
+++ synchronization/supervised/recover_disabled.go
@@ -2,6 +2,17 @@
 
 package supervised
 
+import (
+	"github.com/orbs-network/orbs-network-go/instrumentation/log"
+	"github.com/pkg/errors"
+	"os"
+	"runtime/debug"
+)
+
 func recoverPanics(logger Errorer) {
-	// do nothing, let it panic
+	if p := recover(); p != nil {
+		e := errors.Errorf("goroutine panicked at [%s]: %v", identifyPanic(), p)
+		logger.Error("Fatal error", log.Error(e), log.String("stack-trace", string(debug.Stack())))
+		os.Exit(42) // because otherwise the system might be a zombie with partial functionality
+	}
 }
