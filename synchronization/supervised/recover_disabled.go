b8b70b0b51959cbd5cc3b11de8f88d1de442e5dc
Shai Yallin <shai.yallin@gmail.com>
2019-01-23 08:59:48 +0200

    Revert "when norecover tag is on, exit the system forcefully so that we can discover dying goroutines"
    This reverts commit 239daf4734573cb339628627a544a5497efd4b01.

--- synchronization/supervised/recover_disabled.go
+++ synchronization/supervised/recover_disabled.go
@@ -2,17 +2,6 @@
 
 package supervised
 
-import (
-	"github.com/orbs-network/orbs-network-go/instrumentation/log"
-	"github.com/pkg/errors"
-	"os"
-	"runtime/debug"
-)
-
 func recoverPanics(logger Errorer) {
-	if p := recover(); p != nil {
-		e := errors.Errorf("goroutine panicked at [%s]: %v", identifyPanic(), p)
-		logger.Error("Fatal error", log.Error(e), log.String("stack-trace", string(debug.Stack())))
-		os.Exit(42) // because otherwise the system might be a zombie with partial functionality
-	}
+	// do nothing, let it panic
 }
