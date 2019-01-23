422fde292bdb788be2bb4df1d637dded132fc863
Shai Yallin <shai.yallin@gmail.com>
2019-01-23 10:49:29 +0200

    restoring log printing in tests other than the basic full suite

--- test.common.sh
+++ test.common.sh
@@ -5,6 +5,19 @@ mkdir -p _out
 check_exit_code_and_report () {
     export EXIT_CODE=$?
 
+    if [ $EXIT_CODE != 0 ]; then
+        grep -B 150 -A 15 -- "FAIL:" ./_out/test.out > ./_out/fail.out
+        cat ./_out/fail.out
+
+        grep -B 150 -A 150 -- "test timed out" ./_out/test.out > ./_out/timed.out
+        cat ./_out/timed.out
+
+        if [ ! -s ./_out/fail.out ] && [ ! -s ./_out/timed.out ]; then
+            cat ./_out/test.out
+        fi
+
+    fi
+
     # copy full log for further investigation
     mkdir -p ./_logs
     cp ./_out/*.out ./_logs
