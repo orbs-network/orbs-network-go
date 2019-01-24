682d3096883b66d84d98cef6f904347f51550612
Ron <ron@orbs.com>
2019-01-24 19:37:53 +0200

    implement WaitForTransaction using BlockTracker, extract logic to txWaiter

--- services/blockstorage/adapter/testkit/tampering_persistence.go
+++ services/blockstorage/adapter/testkit/tampering_persistence.go
@@ -3,22 +3,13 @@ package testkit
 import (
 	"context"
 	"errors"
-	"fmt"
-	"github.com/orbs-network/orbs-network-go/crypto/digest"
 	"github.com/orbs-network/orbs-network-go/instrumentation/log"
 	"github.com/orbs-network/orbs-network-go/instrumentation/metric"
 	"github.com/orbs-network/orbs-network-go/services/blockstorage/adapter"
 	"github.com/orbs-network/orbs-network-go/services/blockstorage/adapter/memory"
-	"github.com/orbs-network/orbs-network-go/test"
 	"github.com/orbs-network/orbs-spec/types/go/primitives"
 	"github.com/orbs-network/orbs-spec/types/go/protocol"
-	"sync"
 )
-
-type blockHeightChan struct {
-	c chan struct{}
-	h primitives.BlockHeight
-}
 
 type TamperingInMemoryBlockPersistence interface {
 	adapter.BlockPersistence
@@ -28,38 +19,21 @@ type TamperingInMemoryBlockPersistence interface {
 
 func NewBlockPersistence(parent log.BasicLogger, preloadedBlocks []*protocol.BlockPairContainer, metricFactory metric.Factory) *tamperingBlockPersistence {
 	logger := parent.WithTags(log.String("adapter", "block-storage"))
-	p := &tamperingBlockPersistence{
+	return &tamperingBlockPersistence{
 		InMemoryBlockPersistence: *memory.NewBlockPersistence(logger, metricFactory, preloadedBlocks...),
+		txRegistry:               newTxWaiter(logger, preloadedBlocks),
 	}
-
-	p.txToBlockHeightChan.channels = make(map[string]*blockHeightChan)
-	for _, bpc := range preloadedBlocks {
-		p.advertiseAllTransactions(bpc.TransactionsBlock)
-	}
-
-	return p
 }
 
 type tamperingBlockPersistence struct {
 	memory.InMemoryBlockPersistence
 	failNextBlocks bool
 
-	txToBlockHeightChan struct {
-		sync.Mutex
-		channels map[string]*blockHeightChan
-	}
+	txRegistry *txWaiter
 }
 
 func (bp *tamperingBlockPersistence) WaitForTransaction(ctx context.Context, txHash primitives.Sha256) primitives.BlockHeight {
-	bhc := bp.getChanForTxHash(txHash)
-
-	select {
-	case <-bhc.c: // when c closes, h will already be written
-		return bhc.h
-	case <-ctx.Done():
-		test.DebugPrintGoroutineStacks() // since test timed out, help find deadlocked goroutines
-		panic(fmt.Sprintf("timed out waiting for transaction with hash %s", txHash))
-	}
+	return bp.txRegistry.waitForTransaction(ctx, txHash)
 }
 
 func (bp *tamperingBlockPersistence) FailNextBlocks() {
@@ -79,54 +53,5 @@ func (bp *tamperingBlockPersistence) WriteNextBlock(blockPair *protocol.BlockPai
 }
 
 func (bp *tamperingBlockPersistence) advertiseAllTransactions(block *protocol.TransactionsBlockContainer) {
-	bp.txToBlockHeightChan.Lock()
-	defer bp.txToBlockHeightChan.Unlock()
-
-	height := block.Header.BlockHeight()
-	if height == 0 {
-		panic("illegal block h 0")
-	}
-
-	for _, tx := range block.SignedTransactions {
-		txHash := digest.CalcTxHash(tx.Transaction())
-
-		bhc := bp.getChanForTxHashUnlocked(txHash)
-
-		if bhc.h != 0 { // previously advertised
-			checkForConflicts(bhc.h, height, bp.Logger, txHash)
-			continue
-		}
-
-		bhc.h = height // first
-		close(bhc.c)   // second
-		bp.Logger.Info("advertising transaction completion", log.Transaction(txHash), log.BlockHeight(height))
-	}
-}
-
-func checkForConflicts(prevHeight primitives.BlockHeight, newHeight primitives.BlockHeight, logger log.BasicLogger, txHash primitives.Sha256) {
-	if prevHeight != newHeight {
-		logger.Error("FORK/DOUBLE-SPEND!!!! same transaction reported in different heights. may be committed twice", log.Transaction(txHash), log.BlockHeight(newHeight), log.Uint64("previously-reported-height", uint64(prevHeight)))
-		panic(fmt.Sprintf("FORK/DOUBLE-SPEND!!!! transaction %s previously advertised for height %d and now again for height %d. may be committed twice", txHash.String(), prevHeight, newHeight))
-	}
-	logger.Info("advertising transaction completion aborted - already advertised", log.Transaction(txHash), log.BlockHeight(newHeight))
-}
-
-func (bp *tamperingBlockPersistence) getChanForTxHash(txHash primitives.Sha256) *blockHeightChan {
-	bp.txToBlockHeightChan.Lock()
-	defer bp.txToBlockHeightChan.Unlock()
-
-	bhc := bp.getChanForTxHashUnlocked(txHash)
-
-	return bhc
-}
-
-func (bp *tamperingBlockPersistence) getChanForTxHashUnlocked(txHash primitives.Sha256) *blockHeightChan {
-	bhc := bp.txToBlockHeightChan.channels[txHash.KeyForMap()]
-	if bhc == nil {
-		bhc = &blockHeightChan{
-			c: make(chan struct{}),
-		}
-		bp.txToBlockHeightChan.channels[txHash.KeyForMap()] = bhc
-	}
-	return bhc
+	bp.txRegistry.advertiseTransactions(block.Header.BlockHeight(), block.SignedTransactions)
 }
---
+++ services/blockstorage/adapter/testkit/tx_waiter.go
@@ -0,0 +1,104 @@
+package testkit
+
+import (
+	"context"
+	"fmt"
+	"github.com/orbs-network/orbs-network-go/crypto/digest"
+	"github.com/orbs-network/orbs-network-go/instrumentation/log"
+	"github.com/orbs-network/orbs-network-go/instrumentation/trace"
+	"github.com/orbs-network/orbs-network-go/synchronization"
+	"github.com/orbs-network/orbs-network-go/test"
+	"github.com/orbs-network/orbs-spec/types/go/primitives"
+	"github.com/orbs-network/orbs-spec/types/go/protocol"
+	"math"
+	"sync"
+)
+
+type txWaiter struct {
+	sync.Mutex
+	txToHeight     map[string]primitives.BlockHeight
+	topKnownHeight primitives.BlockHeight
+	idxTracker     *synchronization.BlockTracker
+	parent         log.BasicLogger
+}
+
+func newTxWaiter(logger log.BasicLogger, preloadedBlocks []*protocol.BlockPairContainer) *txWaiter {
+	registry := &txWaiter{
+		Mutex:      sync.Mutex{},
+		txToHeight: make(map[string]primitives.BlockHeight),
+		idxTracker: synchronization.NewBlockTracker(logger, 0, math.MaxUint16),
+		parent:     logger,
+	}
+
+	for _, bpc := range preloadedBlocks {
+		registry.advertiseTransactions(bpc.TransactionsBlock.Header.BlockHeight(), bpc.TransactionsBlock.SignedTransactions)
+	}
+
+	return registry
+}
+
+func (txi *txWaiter) getBlockHeight(txHash primitives.Sha256) (primitives.BlockHeight, primitives.BlockHeight) {
+	txi.Lock()
+	defer txi.Unlock()
+
+	return txi.txToHeight[txHash.KeyForMap()], txi.topKnownHeight
+}
+
+func (txi *txWaiter) advertiseTransactions(height primitives.BlockHeight, transactions []*protocol.SignedTransaction) {
+	if height == 0 {
+		panic("illegal block height 0")
+	}
+
+	txi.Lock()
+	defer txi.Unlock()
+
+	if height <= txi.topKnownHeight { // block already advertised
+		txi.parent.Info("advertising block transactions aborted - already advertised", log.BlockHeight(height))
+		return
+	}
+
+	for _, tx := range transactions {
+		txHash := digest.CalcTxHash(tx.Transaction())
+
+		prevHeight, existed := txi.txToHeight[txHash.KeyForMap()]
+
+		if existed {
+			assertSameHeight(prevHeight, height, txi.parent, txHash)
+			panic(fmt.Sprintf("BUG!! txWaiter.txToHeight contains a block height ahead of topKnownHeight. tx %s found listed for height %d. but topKnownHeight is %d", txHash.String(), height, txi.topKnownHeight))
+		}
+
+		txi.txToHeight[txHash.KeyForMap()] = height
+		txi.parent.Info("advertising transaction completion", log.Transaction(txHash), log.BlockHeight(height))
+	}
+	txi.parent.Info("advertising block transactions done", log.BlockHeight(height))
+
+	txi.idxTracker.IncrementTo(height)
+	txi.topKnownHeight = height
+}
+
+func (txi *txWaiter) waitForTransaction(ctx context.Context, txHash primitives.Sha256) primitives.BlockHeight {
+	logger := txi.parent.WithTags(trace.LogFieldFrom(ctx))
+	logger.Info("waiting for transaction", log.Transaction(txHash))
+	for {
+		txHeight, topHeight := txi.getBlockHeight(txHash)
+
+		if txHeight > 0 { // found requested height
+			logger.Info("transaction found in block", log.Transaction(txHash), log.BlockHeight(txHeight))
+			return txHeight
+		}
+
+		logger.Info("transaction not found as of block", log.Transaction(txHash), log.BlockHeight(topHeight))
+		err := txi.idxTracker.WaitForBlock(ctx, topHeight+1) // wait for next block
+		if err != nil {
+			test.DebugPrintGoroutineStacks() // since test timed out, help find deadlocked goroutines
+			panic(fmt.Sprintf("timed out waiting for transaction with hash %s", txHash))
+		}
+	}
+}
+
+func assertSameHeight(prevHeight primitives.BlockHeight, newHeight primitives.BlockHeight, logger log.BasicLogger, txHash primitives.Sha256) {
+	if prevHeight != newHeight {
+		logger.Error("FORK/DOUBLE-SPEND!!!! same transaction reported in different heights. may be committed twice", log.Transaction(txHash), log.BlockHeight(newHeight), log.Uint64("previously-reported-height", uint64(prevHeight)))
+		panic(fmt.Sprintf("FORK/DOUBLE-SPEND!!!! transaction %s previously advertised for height %d and now again for height %d. may be committed twice", txHash.String(), prevHeight, newHeight))
+	}
+}
