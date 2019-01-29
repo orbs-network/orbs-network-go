fb22819526ac8a1c92fe3cd2bbaea6ca7e4c7715
Kirill Maksimov <kirill@orbs.com>
2019-01-29 12:44:23 +0200

    Refactor CI

--- .circleci/gopath.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh -x
-
-mkdir -p /go/src/github.com/orbs-network/
-mv ~/project/ /go/src/github.com/orbs-network/orbs-network-go
-
-mkdir -p /go/src/github.com/orbs-network/orbs-network-go/_out
-
-go get -u github.com/jstemmer/go-junit-report
