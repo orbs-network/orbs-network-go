091fa68a52ad3dfe63b40580a298663b6b40a0f6
Shai Yallin <shai.yallin@gmail.com>
2019-01-30 09:33:01 +0200

    bump block tracker timeout to try to mitigate flakiness

--- config/service_factory_presets.go
+++ config/service_factory_presets.go
@@ -67,7 +67,7 @@ func ForTransactionPoolTests(sizeLimit uint32, keyPair *testKeys.TestEcdsaSecp25
 	cfg.SetNodePrivateKey(keyPair.PrivateKey())
 
 	cfg.SetUint32(VIRTUAL_CHAIN_ID, 42)
-	cfg.SetDuration(BLOCK_TRACKER_GRACE_TIMEOUT, 100*time.Millisecond)
+	cfg.SetDuration(BLOCK_TRACKER_GRACE_TIMEOUT, 500*time.Millisecond)
 	cfg.SetUint32(BLOCK_TRACKER_GRACE_DISTANCE, 5)
 	cfg.SetUint32(TRANSACTION_POOL_PENDING_POOL_SIZE_IN_BYTES, sizeLimit)
 	cfg.SetDuration(TRANSACTION_POOL_NODE_SYNC_REJECT_TIME, 2*time.Minute)
