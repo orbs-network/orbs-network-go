37786661a6cc40b3d6c3cdc22df66003cc825c0a
Shai Yallin <shai.yallin@gmail.com>
2019-01-31 15:17:18 +0200

    attempting to get circleci to notice flakiness test results

--- test.flakiness.sh
+++ test.flakiness.sh
@@ -2,12 +2,12 @@
 
 . ./test.common.sh
 
-go_test_junit_report flakiness/acceptance ./test/acceptance -count 100 -timeout 20m -failfast -tags "cpunoise norecover"
+go_test_junit_report acceptance ./test/acceptance -count 100 -timeout 20m -failfast -tags "cpunoise norecover"
 
-go_test_junit_report flakiness/block_storage ./services/blockstorage/test -count 100 -timeout 7m -failfast -tags "cpunoise norecover"
+go_test_junit_report blockstorage ./services/blockstorage/test -count 100 -timeout 7m -failfast -tags "cpunoise norecover"
 
-go_test_junit_report flakiness/internodesync ./services/blockstorage/internodesync -count 100 -timeout 7m -failfast -tags "cpunoise norecover"
+go_test_junit_report internodesync ./services/blockstorage/internodesync -count 100 -timeout 7m -failfast -tags "cpunoise norecover"
 
-go_test_junit_report flakiness/servicesync ./services/blockstorage/servicesync -count 100 -timeout 7m -failfast -tags -tags "cpunoise norecover"
+go_test_junit_report servicesync ./services/blockstorage/servicesync -count 100 -timeout 7m -failfast -tags -tags "cpunoise norecover"
 
-go_test_junit_report flakiness/transaction_pool ./services/transactionpool/test -count 100 -timeout 7m -failfast -tags -tags "cpunoise norecover"
+go_test_junit_report transactionpool ./services/transactionpool/test -count 100 -timeout 7m -failfast -tags -tags "cpunoise norecover"
