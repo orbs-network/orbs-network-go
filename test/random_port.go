6b2f17705e7ad1f5b4e1bc806a5a2bb705d08764
Tal Kol <talkol@gmail.com>
2019-02-04 20:49:13 +0200

    Improved port randomization to reduce collisions

--- test/random_port.go
+++ test/random_port.go
@@ -2,13 +2,26 @@ package test
 
 import (
 	"math/rand"
+	"sync"
 	"time"
 )
 
-// Do not use this. Instead, in your code, pass 0 as the TCP port and then retrieve the port for client usage:
-// 		listener, err := net.Listen("tcp", "127.0.0.1:0")
-//		port := listener.Addr().(*net.TCPAddr).Port
-func RandomPort_UnsafeDoNotUseMe_I_Am_Going_Away() int {
-	src := rand.NewSource(time.Now().UnixNano())
-	return ((rand.New(src).Intn(25000) / 10) * 10) + 25111
+var mutex = sync.Mutex{}
+var lastPort = 0
+
+// Random ports encourage flakiness. Instead, when possible, pass 0 as the TCP port and then retrieve the port for client usage:
+//  listener, err := net.Listen("tcp", "127.0.0.1:0")
+//  port := listener.Addr().(*net.TCPAddr).Port
+func RandomPort() int {
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if lastPort == 0 {
+		src := rand.NewSource(time.Now().UnixNano())
+		lastPort = rand.New(src).Intn(30000) + 25111
+	} else {
+		lastPort++
+	}
+
+	return lastPort
 }
