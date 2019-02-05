578ddf176d9ac471485c3eb2d62c55e6e83ab57b
Ido Zilberberg <idoz@orbs.com>
2019-02-05 10:48:58 +0200

    Added regression test for extraction of block proof from block

--- services/consensusalgo/leanhelixconsensus/test/block_proof_serialization_test.go
+++ services/consensusalgo/leanhelixconsensus/test/block_proof_serialization_test.go
@@ -16,3 +16,11 @@ func TestBlockProofSerialization(t *testing.T) {
 	actualBlockProof := leanhelixconsensus.ExtractBlockProof(block)
 	require.True(t, bytes.Equal(expectedBlockProof, actualBlockProof), "block proofs should be the same: expectedBlockProof=%v actualBlockProof=%v", expectedBlockProof, actualBlockProof)
 }
+
+func TestBlockProofSerialization_IncorrectExtraction(t *testing.T) {
+	expectedBlockProof := lhbuilders.AMockBlockProof().Raw()
+	block := builders.BlockPair().Build()
+	block.TransactionsBlock.BlockProof = leanhelixconsensus.CreateTransactionBlockProof(block, expectedBlockProof)
+	incorrectBlockProof := block.TransactionsBlock.BlockProof.Raw()
+	require.False(t, bytes.Equal(expectedBlockProof, incorrectBlockProof), "block proofs should be the different")
+}
