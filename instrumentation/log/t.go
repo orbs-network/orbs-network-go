2ea72a27265e156f41a9b40b9205e66b04b6ffde
Ido Zilberberg <idoz@orbs.com>
2019-02-06 17:18:45 +0200

    Rearranged lines in t.go so the log line is on line 9 and no later

--- instrumentation/log/t.go
+++ instrumentation/log/t.go
@@ -2,19 +2,17 @@ package log
 
 import "time"
 
-// this file is part of test_output.go
-// a file with short name (t.go) to make the testLogger prefix less annoying
-
 func (o *testOutput) Append(level string, message string, fields ...*Field) {
-	// we use this mechanism to stop logging new log lines after the test failed from a different goroutine
 	if o.stopLogging {
 		return
 	}
-
-	logLine := o.formatter.FormatRow(time.Now(), level, message, fields...)
-	o.tb.Log(logLine)
+	o.tb.Log(o.formatter.FormatRow(time.Now(), level, message, fields...))
+	// we use this mechanism to stop logging new log lines after the test failed from a different goroutine
 }
 
 func (o *testOutput) StopLogging() {
 	o.stopLogging = true
 }
+
+// this file is part of test_output.go
+// a file with short name (t.go) to make the testLogger prefix less annoying; also we keep o.tb.Log() on a single-digit line for one less char
