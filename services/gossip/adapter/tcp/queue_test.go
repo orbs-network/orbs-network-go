69e7130fc362ada3507211948a06b892b0679bca
Tal Kol <talkol@gmail.com>
2019-02-11 15:16:47 +0200

    Improved test.WithContext instead of context.Background()

--- services/gossip/adapter/tcp/queue_test.go
+++ services/gossip/adapter/tcp/queue_test.go
@@ -3,69 +3,73 @@ package tcp
 import (
 	"context"
 	"github.com/orbs-network/orbs-network-go/services/gossip/adapter"
+	"github.com/orbs-network/orbs-network-go/test"
 	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
 )
 
 func TestQueue_PushAndPopMultiple(t *testing.T) {
-	ctx := context.Background()
-	q := NewTransportQueue(1000, 1000)
+	test.WithContext(func(ctx context.Context) {
+		q := NewTransportQueue(1000, 1000)
 
-	err := q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x01}})
-	require.NoError(t, err)
+		err := q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x01}})
+		require.NoError(t, err)
 
-	err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x02}})
-	require.NoError(t, err)
+		err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x02}})
+		require.NoError(t, err)
 
-	d1 := q.Pop(ctx)
-	require.EqualValues(t, []byte{0x01}, d1.SenderNodeAddress)
+		d1 := q.Pop(ctx)
+		require.EqualValues(t, []byte{0x01}, d1.SenderNodeAddress)
 
-	err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x03}})
-	require.NoError(t, err)
+		err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x03}})
+		require.NoError(t, err)
 
-	d2 := q.Pop(ctx)
-	require.EqualValues(t, []byte{0x02}, d2.SenderNodeAddress)
+		d2 := q.Pop(ctx)
+		require.EqualValues(t, []byte{0x02}, d2.SenderNodeAddress)
 
-	d3 := q.Pop(ctx)
-	require.EqualValues(t, []byte{0x03}, d3.SenderNodeAddress)
+		d3 := q.Pop(ctx)
+		require.EqualValues(t, []byte{0x03}, d3.SenderNodeAddress)
+	})
 }
 
 func TestQueue_CannotPushMoreThanMaxMessages(t *testing.T) {
-	ctx := context.Background()
-	q := NewTransportQueue(1000, 2)
+	test.WithContext(func(ctx context.Context) {
+		q := NewTransportQueue(1000, 2)
 
-	err := q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x01}})
-	require.NoError(t, err)
+		err := q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x01}})
+		require.NoError(t, err)
 
-	err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x02}})
-	require.NoError(t, err)
+		err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x02}})
+		require.NoError(t, err)
 
-	err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x03}})
-	require.Error(t, err, "queue should be full")
+		err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x03}})
+		require.Error(t, err, "queue should be full")
 
-	err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x04}})
-	require.Error(t, err, "queue should be full")
+		err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x04}})
+		require.Error(t, err, "queue should be full")
 
-	d1 := q.Pop(ctx)
-	require.EqualValues(t, []byte{0x01}, d1.SenderNodeAddress)
+		d1 := q.Pop(ctx)
+		require.EqualValues(t, []byte{0x01}, d1.SenderNodeAddress)
 
-	err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x03}})
-	require.NoError(t, err)
+		err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x03}})
+		require.NoError(t, err)
+	})
 }
 
 func TestQueue_PopWhenEmptyWaitsUntilPush(t *testing.T) {
-	ctx := context.Background()
-	q := NewTransportQueue(1000, 1000)
+	test.WithContext(func(ctx context.Context) {
+		q := NewTransportQueue(1000, 1000)
 
-	go func() {
-		time.Sleep(10 * time.Millisecond)
-		err := q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x01}})
-		require.NoError(t, err)
-	}()
+		go func() {
+			time.Sleep(10 * time.Millisecond)
+			err := q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x01}})
+			require.NoError(t, err)
+		}()
 
-	d1 := q.Pop(ctx)
-	require.EqualValues(t, []byte{0x01}, d1.SenderNodeAddress)
+		d1 := q.Pop(ctx)
+		require.EqualValues(t, []byte{0x01}, d1.SenderNodeAddress)
+	})
 }
 
 func TestQueue_PopWhenEmptyCancelsWithContext(t *testing.T) {
@@ -82,26 +86,27 @@ func TestQueue_PopWhenEmptyCancelsWithContext(t *testing.T) {
 }
 
 func TestQueue_CannotPushMoreThanMaxBytes(t *testing.T) {
-	ctx := context.Background()
-	q := NewTransportQueue(10, 1000)
+	test.WithContext(func(ctx context.Context) {
+		q := NewTransportQueue(10, 1000)
 
-	err := q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x01}, Payloads: [][]byte{buf(3), buf(4)}})
-	require.NoError(t, err)
+		err := q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x01}, Payloads: [][]byte{buf(3), buf(4)}})
+		require.NoError(t, err)
 
-	err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x02}, Payloads: [][]byte{buf(1), buf(6)}})
-	require.Error(t, err, "queue should be full")
+		err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x02}, Payloads: [][]byte{buf(1), buf(6)}})
+		require.Error(t, err, "queue should be full")
 
-	err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x03}, Payloads: [][]byte{buf(4)}})
-	require.Error(t, err, "queue should be full")
+		err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x03}, Payloads: [][]byte{buf(4)}})
+		require.Error(t, err, "queue should be full")
 
-	err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x04}, Payloads: [][]byte{buf(3)}})
-	require.NoError(t, err)
+		err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x04}, Payloads: [][]byte{buf(3)}})
+		require.NoError(t, err)
 
-	d1 := q.Pop(ctx)
-	require.EqualValues(t, []byte{0x01}, d1.SenderNodeAddress)
+		d1 := q.Pop(ctx)
+		require.EqualValues(t, []byte{0x01}, d1.SenderNodeAddress)
 
-	err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x05}, Payloads: [][]byte{buf(1), buf(6)}})
-	require.NoError(t, err)
+		err = q.Push(&adapter.TransportData{SenderNodeAddress: []byte{0x05}, Payloads: [][]byte{buf(1), buf(6)}})
+		require.NoError(t, err)
+	})
 }
 
 func TestQueue_DirectPopFromChannel(t *testing.T) {
