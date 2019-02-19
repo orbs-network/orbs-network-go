dba81c5c336adb0f1eb7b1800d895ad8aff24d58
itamar Arjuan <itamar.arjuan@gmail.com>
2019-02-19 12:46:32 +0200

    commit messages including hashtag extraflaky will run the flakiness step for acceptance x10 and with no fail fast option

--- test.flakiness.sh
+++ test.flakiness.sh
@@ -1,9 +1,20 @@
 #!/bin/bash -x
 
 . ./test.common.sh
+LAST_COMMIT_MESSAGE=`git --no-pager log --decorate=short --pretty=oneline -n1 $CIRCLE_SHA1`
+EXTRA_FLAKY_FAILFAST="-failfast"
+EXTRA_FLAKY_COUNT=35
+
+if [[ "${LAST_COMMIT_MESSAGE}" == *"#extraflaky"* ]]; then
+    EXTRA_FLAKY_FAILFAST=""
+    EXTRA_FLAKY_COUNT=350
+fi
 
 if [ "$CIRCLE_NODE_INDEX" == 0 ] || [ "$CIRCLE_NODE_INDEX" == 1 ] || [ "$CIRCLE_NODE_INDEX" == 2 ] || [ "$CIRCLE_NODE_INDEX" == 3 ] || [ -z "$CIRCLE_NODE_INDEX" ]; then
-    go_test_junit_report acceptance ./test/acceptance -count 35 -timeout 20m -failfast -tags "norecover"
+    if [[ "${LAST_COMMIT_MESSAGE}" == *"#extraflaky"* ]]; then
+        echo "Running in extra flakiness mode (x10 count) and no fail fast, (take a seat this might take a while)"
+    fi
+    go_test_junit_report acceptance ./test/acceptance -count $EXTRA_FLAKY_COUNT -timeout 20m $EXTRA_FLAKY_FAILFAST -tags "norecover"
 fi
 
 if [ "$CIRCLE_NODE_INDEX" == 4 ] || [ "$CIRCLE_NODE_INDEX" == 5 ] || [ -z "$CIRCLE_NODE_INDEX" ]; then
