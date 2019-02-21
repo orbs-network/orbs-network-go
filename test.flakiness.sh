4dda350f2b449e1be97a3edb0ccf4c0a9322f96c
itamar Arjuan <itamar.arjuan@gmail.com>
2019-02-21 12:59:29 +0200

    changes for PR

--- test.flakiness.sh
+++ test.flakiness.sh
@@ -3,33 +3,39 @@
 . ./test.common.sh
 
 LAST_COMMIT_MESSAGE=`git --no-pager log --decorate=short --pretty=oneline -n1 $CIRCLE_SHA1`
-EXTRA_FLAKY_FAILFAST="-failfast"
-EXTRA_FLAKY_COUNT_ACCEPTANCE=35
-EXTRA_FLAKY_COUNT_REST=70
+FAILFAST="-failfast"
+TIMEOUT_ACCEPTANCE="20m"
+TIMEOUT_REST="10m"
+COUNT_ACCEPTANCE=35
+COUNT_REST=70
 
 if [[ "${LAST_COMMIT_MESSAGE}" == *"#extraflaky"* ]]; then
-    EXTRA_FLAKY_FAILFAST=""
-    EXTRA_FLAKY_COUNT_ACCEPTANCE=350
-    EXTRA_FLAKY_COUNT_REST=700
+    FAILFAST=""
+    TIMEOUT_ACCEPTANCE="500m"
+    TIMEOUT_REST="500m"
+    COUNT_ACCEPTANCE=350
+    COUNT_REST=700
 fi
 
 if [[ $1 == "NIGHTLY" ]]; then
     echo "performing nightly build (count 1000/2000 , no failfast)"
-    EXTRA_FLAKY_FAILFAST=""
-    EXTRA_FLAKY_COUNT_ACCEPTANCE=1000
-    EXTRA_FLAKY_COUNT_REST=2000
+    FAILFAST=""
+    TIMEOUT_ACCEPTANCE="500m"
+    TIMEOUT_REST="500m"
+    COUNT_ACCEPTANCE=1000
+    COUNT_REST=2000
 fi
 
 if [ "$CIRCLE_NODE_INDEX" == 0 ] || [ "$CIRCLE_NODE_INDEX" == 1 ] || [ "$CIRCLE_NODE_INDEX" == 2 ] || [ "$CIRCLE_NODE_INDEX" == 3 ] || [ -z "$CIRCLE_NODE_INDEX" ]; then
-    go_test_junit_report acceptance ./test/acceptance -count $EXTRA_FLAKY_COUNT_ACCEPTANCE -timeout 20m $EXTRA_FLAKY_FAILFAST -tags "norecover"
+    go_test_junit_report acceptance ./test/acceptance -count $COUNT_ACCEPTANCE -timeout $TIMEOUT_ACCEPTANCE $FAILFAST -tags "norecover"
 fi
 
 if [ "$CIRCLE_NODE_INDEX" == 4 ] || [ "$CIRCLE_NODE_INDEX" == 5 ] || [ -z "$CIRCLE_NODE_INDEX" ]; then
-    go_test_junit_report blockstorage ./services/blockstorage/test -count $EXTRA_FLAKY_COUNT_ACCEPTANCE -timeout 10m $EXTRA_FLAKY_FAILFAST -tags "norecover"
+    go_test_junit_report blockstorage ./services/blockstorage/test -count $COUNT_ACCEPTANCE -timeout $TIMEOUT_REST $FAILFAST -tags "norecover"
 
-    go_test_junit_report internodesync ./services/blockstorage/internodesync -count $EXTRA_FLAKY_COUNT_ACCEPTANCE -timeout 7m $EXTRA_FLAKY_FAILFAST -tags "norecover"
+    go_test_junit_report internodesync ./services/blockstorage/internodesync -count $COUNT_ACCEPTANCE -timeout $TIMEOUT_REST $FAILFAST -tags "norecover"
 
-    go_test_junit_report servicesync ./services/blockstorage/servicesync -count $EXTRA_FLAKY_COUNT_ACCEPTANCE -timeout 7m $EXTRA_FLAKY_FAILFAST -tags -tags "norecover"
+    go_test_junit_report servicesync ./services/blockstorage/servicesync -count $COUNT_ACCEPTANCE -timeout $TIMEOUT_REST $FAILFAST -tags -tags "norecover"
 
-    go_test_junit_report transactionpool ./services/transactionpool/test -count $EXTRA_FLAKY_COUNT_ACCEPTANCE -timeout 7m $EXTRA_FLAKY_FAILFAST -tags -tags "norecover"
+    go_test_junit_report transactionpool ./services/transactionpool/test -count $COUNT_ACCEPTANCE -timeout $TIMEOUT_REST $FAILFAST -tags -tags "norecover"
 fi
