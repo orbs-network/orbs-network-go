91e79b72963631790d4c8108b79bb878000714fb
Tal Kol <talkol@gmail.com>
2019-03-05 23:29:53 +0200

    Made txPool.addCommitLock more fine grained because it ruined performance

--- services/transactionpool/commit_transaction_receipts.go
+++ services/transactionpool/commit_transaction_receipts.go
@@ -21,14 +21,14 @@ func (s *service) CommitTransactionReceipts(ctx context.Context, input *services
 		}, nil
 	}
 
-	s.addCommitLock.Lock()
-	defer s.addCommitLock.Unlock()
-
 	newBh, ts := s.updateBlockHeightAndTimestamp(input.ResultsBlockHeader) //TODO(v1): should this be updated separately from blockTracker? are we updating block height too early?
 
 	c := &committer{logger: logger, adder: s.committedPool, remover: s.pendingPool, nodeAddress: s.config.NodeAddress(), blockHeight: newBh, blockTime: ts}
 
+	// TODO(https://github.com/orbs-network/orbs-network-go/issues/1020): improve addCommitLock workaround
+	s.addCommitLock.Lock()
 	c.commit(ctx, input.TransactionReceipts...)
+	s.addCommitLock.Unlock()
 
 	s.blockTracker.IncrementTo(newBh)
 	s.blockHeightReporter.IncrementTo(newBh)
