1b7f02cd912e9fb1e621605814d1a928b2b53749
itamar Arjuan <itamar.arjuan@gmail.com>
2019-03-06 16:05:36 +0200

    re-adding the unsafe tests flag

--- docker/build/build.sh
+++ docker/build/build.sh
@@ -5,7 +5,7 @@ export SEMVER=$(cat ./.version)
 LAST_COMMIT_MESSAGE=`git --no-pager log --decorate=short --pretty=oneline -n1 $CIRCLE_SHA1`
 
 BUILD_FLAG=""
-if [[ "${LAST_COMMIT_MESSAGE}" == *"unsafe"* ]]; then
+if [[ "${LAST_COMMIT_MESSAGE}" == *"#unsafetests"* ]]; then
     BUILD_FLAG="unsafetests"
 fi
 
