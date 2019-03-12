ad6905e8f41f1c08acd58d3ef742330f5f98b94a
Shai Yallin <shai.yallin@gmail.com>
2019-03-12 15:07:08 +0200

    Update config.go

--- services/processor/native/adapter/config.go
+++ services/processor/native/adapter/config.go
@@ -5,7 +5,7 @@ import "time"
 const SOURCE_CODE_PATH = "native-src"
 const SHARED_OBJECT_PATH = "native-bin"
 const GC_CACHE_PATH = "native-cache"
-const MAX_COMPILATION_TIME = 5 * time.Second
+const MAX_COMPILATION_TIME = 10 * time.Second
 const MAX_WARM_UP_COMPILATION_TIME = 15 * time.Second
 
 type Config interface {
