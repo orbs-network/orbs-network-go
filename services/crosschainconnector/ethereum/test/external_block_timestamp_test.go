1f0221efbae35417c880772f46cdaa58cb729ae1
Jonathan Levison <jonathanl@orbs.com>
2019-03-13 20:47:06 +0200

    changed full flow in call contract to be finality-proof

--- services/crosschainconnector/ethereum/test/external_block_timestamp_test.go
+++ services/crosschainconnector/ethereum/test/external_block_timestamp_test.go
@@ -14,11 +14,31 @@ import (
 	"time"
 )
 
+func moveToRealtimeInGanache(t *testing.T, h *harness, ctx context.Context, buffer int) {
+	latestBlockInGanache, err := h.rpcAdapter.HeaderByNumber(ctx, nil)
+	require.NoError(t, err, "failed to get latest block in ganache")
+
+	now := time.Now().Unix()
+	gap := now - latestBlockInGanache.Time.Int64() - int64(buffer)
+	require.True(t, gap >= 0, "ganache must be set up back enough to the past so finality test would pass in this flow, it was rolled too close to realtime, gap was %d", gap)
+	t.Logf("moving %d blocks into the future to get to now - %d seconds", gap, buffer)
+	h.moveBlocksInGanache(t, int(gap), 1)
+}
+
 //TODO this test does not deal well with gaps in ganache, meaning that a ganache that has been standing idle for more than a few seconds will fail this test while
 // trying to assert that a contract cannot be called before it has been deployed
 func TestFullFlowWithVaryingTimestamps(t *testing.T) {
 	// the idea of this test is to make sure that the entire 'call-from-ethereum' logic works on a specific timestamp and different states in time (blocks)
 	// it requires ganache or some other RPC backend to transact
+
+	// 1. it **assumes** that ganache started in the past, and is not 'realtime' - it does not care how long but in order
+	//    for it to work it must be greater than the finality value
+	// 2. next it will write a block (to support working on a clean ganache)
+	// 3. it will then find out how much it needs to fast forward to get to realtime, and will write a block per second to fill the gap (using evm_increaseTime, evm_mine)
+	// 4. it does that to realtime - finality - some gap so we deploy our contract enough time in the past
+	// 5. it will then deploy the contract
+	// 6. it will then run to actual realtime
+	// 6. it will attempt to call the contract at realtime, and then call it at the time it did not exists expecting an error
 
 	if !runningWithDocker() {
 		t.Skip("this test relies on external components - ganache, and will be skipped unless running in docker")
@@ -26,29 +46,54 @@ func TestFullFlowWithVaryingTimestamps(t *testing.T) {
 
 	test.WithContext(func(ctx context.Context) {
 		h := newRpcEthereumConnectorHarness(t, ConfigForExternalRPCConnection())
+		finalityComponent := uint32(h.config.finalityTimeComponent.Seconds()) + h.config.finalityBlocksComponent
+
+		// create first block in case we are running only this test (clean ganache, but no real hard in actual)
+		h.moveBlocksInGanache(t, 1, 1)
 		latestBlockInGanache, err := h.rpcAdapter.HeaderByNumber(ctx, nil)
 		require.NoError(t, err, "failed to get latest block in ganache")
+		t.Logf("starting point in ganache is %d | %d", latestBlockInGanache.Number.Int64(), latestBlockInGanache.Time.Int64())
 
+		// time now, gap to ganache time, catch up
+		buffer := finalityComponent + 10 // +10 gap as described above
+		moveToRealtimeInGanache(t, h, ctx, int(buffer))
+
+		latestBlockInGanache, err = h.rpcAdapter.HeaderByNumber(ctx, nil)
+		require.NoError(t, err, "failed to get latest block in ganache")
+		t.Logf("time and block -%d secs %d | %d", finalityComponent+10, latestBlockInGanache.Number.Int64(), latestBlockInGanache.Time.Int64())
 		timeBeforeContractWasDeployed := time.Unix(latestBlockInGanache.Time.Int64(), 0)
-		h.moveBlocksInGanache(t, 2, 1) // this is only to advance blocks
 
+		// deploy takes a while, over a sec, so there is no need to sleep here, the next block will be created okay,
+		// it might take even two secs, which will break our 'relatime' catchup,
+		// but its okay to have ganache one or two secs into the future (finality will cover for us)
 		expectedTextFromEthereum := "test3"
-		contractAddress3, err := h.deployRpcStorageContract(expectedTextFromEthereum)
-		require.NoError(t, err, "failed deploying contract3 to Ethereum")
+		// ========== this will advance one block as well for the delpoy
+		contractAddress, err := h.deployRpcStorageContract(expectedTextFromEthereum)
+		require.NoError(t, err, "failed deploying contract to Ethereum")
 
-		h.moveBlocksInGanache(t, 11, 1) // this is only to advance blocks
+		latestBlockInGanacheAfterDeploy, err := h.rpcAdapter.HeaderByNumber(ctx, nil)
+		require.NoError(t, err, "failed to get latest block in ganache after deploy")
+		t.Logf("block in ganache when deployed %d | %d", latestBlockInGanacheAfterDeploy.Number.Int64(), latestBlockInGanacheAfterDeploy.Time.Int64())
+
+		// pushing to 'realtime'
+		moveToRealtimeInGanache(t, h, ctx, 0)
+
+		actualBlockInGanache, err := h.rpcAdapter.HeaderByNumber(ctx, nil)
+		require.NoError(t, err, "failed to get latest block in ganache")
+		t.Logf("time and block in actual %d | %d, system time %d", actualBlockInGanache.Number.Int64(), actualBlockInGanache.Time.Int64(), time.Now().Unix())
+
+		// =============== test starts here
 
 		methodToCall := "getValues"
-
 		parsedABI, err := abi.JSON(strings.NewReader(contract.SimpleStorageABI))
 		require.NoError(t, err, "abi parse failed for simple storage contract")
 
 		ethCallData, err := ethereum.ABIPackFunctionInputArguments(parsedABI, methodToCall, nil)
 		require.NoError(t, err, "this means we couldn't pack the params for ethereum, something is broken with the harness")
 
+		// request at time now, which should be (with finality) after the contract was deployed
 		input := builders.EthereumCallContractInput().
-			WithTimestamp(timeBeforeContractWasDeployed.Add(14 * time.Second)).
-			WithContractAddress(contractAddress3).
+			WithContractAddress(contractAddress).
 			WithAbi(contract.SimpleStorageABI).
 			WithFunctionName(methodToCall).
 			WithPackedArguments(ethCallData).
@@ -67,7 +112,7 @@ func TestFullFlowWithVaryingTimestamps(t *testing.T) {
 
 		input = builders.EthereumCallContractInput().
 			WithTimestamp(timeBeforeContractWasDeployed).
-			WithContractAddress(contractAddress3).
+			WithContractAddress(contractAddress).
 			WithAbi(contract.SimpleStorageABI).
 			WithFunctionName(methodToCall).
 			WithPackedArguments(ethCallData).
