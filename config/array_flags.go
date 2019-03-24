708dbb6c015f6ae649224b9afe0db70f146f46a4
Tal Kol <talkol@gmail.com>
2019-03-24 14:03:23 +0200

    Added license files

--- config/array_flags.go
+++ config/array_flags.go
@@ -1,3 +1,9 @@
+// Copyright 2019 the orbs-network-go authors
+// This file is part of the orbs-network-go library in the Orbs project.
+//
+// This source code is licensed under the MIT license found in the LICENSE file in the root directory of this source tree.
+// The above notice should be included in all copies or substantial portions of the software.
+
 package config
 
 type ArrayFlags []string
