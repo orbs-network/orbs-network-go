5569bed446d2ea5003f8a9445c2e21ea230dcdd1
Ido Zilberberg <idoz@orbs.com>
2019-04-10 17:15:57 +0300

    Added "node" label to Prometheus metrics

--- instrumentation/metric/prometheus_test.go
+++ instrumentation/metric/prometheus_test.go
@@ -1,6 +1,8 @@
 package metric
 
 import (
+	"encoding/hex"
+	"github.com/orbs-network/orbs-spec/types/go/primitives"
 	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
@@ -24,12 +26,13 @@ func Test_PrometheusFormatterForGauge(t *testing.T) {
 }
 
 func Test_PrometheusFormatterForGaugeWithParams(t *testing.T) {
-	r := NewRegistry().WithVirtualChainId(100000)
+	bytes, _ := hex.DecodeString("0123456789abcdef")
+	r := NewRegistry().WithVirtualChainId(100000).WithNodeAddress(primitives.NodeAddress(bytes))
 	status := r.NewGauge("Ethereum.Node.LastBlock")
 	status.Update(5123441)
 
 	resultWithParams := r.ExportPrometheus()
-	require.Regexp(t, "Ethereum_Node_LastBlock{vcid=\"100000\"} 5123441", resultWithParams)
+	require.Regexp(t, "Ethereum_Node_LastBlock{vcid=\"100000\",node=\"0123456789abcdef\"} 5123441", resultWithParams)
 }
 
 func Test_PrometheusFormatterForHistogram(t *testing.T) {
@@ -46,7 +49,7 @@ func Test_PrometheusFormatterForHistogram(t *testing.T) {
 
 	status.RecordSince(time.Now())
 	updatedResult := r.ExportPrometheus()
-
+	t.Log(result)
 	require.Regexp(t, "Some_Latency{quantile=\"0.01\"} 0.00", updatedResult)
 	require.Regexp(t, "Some_Latency{quantile=\"0.5\"} 0.00", updatedResult)
 	require.Regexp(t, "Some_Latency{quantile=\"0.99\"} 0.00", updatedResult)
