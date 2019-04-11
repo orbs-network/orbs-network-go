2cc5a08268ad438c22f40a85ffdbcfef74f87784
Noam Berg <noamberg@yahoo.com>
2019-04-11 16:28:35 +0300

    new fix rewards

--- services/processor/native/repository/_Elections/exports_unsafetests.go
+++ services/processor/native/repository/_Elections/exports_unsafetests.go
@@ -24,6 +24,7 @@ var PUBLIC = sdk.Export(getTokenEthereumContractAddress, getGuardiansEthereumCon
 	getElectedValidatorsOrbsAddressByIndex, getElectedValidatorsEthereumAddressByIndex, getElectedValidatorsBlockNumberByIndex, getElectedValidatorsBlockHeightByIndex,
 	getCumulativeParticipationReward, getCumulativeGuardianExcellenceReward, getCumulativeValidatorReward,
 	getGuardianStake, getGuardianVotingWeight, getTotalStake, getValidatorStake, getValidatorVote, getExcellenceProgramGuardians,
+	_firstElectionFixRewards, _firstElectionFixRewardsDelegator,
 )
 var SYSTEM = sdk.Export(_init)
 
