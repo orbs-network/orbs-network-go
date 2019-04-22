9b980bb7c2668de6db465cd8e565abe511cc154a
Kirill Maksimov <kirill@orbs.com>
2019-04-22 11:17:15 +0300

    Addressed comments

--- bootstrap/gamma/deploy_native_test.go
+++ bootstrap/gamma/deploy_native_test.go
@@ -27,7 +27,7 @@ func testDeployNativeContractWithConfig(jsonConfig string) func(t *testing.T) {
 
 			t.Log("deploying contract")
 
-			contract.DeployNativeCounterContract(ctx, 1, 0) // leader is nodeIndex 0, validator is nodeIndex 1
+			contract.DeployNativeCounterContract(ctx, 1, 0) // for benchmark: leader is nodeIndex 0, validator is nodeIndex 1
 
 			require.True(t, test.Eventually(3*time.Second, func() bool {
 				return counterStart == contract.CounterGet(ctx, 0)
