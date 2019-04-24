b237cff4888737991179bdeb80f4853162303297
Shai Yallin <shai.yallin@gmail.com>
2019-04-24 11:54:59 +0300

    reproduced bug where multiple blocks are closed irregardless of block time after a single transaction arrives

--- test/acceptance/empty_block_time_test.go
+++ test/acceptance/empty_block_time_test.go
@@ -3,22 +3,24 @@ package acceptance
 import (
 	"context"
 	"github.com/orbs-network/orbs-network-go/test/acceptance/callcontract"
-	"github.com/orbs-network/orbs-spec/types/go/protocol/consensus"
-	"github.com/orbs-network/scribe/log"
+	"github.com/stretchr/testify/require"
 	"testing"
 	"time"
 )
 
-func TestIncomingTransactionTriggersImmediateBlockClosure(t *testing.T) {
+func TestIncomingTransactionTriggersExactlyOneBlock(t *testing.T) {
 	newHarness().
-		WithEmptyBlockTime(1*time.Hour).
-		WithConsensusAlgos(consensus.CONSENSUS_ALGO_TYPE_LEAN_HELIX).
-		WithLogFilters(log.ExcludeEntryPoint("BlockSync")).
+		WithEmptyBlockTime(1*time.Second).
 		Start(t, func(tb testing.TB, ctx context.Context, network *NetworkHarness) {
 			contract := callcontract.NewContractClient(network)
-			time.Sleep(5 * time.Second)
+			time.Sleep(100 * time.Millisecond)
+			heightBeforeTx, _ := network.BlockPersistence(0).GetLastBlockHeight()
+
 			_, txHash := contract.Transfer(ctx, 0, 43, 5, 6)
 			network.WaitForTransactionInState(ctx, txHash)
 
+			heightAfterTx, _ := network.BlockPersistence(0).GetLastBlockHeight()
+
+			require.InDelta(t, uint64(heightAfterTx), uint64(heightBeforeTx), 1.0, "incoming transaction triggered closure of more than one block")
 		})
 }
