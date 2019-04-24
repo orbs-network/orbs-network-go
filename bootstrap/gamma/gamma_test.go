f0ea1f8a60f1c61281196a9fc52dd69f08e017c8
Kirill Maksimov <kirill@orbs.com>
2019-04-24 16:21:56 +0300

    Add more tests for Gamma

--- bootstrap/gamma/gamma_test.go
+++ bootstrap/gamma/gamma_test.go
@@ -7,27 +7,57 @@
 package gamma
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/orbs-network/orbs-client-sdk-go/codec"
 	orbsClient "github.com/orbs-network/orbs-client-sdk-go/orbs"
 	"github.com/orbs-network/orbs-network-go/test"
+	"github.com/orbs-network/orbs-spec/types/go/primitives"
 	"github.com/stretchr/testify/require"
+	"io/ioutil"
+	"net/http"
 	"testing"
 	"time"
 )
 
 const LEAN_HELIX_CONSENSUS_JSON = `{"active-consensus-algo":2}`
 
+type metrics map[string]map[string]interface{}
+
+func waitForBlock(endpoint string, targetBlockHeight primitives.BlockHeight) func() bool {
+	return func() bool {
+		res, err := http.Get(endpoint + "/metrics")
+		if err != nil {
+			fmt.Println(err)
+			return false
+		}
+
+		readBytes, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println(err)
+			return false
+		}
+		m := make(metrics)
+		json.Unmarshal(readBytes, &m)
+
+		blockHeight := m["BlockStorage.BlockHeight"]["Value"].(float64)
+		return primitives.BlockHeight(blockHeight) >= targetBlockHeight
+	}
+}
+
 func testGammaWithJSONConfig(configJSON string) func(t *testing.T) {
 	return func(t *testing.T) {
 		randomPort := test.RandomPort()
-		endpoint := fmt.Sprintf("0.0.0.0:%d", randomPort)
-		gammaServer := StartGammaServer(endpoint, false, configJSON, false)
+		serverAddress := fmt.Sprintf("0.0.0.0:%d", randomPort)
+		endpoint := fmt.Sprintf("http://%s", serverAddress)
+		gammaServer := StartGammaServer(serverAddress, false, configJSON, false)
 		defer gammaServer.GracefulShutdown(1 * time.Second)
+
+		require.True(t, test.Eventually(3*time.Second, waitForBlock(endpoint, 1)))
 
 		sender, _ := orbsClient.CreateAccount()
 		transferTo, _ := orbsClient.CreateAccount()
-		client := orbsClient.NewClient(fmt.Sprintf("http://%s", endpoint), 42, codec.NETWORK_TYPE_TEST_NET)
+		client := orbsClient.NewClient(endpoint, 42, codec.NETWORK_TYPE_TEST_NET)
 
 		payload, _, err := client.CreateTransaction(sender.PublicKey, sender.PrivateKey, "BenchmarkToken", "transfer", uint64(671), transferTo.AddressAsBytes())
 		require.NoError(t, err)
@@ -35,6 +65,19 @@ func testGammaWithJSONConfig(configJSON string) func(t *testing.T) {
 		require.NoError(t, err)
 
 		require.Equal(t, codec.EXECUTION_RESULT_SUCCESS, response.ExecutionResult)
+		require.True(t, test.Eventually(3*time.Second, waitForBlock(endpoint, 2)))
+	}
+}
+
+func testGammaWithEmptyBlocks(configJSON string) func(t *testing.T) {
+	return func(t *testing.T) {
+		randomPort := test.RandomPort()
+		serverAddress := fmt.Sprintf("0.0.0.0:%d", randomPort)
+		endpoint := fmt.Sprintf("http://%s", serverAddress)
+		gammaServer := StartGammaServer(serverAddress, false, configJSON, false)
+		defer gammaServer.GracefulShutdown(1 * time.Second)
+
+		require.True(t, test.Eventually(3*time.Second, waitForBlock(endpoint, 5)))
 	}
 }
 
@@ -45,4 +88,13 @@ func TestGamma(t *testing.T) {
 
 	t.Run("Benchmark", testGammaWithJSONConfig(""))
 	t.Run("LeanHelix", testGammaWithJSONConfig(LEAN_HELIX_CONSENSUS_JSON))
+}
+
+func TestGammaWithEmptyBlocks(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping E2E tests in short mode")
+	}
+
+	t.Run("Benchmark", testGammaWithEmptyBlocks(`{"transaction-pool-time-between-empty-blocks":"200ms"}`))
+	t.Run("LeanHelix", testGammaWithEmptyBlocks(`{"active-consensus-algo":2,"transaction-pool-time-between-empty-blocks":"200ms"}`))
 }
