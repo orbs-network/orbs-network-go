8818a006965b9418f3993dca72f77a1970a53d44
itamar Arjuan <itamar.arjuan@gmail.com>
2019-05-19 17:17:51 +0300

    making changes resulting from the review of @netoneko

--- .circleci/check-testnet-deployment.js
+++ .circleci/check-testnet-deployment.js
@@ -1,9 +1,8 @@
 #!/usr/bin/env node
 
 const { waitUntilSync, getBlockHeight, getCommit } = require('@orbs-network/orbs-nebula/lib/metrics');
-const fetch = require('node-fetch');
 
-const TOPOLOGY = 'https://s3.us-west-2.amazonaws.com/boyar-testnet-bootstrap/boyar/config.json';
+const topology = require('./config.json');
 const TARGET_HASH = process.argv[2];
 
 if (!TARGET_HASH) {
@@ -11,18 +10,12 @@ if (!TARGET_HASH) {
     process.exit(1);
 }
 
-async function getTopology() {
-    const result = await fetch(TOPOLOGY);
-    const topology = await result.json();
-    return topology;
-}
-
 async function eventuallyDeployed({ chainId, nodes }) {
     // First let's poll the nodes for the correct version
     // Until we meet the correct version on all nodes or we bail by the timeout defined
     let versionDeployed = false;
     let runsCount = 0;
-    const maxRetries = 10; // 5 minutes
+    const maxRetries = 30; // 15 minutes
 
     do {
         if (runsCount + 1 > maxRetries) {
@@ -63,7 +56,7 @@ async function eventuallyClosingBlocks({ chainId, nodes }) {
     const currentBlockheight = await getBlockHeight(firstEndpoint);
 
     try {
-        await waitUntilSync(firstEndpoint, currentBlockheight + 50);
+        await waitUntilSync(firstEndpoint, currentBlockheight + 10);
 
         return {
             ok: true,
@@ -76,7 +69,6 @@ async function eventuallyClosingBlocks({ chainId, nodes }) {
 }
 
 (async () => {
-    const topology = await getTopology();
     const nodes = topology.network;
     const chains = topology.chains.map(chain => chain.Id);
 
@@ -84,7 +76,7 @@ async function eventuallyClosingBlocks({ chainId, nodes }) {
     if (results.filter(r => r.ok === true).length === chains.length) {
         console.log('New version deployed successfully on all chains in the testnet');
     } else {
-        console.error('New version was not deployed on all nodes within the defined 5 minutes window, quiting..');
+        console.error('New version was not deployed on all nodes within the defined 15 minutes window, quiting..');
         process.exit(2);
     }
 
@@ -93,7 +85,7 @@ async function eventuallyClosingBlocks({ chainId, nodes }) {
         console.log('Blocks are being closed on all chains in the testnet!');
         process.exit(0);
     } else {
-        console.error('Not all chains are closing blocks after the new version was deployed within the defined 5 minutes window, quiting..')
+        console.error('Not all chains are closing blocks after the new version was deployed within the defined 15 minutes window, quiting..');
         process.exit(3);
     }
 })();
