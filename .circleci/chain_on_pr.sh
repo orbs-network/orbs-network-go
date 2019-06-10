34b85cb6d110dd639fee20065e12b610f12521dc
itamar Arjuan <itamar.arjuan@gmail.com>
2019-06-10 10:34:11 +0200

    trying to troubleshoot permission denied problem with aws s3 cp

--- .circleci/chain_on_pr.sh
+++ .circleci/chain_on_pr.sh
@@ -1,4 +1,15 @@
-#!/bin/bash -xe
+#!/bin/bash
+
+# Installing aws cli
+echo "Installing AWS CLI"
+
+mkdir -p ~/.aws
+echo "[default]
+aws_access_key_id=$AWS_ACCESS_KEY_ID
+aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" > ~/.aws/credentials
+
+echo "[profile default]
+region = us-east-1" > ~/.aws/config
 
 # Installing aws cli
 echo "Installing AWS CLI"
