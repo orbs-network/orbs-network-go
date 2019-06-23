e4418a12efc097151fe6cd8c04641096c658c639
Kirill Maksimov <kirill@orbs.com>
2019-06-23 13:22:38 +0300

    Pull stats from the first node

--- test/acceptance/stress_benchmark_test.go
+++ test/acceptance/stress_benchmark_test.go
@@ -2,6 +2,7 @@ package acceptance
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/orbs-network/orbs-network-go/config"
 	"github.com/orbs-network/orbs-network-go/test/rand"
@@ -25,13 +26,63 @@ func BenchmarkHappyFlow1k(b *testing.B) {
 	})
 }
 
+type EssentialMetricsHistogram struct {
+	Min     float64
+	P50     float64
+	P95     float64
+	P99     float64
+	Max     float64
+	Avg     float64
+	Samples int64
+}
+
+type EssentialMetrics struct {
+	BlockHeight              uint64
+	TxTimeSpentInQueueMillis EssentialMetricsHistogram
+	TxCommitRatePerSecond    float64
+}
+
+func parseEssentialMetricsHistogram(value map[string]interface{}) EssentialMetricsHistogram {
+	return EssentialMetricsHistogram{
+		Min:     value["Min"].(float64),
+		Max:     value["Max"].(float64),
+		P50:     value["P50"].(float64),
+		P95:     value["P95"].(float64),
+		P99:     value["P99"].(float64),
+		Avg:     value["Avg"].(float64),
+		Samples: int64(value["Samples"].(float64)),
+	}
+}
+
+func parseEssentialMetrics(info interface{}) EssentialMetrics {
+	raw, _ := json.Marshal(info)
+	m := make(map[string]map[string]interface{})
+	json.Unmarshal(raw, &m)
+
+	e := EssentialMetrics{}
+
+	for key, value := range m {
+		switch key {
+		case "BlockStorage.BlockHeight":
+			e.BlockHeight = uint64(value["Value"].(float64))
+		case "TransactionPool.PendingPool.TimeSpentInQueue.Millis":
+			e.TxTimeSpentInQueueMillis = parseEssentialMetricsHistogram(value)
+		case "TransactionPool.CommitRate.PerSecond":
+			e.TxCommitRatePerSecond = value["Rate"].(float64)
+		}
+	}
+
+	return e
+}
+
 func BenchmarkHappyFlow1kWithOverrides(b *testing.B) {
 	numTransactions := 1000
 
 	rnd := rand.NewControlledRand(b)
 	newHarness().WithConfigOverride(func(cfg config.OverridableConfig) config.OverridableConfig {
 		c, err := cfg.MergeWithFileConfig(`{
-	"transaction-pool-propagation-batch-size": 1000
+	"consensus-context-maximum-transactions-in-block": 70,
+	"transaction-pool-propagation-batch-size": 100
 }`)
 		require.NoError(b, err)
 		return c
@@ -43,5 +94,8 @@ func BenchmarkHappyFlow1kWithOverrides(b *testing.B) {
 			fmt.Println("finished sending ", numTransactions, "transactions in", transferDuration)
 			fmt.Println("finished waiting for", numTransactions, "transactions in", waitDuration)
 		}
+
+		e := parseEssentialMetrics(network.MetricRegistry(0).ExportAll())
+		fmt.Println("stats", e)
 	})
 }
