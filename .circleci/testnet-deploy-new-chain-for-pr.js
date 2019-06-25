640379fdcd0c7340db96240902ece4cd9c966487
itamar Arjuan <itamar.arjuan@gmail.com>
2019-06-25 12:24:19 +0300

    adding different ports

--- .circleci/testnet-deploy-new-chain-for-pr.js
+++ .circleci/testnet-deploy-new-chain-for-pr.js
@@ -33,11 +33,12 @@ if (!targetTag) {
 
 // The namespace 100000 is PR chains teritory
 const prLinkParts = githubPRLink.split('/');
-const prNumber = parseInt(prLinkParts[prLinkParts.length - 1]) + 100000;
+const prNumber = parseInt(prLinkParts[prLinkParts.length - 1]);
+const chainNumber = prNumber + 100000;
 
 const configuration = require(configFilePath);
 
-const chainIndex = configuration.chains.findIndex(chain => chain.Id === prNumber);
+const chainIndex = configuration.chains.findIndex(chain => chain.Id === chainNumber);
 
 if (chainIndex !== -1) {
     // This means we already have a chain in the config, let's just update it's version ref
@@ -47,15 +48,16 @@ if (chainIndex !== -1) {
 
     // Clone the last chain and make modifications on top of it.
     const newChain = Object.assign({}, lastChain);
+    const basePort = 9000;
     newChain.DockerConfig.Tag = targetTag;
-    newChain.Id = prNumber;
-    newChain.HttpPort = newChain.HttpPort + 2;
-    newChain.GossipPort = newChain.GossipPort + 2;
+    newChain.Id = chainNumber;
+    newChain.HttpPort = basePort + prNumber;
+    newChain.GossipPort = basePort + prNumber + 2;
 
     configuration.chains.push(newChain);
 }
 
 fs.writeFileSync(configFilePath, JSON.stringify(configuration, 2, 2));
 
-console.log(prNumber);
+console.log(chainNumber);
 process.exit(0);
