096741d877b05797d410bec109b3dde3ce8185be
Kirill Maksimov <kirill@orbs.com>
2019-07-15 12:20:17 +0300

    Disable cache so e2e that runs consequitively in docker does not rely on it

--- test.adapters.sh
+++ test.adapters.sh
@@ -1,13 +1,13 @@
 #!/bin/sh -xe
 
 for pkg in $(find . -type d -name e2e | grep -v vendor); do
-    time go test $pkg -v
+    time go test $pkg -v -count 1
 done
 
 for pkg in $(find . -type d -name adapter | grep -v vendor); do
-    time go test $pkg -v
+    time go test $pkg -v -count 1
 done
 
 for pkg in $(find . -type d -name ethereum | grep -v vendor); do
-    time go test "$pkg/..." -v
+    time go test "$pkg/..." -v -count 1
 done
