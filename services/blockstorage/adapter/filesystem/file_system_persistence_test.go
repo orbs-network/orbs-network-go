7bad9a57cbbd0d24a09a67056619ed64606bff88
Ron <ron@orbs.com>
2019-07-19 19:59:13 +0300

    mark filesystem accessing unit tests as integration tests for exclusion in fast testing. use temp files instead of exec file for test: using the binary file breaks in CI

--- services/blockstorage/adapter/filesystem/file_system_persistence_test.go
+++ services/blockstorage/adapter/filesystem/file_system_persistence_test.go
@@ -2,29 +2,48 @@ package filesystem
 
 import (
 	"github.com/stretchr/testify/require"
+	"io/ioutil"
 	"os"
 	"testing"
 )
 
 func TestGetFileSizeGoodFile(t *testing.T) {
-	ex, err := os.Executable()
-	require.NoError(t, err, "os function failed")
-	file, err := os.OpenFile(ex, os.O_RDWR, 0600)
-	require.NoError(t, err, "should be able to read the current executing file size")
+	if testing.Short() {
+		t.Skip("Skipping integration tests")
+	}
+
+	file, err := ioutil.TempFile("", "")
+	require.NoError(t, err, "failed to create a temp file")
+	defer func() { _ = os.Remove(file.Name()) }()
 
 	size, err := getBlockFileSize(file)
 
-	require.NoError(t, err, "should be able to read the current executing file size")
-	require.Condition(t, func() bool { return size > 0 }, "size should be read okay for the current executing file")
+	require.NoError(t, err, "should be able to read file size of an open file")
+	require.EqualValues(t, 0, size, "size should be read okay for the current executing file")
+
+	// write some data
+	data := []byte{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	n, err := file.Write(data)
+	require.NoError(t, err, "failed to write some bytes to temp file")
+	require.EqualValues(t, len(data), n, "expected written size to equal bytes array size")
+
+	size, err = getBlockFileSize(file)
+	require.NoError(t, err, "should be able to read file size of an open file")
+	require.EqualValues(t, n, size, "file size does not match the number of bytes written to it")
 }
 
 func TestGetFileSizeBadFile(t *testing.T) {
-	ex, err := os.Executable()
-	require.NoError(t, err, "os function failed")
-	file, err := os.OpenFile(ex, os.O_RDWR, 0600)
-	require.NoError(t, err, "should be able to read the current executing file size")
+	if testing.Short() {
+		t.Skip("Skipping integration tests")
+	}
 
-	file.Close()
+	file, err := ioutil.TempFile("", "")
+	require.NoError(t, err, "failed to create a temp file")
+	defer func() { _ = os.Remove(file.Name()) }()
+
+	err = file.Close()
+	require.NoError(t, err, "expected closing of the temp file to succeed")
+
 	_, errOfReadSize := getBlockFileSize(file)
 
 	require.Error(t, errOfReadSize, "should not able to read size of closed file handle")
