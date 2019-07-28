5ddb5e3a8de732159e1e7fd6f47030917f788ddc
itamar Arjuan <itamar.arjuan@gmail.com>
2019-07-28 17:58:13 +0300

    adding changes to the ci pipeline

--- test.sh
+++ test.sh
@@ -1,16 +1,7 @@
-#!/bin/bash -x
+#!/bin/bash -e
 
 ulimit -S -n 20000
 
 . ./test.common.sh
 
 time go_test_junit_report standard -tags "unsafetests" -timeout 10m ./... -failfast
-
-# this test must run separately since zero parallel package tests are allowed concurrently
-. ./test.goroutine-leaks.sh
-
-# this test must run separately since zero parallel package tests are allowed concurrently
-. ./test.memory-leaks.sh
-
-# uncomment to run component tests
-# ./test.components.sh
