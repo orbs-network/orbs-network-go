9a21f279cf7d10be790e861f109ee4c29135afd9
Shai Yallin <shai.yallin@gmail.com>
2019-07-29 12:19:01 +0300

    added test for notifying on config change

--- config/file_config.go
+++ config/file_config.go
@@ -7,9 +7,16 @@
 package config
 
 import (
+	"context"
+	"github.com/orbs-network/orbs-network-go/crypto/hash"
+	"github.com/orbs-network/orbs-network-go/synchronization"
+	"github.com/orbs-network/orbs-spec/types/go/primitives"
+	"github.com/orbs-network/scribe/log"
 	"github.com/pkg/errors"
 	"io/ioutil"
 	"os"
+	"sync"
+	"time"
 )
 
 type ArrayFlags []string
@@ -23,12 +30,19 @@ func (i *ArrayFlags) Set(value string) error {
 	return nil
 }
 
+type configChangeHandler func(newConfig *MapBasedConfig)
+
 type Loader struct {
-	files []string
+	sync.Mutex
+	files              []string
+	handlers           []configChangeHandler
+	contentsForPolling map[string]primitives.Sha256
 }
 
 func NewLoader(configFiles ...string) *Loader {
-	return &Loader{files: configFiles}
+	l := &Loader{files: configFiles}
+	l.contentsForPolling = make(map[string]primitives.Sha256)
+	return l
 }
 
 func (l *Loader) Load() (*MapBasedConfig, error) {
@@ -36,13 +50,15 @@ func (l *Loader) Load() (*MapBasedConfig, error) {
 
 	for _, configFile := range l.files {
 		if _, err := os.Stat(configFile); os.IsNotExist(err) {
-			return nil, errors.Errorf("could not open config file: %s", err)
+			return nil, errors.Wrapf(err, "could not open config file: %s", configFile)
 		}
 
 		contents, err := ioutil.ReadFile(configFile)
 		if err != nil {
-			return nil, err
+			return nil, errors.Wrapf(err, "could not read config file: %s", configFile)
 		}
+
+		l.storeContents(configFile, contents)
 
 		cfg, err = cfg.MergeWithJSONConfig(string(contents))
 
@@ -52,4 +68,64 @@ func (l *Loader) Load() (*MapBasedConfig, error) {
 	}
 
 	return cfg, nil
+}
+
+func (l *Loader) OnConfigChanged(handler configChangeHandler) {
+	l.Lock()
+	defer l.Unlock()
+	l.handlers = append(l.handlers, handler)
+}
+
+func (l *Loader) ListenForChanges(ctx context.Context, logger log.Logger) {
+	synchronization.NewPeriodicalTrigger(ctx, 100*time.Millisecond, logger, func() {
+		if err := l.pollForChangesAndMaybeNotify(); err != nil {
+			logger.Error("failed polling for config changes", log.Error(err))
+		}
+	}, func() {
+		// do nothing on purpose
+	})
+}
+
+func (l *Loader) pollForChangesAndMaybeNotify() error {
+	for _, configFile := range l.files {
+		if _, err := os.Stat(configFile); os.IsNotExist(err) {
+			return errors.Wrapf(err, "could not open config file: %s", configFile)
+		}
+
+		newContents, err := ioutil.ReadFile(configFile)
+		if err != nil {
+			return errors.Wrapf(err, "could not read config file: %s", configFile)
+		}
+
+		if l.hasChanged(configFile, newContents) {
+			if newCfg, err := l.Load(); err != nil {
+				return err
+			} else {
+				l.notifyHandlers(newCfg)
+			}
+		}
+	}
+
+	return nil
+}
+
+func (l *Loader) notifyHandlers(newConfig *MapBasedConfig) {
+	l.Lock()
+	defer l.Unlock()
+	for i := range l.handlers {
+		handler := l.handlers[i]
+		handler(newConfig)
+	}
+}
+
+func (l *Loader) hasChanged(fileName string, newContents []byte) bool {
+	l.Lock()
+	defer l.Unlock()
+	return !l.contentsForPolling[fileName].Equal(hash.CalcSha256(newContents))
+}
+
+func (l *Loader) storeContents(fileName string, newContent []byte) {
+	l.Lock()
+	defer l.Unlock()
+	l.contentsForPolling[fileName] = hash.CalcSha256(newContent)
 }
--- config/file_config_test.go
+++ config/file_config_test.go
@@ -1,9 +1,13 @@
 package config
 
 import (
+	"context"
 	"github.com/orbs-network/orbs-network-go/test/files"
+	"github.com/orbs-network/scribe/log"
 	"github.com/stretchr/testify/require"
+	"io/ioutil"
 	"testing"
+	"time"
 )
 
 func TestNewFromMultipleFiles_HandlesEmptyFileList(t *testing.T) {
@@ -41,4 +45,44 @@ func TestNewFromMultipleFiles_ForE2E(t *testing.T) {
 	require.NoError(t, err, "failed parsing config file")
 
 	require.EqualValues(t, "a328846cd5b4979d68a8c58a9bdfeee657b34de7", cfg.NodeAddress().String())
+}
+
+func TestConfigLoader_CallsReconfigureOnFileChange(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	f1 := files.NewTempFileWithContent(t, `{"a":"b"}`)
+	defer files.RemoveSilently(f1)
+	f2 := files.NewTempFileWithContent(t, `{"c":"d"}`)
+	defer files.RemoveSilently(f2)
+
+	loader := NewLoader(f1, f2)
+
+	ch := make(chan *MapBasedConfig)
+	loader.OnConfigChanged(func(newConfig *MapBasedConfig) {
+		ch <- newConfig
+	})
+
+	// load so that we initialize our state
+	_, err := loader.Load()
+	require.NoError(t, err, "failed initial loading of config file")
+
+	loader.ListenForChanges(ctx, log.DefaultTestingLogger(t))
+
+	require.NoError(t, ioutil.WriteFile(f1, []byte(`{"a":"b1"}`), 0644))
+	select {
+	case <-ctx.Done():
+		t.Errorf("config change event not triggered")
+	case cfg := <-ch:
+		require.Equal(t, "b1", cfg.kv["A"].StringValue, "config change not reflected in new config")
+	}
+
+	require.NoError(t, ioutil.WriteFile(f2, []byte(`{"c":"d1"}`), 0644))
+	select {
+	case <-ctx.Done():
+		t.Errorf("config change event not triggered")
+	case cfg := <-ch:
+		require.Equal(t, "b1", cfg.kv["A"].StringValue, "config change not reflected in new config")
+		require.Equal(t, "d1", cfg.kv["C"].StringValue, "config change not reflected in new config")
+	}
 }
