ce11f45439924287f29d14cfcdc106f110462054
itamar Arjuan <itamar.arjuan@gmail.com>
2019-07-29 15:33:03 +0300

    add script to install a newer go version

--- .circleci/install-go.sh
+++ .circleci/install-go.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 wget https://dl.google.com/go/go1.12.6.linux-amd64.tar.gz
-sudo tar -xvf go1.12.6.linux-amd64.tar.gz
+sudo tar -xf go1.12.6.linux-amd64.tar.gz
 sudo mv go /usr/local
 go version
