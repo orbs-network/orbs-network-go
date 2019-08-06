a7321cc5449213649f4040e648b78800ae1bd265
itamar Arjuan <itamar.arjuan@gmail.com>
2019-08-06 14:55:18 +0300

    install a newer version of Go to support strings.Builder

--- .circleci/install-go.sh
+++ .circleci/install-go.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -e
 
+GO_VERSION="1.12.7"
+
 sudo rm -rf /usr/local/go
-wget https://dl.google.com/go/go1.12.7.linux-amd64.tar.gz
-sudo tar -xf go1.12.6.linux-amd64.tar.gz
+wget "https://dl.google.com/go/go${GO_VERSION}.linux-amd64.tar.gz"
+sudo tar -xf "go${GO_VERSION}.linux-amd64.tar.gz"
 sudo mv go /usr/local/
 go version
