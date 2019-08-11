aa317b1e7fc97fb7f95df381f0fa3c5543ba3fcb
itamar Arjuan <itamar.arjuan@gmail.com>
2019-08-11 15:48:34 +0300

    adding initial nvm setup for the session so that npm be discoverable in circle ci

--- .circleci/chain_on_pr.sh
+++ .circleci/chain_on_pr.sh
@@ -1,4 +1,11 @@
 #!/bin/bash -e
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+nvm use $NODE_VERSION
+
 cd .circleci && npm install @orbs-network/orbs-nebula && cd ..
 
 COMMIT_HASH=$(./docker/hash.sh)
