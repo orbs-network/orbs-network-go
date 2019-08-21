48d4e57df6b4638db2c6e7cbc324c5cbfb320010
Shai Yallin <shai.yallin@gmail.com>
2019-08-21 12:43:24 +0300

    eth connector tests use logging harness

--- services/crosschainconnector/ethereum/test/get_transaction_logs_test.go
+++ services/crosschainconnector/ethereum/test/get_transaction_logs_test.go
@@ -15,6 +15,7 @@ import (
 	"github.com/orbs-network/orbs-network-go/services/crosschainconnector/ethereum"
 	"github.com/orbs-network/orbs-network-go/services/crosschainconnector/ethereum/contract"
 	"github.com/orbs-network/orbs-network-go/test"
+	"github.com/orbs-network/orbs-network-go/test/with"
 	"github.com/orbs-network/orbs-spec/types/go/primitives"
 	"github.com/orbs-network/orbs-spec/types/go/services"
 	"github.com/stretchr/testify/require"
@@ -28,116 +29,122 @@ import (
 
 func TestEthereumConnector_GetTransactionLogs_ParsesASBEvent(t *testing.T) {
 	test.WithContext(func(ctx context.Context) {
-		h := newSimulatedEthereumConnectorHarness(t)
+		with.Logging(t, func(parent *with.LoggingHarness) {
+			h := newSimulatedEthereumConnectorHarness(parent.Logger)
 
-		contractAddress, deployedContract, err := h.simAdapter.DeployEthereumContract(h.simAdapter.GetAuth(), contract.EmitEventAbi, contract.EmitEventBin)
-		h.simAdapter.Commit()
-		require.NoError(t, err, "failed deploying contract to Ethereum")
+			contractAddress, deployedContract, err := h.simAdapter.DeployEthereumContract(h.simAdapter.GetAuth(), contract.EmitEventAbi, contract.EmitEventBin)
+			h.simAdapter.Commit()
+			require.NoError(t, err, "failed deploying contract to Ethereum")
 
-		amount := big.NewInt(42)
-		tuid := big.NewInt(33)
-		ethAddress := common.BigToAddress(big.NewInt(42000000000))
-		orbsAddress := anOrbsAddress()
+			amount := big.NewInt(42)
+			tuid := big.NewInt(33)
+			ethAddress := common.BigToAddress(big.NewInt(42000000000))
+			orbsAddress := anOrbsAddress()
 
-		tx, err := deployedContract.Transact(h.simAdapter.GetAuth(), "transferOut", tuid, ethAddress, orbsAddress, amount)
-		h.simAdapter.Commit()
-		require.NoError(t, err, "failed emitting event")
+			tx, err := deployedContract.Transact(h.simAdapter.GetAuth(), "transferOut", tuid, ethAddress, orbsAddress, amount)
+			h.simAdapter.Commit()
+			require.NoError(t, err, "failed emitting event")
 
-		out, err := h.connector.EthereumGetTransactionLogs(ctx, &services.EthereumGetTransactionLogsInput{
-			EthereumContractAddress: contractAddress.Hex(),
-			EthereumTxhash:          tx.Hash().Hex(),
-			EthereumEventName:       "TransferredOut",
-			EthereumJsonAbi:         contract.EmitEventAbi,
-			ReferenceTimestamp:      primitives.TimestampNano(time.Now().UnixNano()),
+			out, err := h.connector.EthereumGetTransactionLogs(ctx, &services.EthereumGetTransactionLogsInput{
+				EthereumContractAddress: contractAddress.Hex(),
+				EthereumTxhash:          tx.Hash().Hex(),
+				EthereumEventName:       "TransferredOut",
+				EthereumJsonAbi:         contract.EmitEventAbi,
+				ReferenceTimestamp:      primitives.TimestampNano(time.Now().UnixNano()),
+			})
+			require.NoError(t, err, "failed getting logs")
+
+			parsedABI, err := abi.JSON(strings.NewReader(contract.EmitEventAbi))
+			require.NoError(t, err, "failed parsing ABI")
+
+			event := new(contract.EmitEvent)
+			err = ethereum.ABIUnpackAllEventArguments(parsedABI, event, "TransferredOut", out.EthereumAbiPackedOutputs[0])
+			require.NoError(t, err, "failed getting amount from tx log")
+			require.EqualValues(t, ethAddress, event.EthAddress, "failed getting ethAddress from unpacked data")
+			require.EqualValues(t, tuid, event.Tuid, "failed getting tuid from unpacked data")
+			require.EqualValues(t, orbsAddress, event.OrbsAddress, "failed getting orbsAddress from unpacked data")
+			require.EqualValues(t, amount, event.Value, "failed getting amount from unpacked data")
+			require.NotZero(t, out.EthereumBlockNumber, "expected returned block number to be non-zero")
+			require.Equal(t, uint32(0), out.EthereumTxindex, "expected returned txIndex to zero")
 		})
-		require.NoError(t, err, "failed getting logs")
-
-		parsedABI, err := abi.JSON(strings.NewReader(contract.EmitEventAbi))
-		require.NoError(t, err, "failed parsing ABI")
-
-		event := new(contract.EmitEvent)
-		err = ethereum.ABIUnpackAllEventArguments(parsedABI, event, "TransferredOut", out.EthereumAbiPackedOutputs[0])
-		require.NoError(t, err, "failed getting amount from tx log")
-		require.EqualValues(t, ethAddress, event.EthAddress, "failed getting ethAddress from unpacked data")
-		require.EqualValues(t, tuid, event.Tuid, "failed getting tuid from unpacked data")
-		require.EqualValues(t, orbsAddress, event.OrbsAddress, "failed getting orbsAddress from unpacked data")
-		require.EqualValues(t, amount, event.Value, "failed getting amount from unpacked data")
-		require.NotZero(t, out.EthereumBlockNumber, "expected returned block number to be non-zero")
-		require.Equal(t, uint32(0), out.EthereumTxindex, "expected returned txIndex to zero")
 	})
 }
 
 func TestEthereumConnector_GetTransactionLogs_ParsesEventsWithAddressArray(t *testing.T) {
 	test.WithContext(func(ctx context.Context) {
-		h := newSimulatedEthereumConnectorHarness(t)
+		with.Logging(t, func(parent *with.LoggingHarness) {
+			h := newSimulatedEthereumConnectorHarness(parent.Logger)
 
-		contractABI, err := readFile("../contract/EmitAddressArrayEvent_sol_EmitAddressArrayEvent.abi")
-		require.NoError(t, err, "failed reading contract ABI")
+			contractABI, err := readFile("../contract/EmitAddressArrayEvent_sol_EmitAddressArrayEvent.abi")
+			require.NoError(t, err, "failed reading contract ABI")
 
-		contractBin, err := readFile("../contract/EmitAddressArrayEvent_sol_EmitAddressArrayEvent.bin")
-		require.NoError(t, err, "failed reading contract binary")
+			contractBin, err := readFile("../contract/EmitAddressArrayEvent_sol_EmitAddressArrayEvent.bin")
+			require.NoError(t, err, "failed reading contract binary")
 
-		contractAddress, deployedContract, err := h.simAdapter.DeployEthereumContract(h.simAdapter.GetAuth(), string(contractABI), string(contractBin))
-		h.simAdapter.Commit()
-		require.NoError(t, err, "failed deploying contract to Ethereum")
+			contractAddress, deployedContract, err := h.simAdapter.DeployEthereumContract(h.simAdapter.GetAuth(), string(contractABI), string(contractBin))
+			h.simAdapter.Commit()
+			require.NoError(t, err, "failed deploying contract to Ethereum")
 
-		addresses := []common.Address{{0x1, 0x2, 0x3}, {0x4, 0x5, 0x6}, {0x7, 0x8}, {0x9}}
+			addresses := []common.Address{{0x1, 0x2, 0x3}, {0x4, 0x5, 0x6}, {0x7, 0x8}, {0x9}}
 
-		tx, err := deployedContract.Transact(h.simAdapter.GetAuth(), "fire", addresses)
-		h.simAdapter.Commit()
-		require.NoError(t, err, "failed emitting event")
+			tx, err := deployedContract.Transact(h.simAdapter.GetAuth(), "fire", addresses)
+			h.simAdapter.Commit()
+			require.NoError(t, err, "failed emitting event")
 
-		out, err := h.connector.EthereumGetTransactionLogs(ctx, &services.EthereumGetTransactionLogsInput{
-			EthereumContractAddress: contractAddress.Hex(),
-			EthereumTxhash:          tx.Hash().Hex(),
-			EthereumEventName:       "Vote",
-			EthereumJsonAbi:         string(contractABI),
-			ReferenceTimestamp:      primitives.TimestampNano(time.Now().UnixNano()),
+			out, err := h.connector.EthereumGetTransactionLogs(ctx, &services.EthereumGetTransactionLogsInput{
+				EthereumContractAddress: contractAddress.Hex(),
+				EthereumTxhash:          tx.Hash().Hex(),
+				EthereumEventName:       "Vote",
+				EthereumJsonAbi:         string(contractABI),
+				ReferenceTimestamp:      primitives.TimestampNano(time.Now().UnixNano()),
+			})
+			require.NoError(t, err, "failed getting logs")
+
+			parsedABI, err := abi.JSON(bytes.NewReader(contractABI))
+			require.NoError(t, err, "failed parsing ABI")
+
+			event := new(struct {
+				Voter        common.Address
+				Nodeslist    []common.Address
+				Vote_counter *big.Int
+			})
+			err = ethereum.ABIUnpackAllEventArguments(parsedABI, event, "Vote", out.EthereumAbiPackedOutputs[0])
+			require.NoError(t, err, "failed unpacking event")
+			require.EqualValues(t, addresses, event.Nodeslist, "event did not include expected addresses")
 		})
-		require.NoError(t, err, "failed getting logs")
-
-		parsedABI, err := abi.JSON(bytes.NewReader(contractABI))
-		require.NoError(t, err, "failed parsing ABI")
-
-		event := new(struct {
-			Voter        common.Address
-			Nodeslist    []common.Address
-			Vote_counter *big.Int
-		})
-		err = ethereum.ABIUnpackAllEventArguments(parsedABI, event, "Vote", out.EthereumAbiPackedOutputs[0])
-		require.NoError(t, err, "failed unpacking event")
-		require.EqualValues(t, addresses, event.Nodeslist, "event did not include expected addresses")
 	})
 }
 
 func TestEthereumConnector_GetTransactionLogs_FailsOnWrongContract(t *testing.T) {
 	test.WithContext(func(ctx context.Context) {
-		h := newSimulatedEthereumConnectorHarness(t)
+		with.Logging(t, func(parent *with.LoggingHarness) {
+			h := newSimulatedEthereumConnectorHarness(parent.Logger)
 
-		contractAddress, deployedContract, err := h.simAdapter.DeployEthereumContract(h.simAdapter.GetAuth(), contract.EmitEventAbi, contract.EmitEventBin)
-		h.simAdapter.Commit()
-		require.NoError(t, err, "failed deploying contract to Ethereum")
+			contractAddress, deployedContract, err := h.simAdapter.DeployEthereumContract(h.simAdapter.GetAuth(), contract.EmitEventAbi, contract.EmitEventBin)
+			h.simAdapter.Commit()
+			require.NoError(t, err, "failed deploying contract to Ethereum")
 
-		amount := big.NewInt(42)
-		tuid := big.NewInt(33)
-		ethAddress := common.BigToAddress(big.NewInt(42000000000))
-		orbsAddress := anOrbsAddress()
+			amount := big.NewInt(42)
+			tuid := big.NewInt(33)
+			ethAddress := common.BigToAddress(big.NewInt(42000000000))
+			orbsAddress := anOrbsAddress()
 
-		tx, err := deployedContract.Transact(h.simAdapter.GetAuth(), "transferOut", tuid, ethAddress, orbsAddress, amount)
-		h.simAdapter.Commit()
-		require.NoError(t, err, "failed emitting event")
+			tx, err := deployedContract.Transact(h.simAdapter.GetAuth(), "transferOut", tuid, ethAddress, orbsAddress, amount)
+			h.simAdapter.Commit()
+			require.NoError(t, err, "failed emitting event")
 
-		incorrectContractAddress := "0x6C94224Eb459535C752D2684F3654a0D71e32516" // taken from somewhere else
-		require.NotEqual(t, incorrectContractAddress, contractAddress.Hex(), "contract should not accidentally match the one we use as incorrect")
+			incorrectContractAddress := "0x6C94224Eb459535C752D2684F3654a0D71e32516" // taken from somewhere else
+			require.NotEqual(t, incorrectContractAddress, contractAddress.Hex(), "contract should not accidentally match the one we use as incorrect")
 
-		_, err = h.connector.EthereumGetTransactionLogs(ctx, &services.EthereumGetTransactionLogsInput{
-			EthereumContractAddress: incorrectContractAddress,
-			EthereumTxhash:          tx.Hash().Hex(),
-			EthereumEventName:       "TransferredOut",
-			EthereumJsonAbi:         contract.EmitEventAbi,
-			ReferenceTimestamp:      primitives.TimestampNano(time.Now().UnixNano()),
+			_, err = h.connector.EthereumGetTransactionLogs(ctx, &services.EthereumGetTransactionLogsInput{
+				EthereumContractAddress: incorrectContractAddress,
+				EthereumTxhash:          tx.Hash().Hex(),
+				EthereumEventName:       "TransferredOut",
+				EthereumJsonAbi:         contract.EmitEventAbi,
+				ReferenceTimestamp:      primitives.TimestampNano(time.Now().UnixNano()),
+			})
+			require.Error(t, err, "should fail getting logs due to incorrect contract")
 		})
-		require.Error(t, err, "should fail getting logs due to incorrect contract")
 	})
 }
 
