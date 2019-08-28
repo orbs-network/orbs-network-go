846710fbd7d276cae619d1614c9a15fb97731dfd
Ron <ron@orbs.com>
2019-08-29 01:34:58 +0300

    testnet e2e supports management chains

--- .circleci/e2e-testnet.sh
+++ .circleci/e2e-testnet.sh
@@ -1,8 +1,10 @@
 #!/bin/bash -x
 
 export VCHAIN=$1
+export MGMT_VCHAIN=$2
 
 export API_ENDPOINT=http://35.172.102.63/vchains/$VCHAIN/ \
+    MGMT_API_ENDPOINT=http://35.172.102.63/vchains/$MGMT_VCHAIN/ \
     REMOTE_ENV="true" \
     STRESS_TEST_NUMBER_OF_TRANSACTIONS=100 \
     STRESS_TEST_FAILURE_RATE=20 \
