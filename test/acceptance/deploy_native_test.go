5ba40b0310e0f72f9e49f6cc28b7770e36f66039
Ron <ron@orbs.com>
2019-09-17 22:36:14 +0300

    allow new errors

--- test/acceptance/deploy_native_test.go
+++ test/acceptance/deploy_native_test.go
@@ -46,53 +46,55 @@ func TestDeployNativeContract(t *testing.T) {
 }
 
 func TestLockNativeDeployment(t *testing.T) {
-	newHarness().Start(t, func(t testing.TB, ctx context.Context, network *Network) {
+	newHarness().
+		AllowingErrors("failed contract auto deployment").
+		Start(t, func(t testing.TB, ctx context.Context, network *Network) {
 
-		counterStart := contracts.MOCK_COUNTER_CONTRACT_START_FROM
-		network.MockContract(contracts.MockForCounter(), string(contracts.NativeSourceCodeForCounter(counterStart)))
-		contract := callcontract.NewContractClient(network)
+			counterStart := contracts.MOCK_COUNTER_CONTRACT_START_FROM
+			network.MockContract(contracts.MockForCounter(), string(contracts.NativeSourceCodeForCounter(counterStart)))
+			contract := callcontract.NewContractClient(network)
 
-		t.Log("lock native deployment to account 5 should succeed")
+			t.Log("lock native deployment to account 5 should succeed")
 
-		response, _ := contract.LockNativeDeployment(ctx, 0, 5)
-		test.RequireSuccess(t, response, "lock to address 5 failed")
+			response, _ := contract.LockNativeDeployment(ctx, 0, 5)
+			test.RequireSuccess(t, response, "lock to address 5 failed")
 
-		t.Log("lock native deployment to account 6 should fail (already locked)")
+			t.Log("lock native deployment to account 6 should fail (already locked)")
 
-		response, _ = contract.LockNativeDeployment(ctx, 0, 6)
-		test.RequireResult(t, protocol.EXECUTION_RESULT_ERROR_SMART_CONTRACT, response, "lock to account 6 should have failed")
+			response, _ = contract.LockNativeDeployment(ctx, 0, 6)
+			test.RequireResult(t, protocol.EXECUTION_RESULT_ERROR_SMART_CONTRACT, response, "lock to account 6 should have failed")
 
-		t.Log("deploy native contract from account 6 should fail")
+			t.Log("deploy native contract from account 6 should fail")
 
-		response, txHash := contract.DeployNativeCounterContract(ctx, 0, 6)
-		test.RequireResult(t, protocol.EXECUTION_RESULT_ERROR_SMART_CONTRACT, response, "deployment by account 6 should have failed")
-		network.WaitForTransactionInNodeState(ctx, txHash, 0)
+			response, txHash := contract.DeployNativeCounterContract(ctx, 0, 6)
+			test.RequireResult(t, protocol.EXECUTION_RESULT_ERROR_SMART_CONTRACT, response, "deployment by account 6 should have failed")
+			network.WaitForTransactionInNodeState(ctx, txHash, 0)
 
-		t.Log("transacting with contract should fail")
+			t.Log("transacting with contract should fail")
 
-		response, _ = contract.CounterAdd(ctx, 0, 17)
-		test.RequireResult(t, protocol.EXECUTION_RESULT_ERROR_CONTRACT_NOT_DEPLOYED, response, "transaction should fail because contract is not deployed")
+			response, _ = contract.CounterAdd(ctx, 0, 17)
+			test.RequireResult(t, protocol.EXECUTION_RESULT_ERROR_CONTRACT_NOT_DEPLOYED, response, "transaction should fail because contract is not deployed")
 
-		t.Log("unlock native deployment from account 5 should succeed")
+			t.Log("unlock native deployment from account 5 should succeed")
 
-		response, _ = contract.UnlockNativeDeployment(ctx, 0, 5)
-		test.RequireSuccess(t, response, "unlock by address 5 failed")
+			response, _ = contract.UnlockNativeDeployment(ctx, 0, 5)
+			test.RequireSuccess(t, response, "unlock by address 5 failed")
 
-		t.Log("lock native deployment to account 6 should succeed")
+			t.Log("lock native deployment to account 6 should succeed")
 
-		response, _ = contract.LockNativeDeployment(ctx, 0, 6)
-		test.RequireSuccess(t, response, "lock to address 6 failed")
+			response, _ = contract.LockNativeDeployment(ctx, 0, 6)
+			test.RequireSuccess(t, response, "lock to address 6 failed")
 
-		t.Log("deploy native contract from account 6 should succeed")
+			t.Log("deploy native contract from account 6 should succeed")
 
-		response, txHash = contract.DeployNativeCounterContract(ctx, 0, 6)
-		test.RequireSuccess(t, response, "deployment by address 6 failed")
-		network.WaitForTransactionInNodeState(ctx, txHash, 0)
+			response, txHash = contract.DeployNativeCounterContract(ctx, 0, 6)
+			test.RequireSuccess(t, response, "deployment by address 6 failed")
+			network.WaitForTransactionInNodeState(ctx, txHash, 0)
 
-		t.Log("transacting with contract should succeed")
+			t.Log("transacting with contract should succeed")
 
-		response, _ = contract.CounterAdd(ctx, 0, 17)
-		test.RequireSuccess(t, response, "transaction by address 6 failed")
+			response, _ = contract.CounterAdd(ctx, 0, 17)
+			test.RequireSuccess(t, response, "transaction by address 6 failed")
 
-	})
+		})
 }
