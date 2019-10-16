a92074298ccbd27f567fbf48f3ab9ce8c0c7b27d
Shai Yallin <shai.yallin@gmail.com>
2019-10-16 11:06:34 +0300

    run gamma tests before ethereum tests as they are more sensitive to stale gamma

--- docker/test/ganache-related-test.sh
+++ docker/test/ganache-related-test.sh
@@ -15,9 +15,8 @@ docker-compose -f ./docker/test/docker-compose-ganache.yml up -d
 export ETHEREUM_ENDPOINT=http://localhost:8545/ \
       ETHEREUM_PRIVATE_KEY=f2ce3a9eddde6e5d996f6fe7c1882960b0e8ee8d799e0ef608276b8de4dc7f19
 
-echo "Running cross chain connector tests with Ganache.."
-time go_test_junit_report crosschainconnector -timeout 10m -count=1 ./services/crosschainconnector/ethereum/...
-
 echo "Running Gamma server tests with Ganache.."
 time go_test_junit_report gamma -timeout 10m -count=1 ./bootstrap/gamma/ethereum/...
 
+echo "Running cross chain connector tests with Ganache.."
+time go_test_junit_report crosschainconnector -timeout 10m -count=1 ./services/crosschainconnector/ethereum/...
