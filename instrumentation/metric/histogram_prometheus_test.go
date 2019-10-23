cfef3c6b59e6101928b4048b0778963ede0adb88
Ido Zilberberg <idoz@orbs.com>
2019-10-23 10:12:38 +0300

    Removed redundant test

--- instrumentation/metric/histogram_prometheus_test.go
+++ instrumentation/metric/histogram_prometheus_test.go
@@ -17,26 +17,7 @@ import (
 // This does NOT test correctness of Histogram
 // (e.g. that calculation of quantiles for given values is correct)
 // It only verifies the accurate conversion of metric values into Prometheus format.
-func Test_PrometheusFormatterForHistogram(t *testing.T) {
-	r := NewRegistry()
-	const SEC = int64(time.Second)
-	histo := r.NewHistogram("Some.Latency", 1000*SEC)
-
-	for i := 0; i < 1000; i++ {
-		histo.Record(int64(i) * SEC)
-	}
-
-	metrics := r.ExportAll()
-	promStr := r.ExportPrometheus()
-	require.Regexp(t, "# TYPE Some_Latency histogram", promStr)
-	for _, row := range metrics["Some.Latency"].PrometheusRow() {
-		expectedStr := fmt.Sprintf("Some_Latency{quantile=\"%s\"} %s",
-			quantileAsStr(row.quantile), row.value)
-		require.Regexp(t, expectedStr, promStr)
-	}
-}
-
-func Test_PrometheusFormatterForHistogramWithParams(t *testing.T) {
+func Test_PrometheusFormatterForHistogramWithLabels(t *testing.T) {
 	r := NewRegistry().WithVirtualChainId(100000)
 	const SEC = int64(time.Second)
 	histo := r.NewHistogram("Some.Latency", 1000*SEC)
@@ -53,10 +34,4 @@ func Test_PrometheusFormatterForHistogramWithParams(t *testing.T) {
 			quantileAsStr(row.quantile), row.value)
 		require.Regexp(t, expectedStr, promStr)
 	}
-
-	//status := r.NewHistogram("Some.Latency", 10000)
-	//status.RecordSince(time.Now())
-	//
-	//resultWithParams := r.ExportPrometheus()
-	//require.Regexp(t, "Some_Latency{vcid=\"100000\",quantile=\"0.01\"} 0.00", resultWithParams)
 }
