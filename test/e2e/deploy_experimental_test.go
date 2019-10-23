9e75d2f4d7e974b4114d6cc0bd1e0802c97e36dd
Kirill Maksimov <kirill@orbs.com>
2019-10-23 11:54:41 +0300

    Add external libraries back to experimental builds

---
+++ test/e2e/deploy_experimental_test.go
@@ -0,0 +1,53 @@
+//+build !race
+// Copyright 2019 the orbs-network-go authors
+// This file is part of the orbs-network-go library in the Orbs project.
+//
+// This source code is licensed under the MIT license found in the LICENSE file in the root directory of this source tree.
+// The above notice should be included in all copies or substantial portions of the software.
+
+package e2e
+
+import (
+	"fmt"
+	"io/ioutil"
+	"testing"
+	"time"
+)
+
+func TestContractExperimentalLibraries(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping E2E tests in short mode")
+	}
+
+	runMultipleTimes(t, func(t *testing.T) {
+
+		h := newAppHarness()
+		lt := time.Now()
+		printTestTime(t, "started", &lt)
+
+		h.waitUntilTransactionPoolIsReady(t)
+		printTestTime(t, "first block committed", &lt)
+
+		counterStart := uint64(time.Now().UnixNano())
+		contractName := fmt.Sprintf("Experimental%d", counterStart)
+		contractSource, _ := ioutil.ReadFile("../contracts/experimental/experimental.go")
+
+		printTestTime(t, "send deploy - start", &lt)
+
+		h.deployContractAndRequireSuccess(t, OwnerOfAllSupply, contractName,
+			[]byte(contractSource))
+
+		printTestTime(t, "send deploy - end", &lt)
+		//
+		//// first query after contract deployment requires error tolerance (service-sync race)
+		//sha2Response, err := h.eventuallyRunQueryWithoutError(5*time.Second, OwnerOfAllSupply.PublicKey(), contractName, "sha2_256", []byte(contractName))
+		//require.NoError(t, err)
+		//sha2ExpectedValue := sha256.Sum256([]byte(contractName))
+		//require.EqualValues(t, sha2ExpectedValue[:], sha2Response.OutputArguments[0])
+		//
+		//sha3Response, err := h.runQuery(OwnerOfAllSupply.PublicKey(), contractName, "sha3_256", []byte(contractName))
+		//require.NoError(t, err)
+		//sha3ExpectedValue := sha3.Sum256([]byte(contractName))
+		//require.EqualValues(t, sha3ExpectedValue[:], sha3Response.OutputArguments[0])
+	})
+}
