68b19266fe0cfcb2bfbe8390efda26a5f7ac0fb7
itamar Arjuan <itamar.arjuan@gmail.com>
2019-11-04 22:25:49 +0200

    adding node installation to e2e

--- docker/test/test.sh
+++ docker/test/test.sh
@@ -4,7 +4,7 @@
 # as shown in this stack overflow URL:
 # https://stackoverflow.com/questions/7578594/how-to-increase-limits-on-sockets-on-osx-for-load-testing
 
-export NVM_DIR="$HOME/.nvm"
+export NVM_DIR="/opt/circleci/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
