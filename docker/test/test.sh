8c4c99f9d51fdd84ee48db59204a386cc4ac1615
itamar Arjuan <itamar.arjuan@gmail.com>
2019-11-04 22:38:56 +0200

    adding node installation to e2e

--- docker/test/test.sh
+++ docker/test/test.sh
@@ -7,6 +7,7 @@
 export NVM_DIR="/opt/circleci/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+nvm use v11.2
 
 . ./test.common.sh
 
