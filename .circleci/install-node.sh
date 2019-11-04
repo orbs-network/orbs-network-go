4e16002aeb9cfd4cbec8d9420d5f535fe63bcabd
itamar Arjuan <itamar.arjuan@gmail.com>
2019-11-04 22:18:33 +0200

    adding node installation to e2e

--- .circleci/install-node.sh
+++ .circleci/install-node.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
 
-curl -sL https://deb.nodesource.com/setup_10.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt-get install nodejs
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+export NVM_DIR="/home/circleci/.nvm" && . $NVM_DIR/nvm.sh && nvm install v11.2 && nvm use v11.2
--- docker/test/test.sh
+++ docker/test/test.sh
@@ -4,6 +4,10 @@
 # as shown in this stack overflow URL:
 # https://stackoverflow.com/questions/7578594/how-to-increase-limits-on-sockets-on-osx-for-load-testing
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 . ./test.common.sh
 
 echo "Cleaning up all containers, if any are running"
