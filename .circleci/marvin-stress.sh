c09145178cb708fc5a7b73419319eeb6fc40f502
Ido Zilberberg <idoz@orbs.com>
2019-11-10 18:29:06 +0200

    Better URI for Orchestrator

--- .circleci/marvin-stress.sh
+++ .circleci/marvin-stress.sh
@@ -2,9 +2,10 @@
 
 echo "Launching Marvin stress test"
 
-URI="34.222.245.15:4567/jobs/start"
+URI="ec2-34-222-245-15.us-west-2.compute.amazonaws.com:4567/jobs/start"
 
-curl -d '{"tpm":10, "duration_sec":60}' -H "Content-Type: application/json" -X POST ${URI}
+
+curl -d '{"tpm":10, "duration_sec":300}' -H "Content-Type: application/json" -X POST ${URI}
 
 echo "Finished Marvin stress test"
 
