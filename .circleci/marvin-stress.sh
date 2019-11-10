e823489c06cc2c66cdd2687e353c2f9ca2132764
Ido Zilberberg <idoz@orbs.com>
2019-11-10 18:44:49 +0200

    Print to stdout

--- .circleci/marvin-stress.sh
+++ .circleci/marvin-stress.sh
@@ -4,8 +4,8 @@ echo "Launching Marvin stress test"
 
 URI="ec2-34-222-245-15.us-west-2.compute.amazonaws.com:4567/jobs/start"
 
-
 curl -d '{"tpm":10, "duration_sec":300}' -H "Content-Type: application/json" -X POST ${URI}
-
-echo "Finished Marvin stress test"
+echo
+echo "Started Marvin test. Results will be posted to Slack channel #marvin-results."
+echo
 
