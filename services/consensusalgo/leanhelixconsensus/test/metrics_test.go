d66ce09791266cd36c543746c41b7c922b7c8386
aviram <aviram@orbs.com>
2019-11-13 22:48:54 +0200

    move LH consensus logic to separate functions

--- services/consensusalgo/leanhelixconsensus/test/metrics_test.go
+++ services/consensusalgo/leanhelixconsensus/test/metrics_test.go
@@ -7,11 +7,13 @@ import (
 	"github.com/orbs-network/lean-helix-go/services/blockproof"
 	"github.com/orbs-network/lean-helix-go/services/interfaces"
 	"github.com/orbs-network/lean-helix-go/services/randomseed"
+	lhprimitives "github.com/orbs-network/lean-helix-go/spec/types/go/primitives"
 	"github.com/orbs-network/lean-helix-go/spec/types/go/protocol"
 	"github.com/orbs-network/orbs-network-go/test"
 	"github.com/orbs-network/orbs-network-go/test/builders"
 	"github.com/orbs-network/orbs-network-go/test/crypto/keys"
 	"github.com/orbs-network/orbs-network-go/test/with"
+	"github.com/orbs-network/orbs-spec/types/go/primitives"
 	"github.com/stretchr/testify/require"
 	"regexp"
 	"strings"
@@ -69,27 +71,19 @@ func TestMetricsAreUpdatedOnCommit(t *testing.T) {
 		const syncedBlockHeight = 1
 		h.handleBlockSync(ctx, syncedBlockHeight)
 
+		metrics := h.getMetrics()
+
 		orderedCommittee := h.requestOrderingCommittee(ctx)
 		orderedComitteeNodeIndicies := keys.NodeAddressesForTestsToIndexes(orderedCommittee.NodeAddresses)
 
 		leaderNodeIndex := orderedComitteeNodeIndicies[0] // leader for view 0 is always the first member in the ordered committee
 
-		prevBlockProof := protocol.BlockProofReader(nil) // the previous block (from sync) had no proof
-		randomSeed := randomseed.CalculateRandomSeed(prevBlockProof.RandomSeedSignature())
+		randomSeed := getInitialRandomSeed()
 
 		const firstTermHeight = syncedBlockHeight + 1
 		const view = 0
-		blockPair := builders.BlockPair().WithHeight(firstTermHeight).WithTransactions(1).WithEmptyLeanHelixBlockProof().Build()
 
-		h.handlePreprepareMessage(ctx, blockPair, firstTermHeight, view, leaderNodeIndex)
-
-		commitMessages := []*interfaces.CommitMessage{}
-		for i := 0; i < h.networkSize(); i++ {
-			cmsg := h.handleCommitMessage(ctx, blockPair, firstTermHeight, view, randomSeed, i)
-			commitMessages = append(commitMessages, cmsg)
-		}
-
-		metrics := h.getMetrics()
+		randomSeed = advanceConsesnsusToNextBlock(t, ctx, h, randomSeed, firstTermHeight, view, leaderNodeIndex)
 
 		// At this point the first block should be committed, lastCommitTime should update
 		now := time.Now()
@@ -104,19 +98,9 @@ func TestMetricsAreUpdatedOnCommit(t *testing.T) {
 
 		// Starting the second term
 
-		// Use commits from previous term to calculate the new random seed
-		proof := blockproof.GenerateLeanHelixBlockProof(h.keyManagerForNode(h.nodeIndex()), commitMessages)
-		prevBlockProof = protocol.BlockProofReader(proof.Raw())
-		randomSeed = randomseed.CalculateRandomSeed(prevBlockProof.RandomSeedSignature())
-
 		const secondTermHeight = firstTermHeight + 1
-		secondBlockPair := builders.BlockPair().WithHeight(secondTermHeight).WithTransactions(1).WithEmptyLeanHelixBlockProof().Build()
 
-		h.handlePreprepareMessage(ctx, secondBlockPair, secondTermHeight, view, leaderNodeIndex)
-
-		for i := 0; i < h.networkSize(); i++ {
-			h.handleCommitMessage(ctx, secondBlockPair, secondTermHeight, view, randomSeed, i)
-		}
+		advanceConsesnsusToNextBlock(t, ctx, h, randomSeed, secondTermHeight, view, leaderNodeIndex)
 
 		// A second commit should take place now and this time timeSinceLastCommitMillis should update
 
@@ -134,6 +118,41 @@ func TestMetricsAreUpdatedOnCommit(t *testing.T) {
 
 	})
 
+}
+
+func getInitialRandomSeed() uint64 {
+	prevBlockProof := protocol.BlockProofReader(nil) // the previous block (from sync) had no proof
+	return randomseed.CalculateRandomSeed(prevBlockProof.RandomSeedSignature())
+}
+
+func advanceConsesnsusToNextBlock(t *testing.T, ctx context.Context, h *singleLhcNodeHarness, randomSeed uint64, height primitives.BlockHeight, view lhprimitives.View, leaderNodeIndex int) uint64 {
+	blockPair := builders.BlockPair().WithHeight(height).WithTransactions(1).WithEmptyLeanHelixBlockProof().Build()
+
+	h.handlePreprepareMessage(ctx, blockPair, height, view, leaderNodeIndex)
+
+	commitMessages := []*interfaces.CommitMessage{}
+	for i := 0; i < h.networkSize(); i++ {
+		cmsg := h.handleCommitMessage(ctx, blockPair, height, view, randomSeed, i)
+		commitMessages = append(commitMessages, cmsg)
+	}
+
+	metrics := h.getMetrics()
+
+	// At this point the first block should be committed, lastCommitTime should update
+	now := time.Now()
+	require.True(t, test.Eventually(1*time.Second, func() bool {
+		return abs(now.UnixNano()-metrics.lastCommittedTime.Value()) < int64(time.Minute)
+	}), "expected lastCommittedTime metric not to update on first commit")
+
+	// timeSinceLastCommitMillis will NOT update because this is the first commit
+	require.True(t, strings.Contains(metrics.timeSinceLastCommitMillis.String(), "samples=0"), "expected lastCommittedTime to not update on first commit")
+
+	// Use commits from previous term to calculate the random seed for the next term
+	proof := blockproof.GenerateLeanHelixBlockProof(h.keyManagerForNode(h.nodeIndex()), commitMessages)
+	prevBlockProof := protocol.BlockProofReader(proof.Raw())
+	randomSeedForNextTerm := randomseed.CalculateRandomSeed(prevBlockProof.RandomSeedSignature())
+
+	return randomSeedForNextTerm
 }
 
 func abs(x int64) int64 {
