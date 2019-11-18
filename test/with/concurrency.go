f4666ce27ca9c6fb7c522ec0a96a3f857db6355a
amir-arad <amir@orbs.com>
2019-11-18 10:34:23 +0200

    increase concurrency tests timeout to 1 minute to reduce noise

--- test/with/concurrency.go
+++ test/with/concurrency.go
@@ -38,7 +38,7 @@ func Concurrency(tb testing.TB, f func(ctx context.Context, harness *Concurrency
 }
 
 func shutdown(waiter govnr.ShutdownWaiter) {
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
 
 	waiter.WaitUntilShutdown(ctx)
