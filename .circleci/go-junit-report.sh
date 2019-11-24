e5fa44e3d7c3cb03579e51d68463f62025b7f8da
aviram <aviram@orbs.com>
2019-11-24 22:51:20 +0200

    dockerized go-junit-report

--- .circleci/go-junit-report.sh
+++ .circleci/go-junit-report.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $USE_DOCKERIZED_JUNIT_REPORT == "true" ]
+if [ $USE_DOCKERIZED_JUNIT_REPORT == "true" ]; then
   $(aws ecr get-login --no-include-email --region us-west-2) >/dev/null 2>&1
   docker run -i 727534866935.dkr.ecr.us-west-2.amazonaws.com/go-junit-report:latest $@
 else
