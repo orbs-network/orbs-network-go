c5a1ebb20051ed2340f9087b9a8ceec6c4a67976
aviram <aviram@orbs.com>
2019-11-24 10:36:27 +0200

    base docker image

--- .circleci/release-base-image.sh
+++ .circleci/release-base-image.sh
@@ -3,5 +3,5 @@
 $(aws ecr get-login --no-include-email --region us-west-2)
 docker login -u $DOCKER_HUB_LOGIN -p $DOCKER_HUB_PASSWORD
 
-docker build -f ./docker/build/Dockerfile.base -t orbsnetwork/base:latest .
-docker push orbsnetwork/base:latest
+docker build -f ./docker/build/Dockerfile.base -t 727534866935.dkr.ecr.us-west-2.amazonaws.com/orbs-network-base:latest .
+docker push 727534866935.dkr.ecr.us-west-2.amazonaws.com/orbs-network-base:latest
