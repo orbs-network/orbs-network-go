428095e82042ce9c97ff48b4f1ae00d0dae89129
aviram <aviram@orbs.com>
2019-11-24 15:12:23 +0200

    dockerized go-junit-report

--- .circleci/dockerized-go-junit-report.sh
+++ .circleci/dockerized-go-junit-report.sh
@@ -3,8 +3,9 @@
 # see if aws is available, if not fallback to local go-junit-report
 aws >/dev/null 2>&1
 if [ $? == 0 ]; then
-  $(aws ecr get-login --no-include-email --region us-west-2)
-  docker run -i 727534866935.dkr.ecr.us-west-2.amazonaws.com/go-junit-report:latest $@
+  $(aws ecr get-login --no-include-email --region us-west-2) >/dev/null 2>&1
+  go-junit-report $@
+#  docker run -i 727534866935.dkr.ecr.us-west-2.amazonaws.com/go-junit-report:latest $@
 else
   go-junit-report $@
 fi
