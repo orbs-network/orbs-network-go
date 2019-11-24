f82a5b5ab9c3ae968eb190c866ef6e8bb523754c
aviram <aviram@orbs.com>
2019-11-24 14:58:28 +0200

    dockerized go-junit-report

--- .circleci/dockerized-go-junit-report.sh
+++ .circleci/dockerized-go-junit-report.sh
@@ -2,4 +2,4 @@
 
 $(aws ecr get-login --no-include-email --region us-west-2)
 
-docker run -i 727534866935.dkr.ecr.us-west-2.amazonaws.com/go-junit-report:latest
+docker run -i 727534866935.dkr.ecr.us-west-2.amazonaws.com/go-junit-report:latest $@
