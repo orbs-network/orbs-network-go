7eda52bc889c57cecba9e18a77b4ad3320ed0129
itamar Arjuan <itamar.arjuan@gmail.com>
2019-12-03 11:47:42 +0200

    login to aws ecr

--- docker/test/import-gamma-ecr.sh
+++ docker/test/import-gamma-ecr.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+$(aws ecr get-login --no-include-email --region us-west-2)
 DOCKER_HASH=$(./docker/hash.sh)
 ECR_IMAGE="727534866935.dkr.ecr.us-west-2.amazonaws.com/orbs-network-gamma:$DOCKER_HASH"
 docker pull $ECR_IMAGE
