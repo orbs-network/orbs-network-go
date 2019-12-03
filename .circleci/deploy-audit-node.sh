279d9a7fa9da32b6d33d785c34d4c586ce9c9510
itamar Arjuan <itamar.arjuan@gmail.com>
2019-12-03 13:41:24 +0200

    deploy audit node installing node.js

--- .circleci/deploy-audit-node.sh
+++ .circleci/deploy-audit-node.sh
@@ -1,7 +1,12 @@
 #!/bin/bash -e
 
+NODE_VERSION="v11.2"
+
 aws --version
-export NVM_DIR="$HOME/.nvm"
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+export NVM_DIR="/opt/circleci/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
