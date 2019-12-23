5ecaf042ba4bfcd9f545c92d60af6615634cbda0
Ido Zilberberg <idoz@orbs.com>
2019-12-23 16:54:37 +0200

    Fix marvin-reporter.sh

--- .circleci/marvin-reporter.sh
+++ .circleci/marvin-reporter.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-aws --version
-
 # If running locally, need to disable these next 4 lines
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
@@ -10,4 +8,4 @@ nvm use "${NODE_VERSION}"
 
 cd .circleci && npm install && cd ..
 
-./.circleci/marvin-reporter.js "${APP_CHAIN_ID}" "${TESTNET_IP}" "$1"
+./.circleci/marvin-reporter.js "$1"
