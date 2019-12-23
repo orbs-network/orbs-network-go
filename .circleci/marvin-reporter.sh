3659e3972b4effe5f9f23d2701bf7358d0a16441
Ido Zilberberg <idoz@orbs.com>
2019-12-23 17:00:23 +0200

    Run nvm when in CI only (marvin-reporter.sh)

--- .circleci/marvin-reporter.sh
+++ .circleci/marvin-reporter.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -e
 
 # If running locally, need to disable these next 4 lines
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
-nvm use "${NODE_VERSION}"
+if [[ "$CI" == "true" ]]; then
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"                   # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # This loads nvm bash_completion
+  nvm use "${NODE_VERSION}"
+fi
 
 cd .circleci && npm install && cd ..
+
 
 ./.circleci/marvin-reporter.js "$1"
