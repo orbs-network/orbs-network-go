685c78fc9b33dd04995526bd31366f1d98e1c0cb
Ido Zilberberg <idoz@orbs.com>
2019-12-24 10:35:29 +0200

    Better slack II

--- .circleci/marvin-reporter.js
+++ .circleci/marvin-reporter.js
@@ -95,18 +95,19 @@ async function createSlackMessageJobDone(jobUpdate) {
     const passedEmoji = jobUpdate.analysis.passed ? ':white_check_mark:' : ':x:';
     const passedWord = jobUpdate.analysis.passed ? 'PASSED' : 'FAILED';
 
-    return `*--------------------------------------------------------------------------*
-*DONE* running after *${Math.floor((jobUpdate.runtime || 0) / 1000)}* seconds on vchain ${jobUpdate.vchain} with ${jobUpdate.tpm} tx/min.
+    return `
 *--------------------------------------------------------------------------*
 ${passedEmoji} _Job ID: [${jobUpdate.jobId || 'NA'}] ${passedWord}_ ${passedEmoji} 
 ${failReason}
-Sent *${jobUpdate.summary.total_tx_count}* transactions with *${jobUpdate.summary.err_tx_count}* errors.
+Sent *${jobUpdate.summary.total_tx_count}* transactions with *${jobUpdate.summary.err_tx_count}* errors, in ${Math.floor((jobUpdate.runtime || 0) / 1000)} seconds on vchain ${jobUpdate.vchain} at ${jobUpdate.tpm} tx/min.
 Service times (ms): AVG=*${jobUpdate.summary.avg_service_time_ms}* MEDIAN=*${jobUpdate.summary.median_service_time_ms}* P90=*${jobUpdate.summary.p90_service_time_ms}* P99=*${jobUpdate.summary.p99_service_time_ms}* MAX=*${jobUpdate.summary.max_service_time_ms}* STDDEV=*${jobUpdate.summary.stddev_service_time_ms}*
-MinAllocMem: ${jobUpdate.summary.min_alloc_mem} MaxAllocMem: ${jobUpdate.summary.max_alloc_mem} bytes, MaxGoroutines: ${jobUpdate.summary.max_goroutines}
+MinAllocMem: ${jobUpdate.summary.min_alloc_mem} MaxAllocMem: ${jobUpdate.summary.max_alloc_mem} bytes
+MaxGoroutines: ${jobUpdate.summary.max_goroutines}
 Errors: ${jobUpdate.error || 'none'}
 <http://ec2-34-222-245-15.us-west-2.compute.amazonaws.com:3000/d/a-3pW-3mk/testnet-results?orgId=1&from=${startTime}&to=${endTime}&var-vchain=${jobUpdate.vchain}&var-validator=All|Grafana> 
 Version: ${jobUpdate.summary.semantic_version || 'NA'} Hash: ${jobUpdate.summary.commit_hash || 'NA'}
-${passedEmoji} Marvin build triggered by <${slackUsername}> ${passedEmoji}
+Marvin build triggered by <${slackUsername}>
+*--------------------------------------------------------------------------*
 `;
     // All: ${JSON.stringify(jobUpdate)}`;
 }
