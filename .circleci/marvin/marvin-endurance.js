c15a7a41c83d1eda165e5401694cd37e542905fc
Ido Zilberberg <idoz@orbs.com>
2020-01-14 20:28:47 +0200

    Gracefully handle no response from Orchestrator in marvin-endurance.js

--- .circleci/marvin/marvin-endurance.js
+++ .circleci/marvin/marvin-endurance.js
@@ -106,27 +106,31 @@ async function waitUntilDone({jobId, timeoutInSeconds = 30, acceptableDurationIn
 
         const res = await fetch(`${marvinUrl}/jobs/${jobId}/status`);
         const response = await res.json();
+        if (!response || !response.meta || response.updates.length === 0) {
+            console.log('No update from Marvin Orchestrator, skip printing update.');
+        } else {
 
-        const latestUpdate = response.updates[response.updates.length - 1];
-        const latestSummary = latestUpdate.summary;
+            const latestUpdate = response.updates[response.updates.length - 1];
+            const latestSummary = latestUpdate.summary;
 
-        console.log('');
-        console.log(`------------------------------------------`);
-        console.log(`JobId: ${response.jobId} - ${response.meta.duration_sec} seconds at ${response.meta.tpm} tx/minute on vchain ${response.meta.vchain}`);
-        console.log(`Status #${tick}: ${response.status}`);
-        console.log(`Time: ${new Date().toISOString()}. Runtime so far: ${latestUpdate.runtime}ms`);
-        console.log(`Updates so far: ${response.updates.length}`);
-        console.log(`Total successful transactions: ${latestSummary.total_tx_count}`);
-        console.log(`Total erroneous transactions: ${latestSummary.err_tx_count}`);
-        console.log(`Average service time: ${latestSummary.avg_service_time_ms}`);
-        console.log(`P99 service time: ${latestSummary.p99_service_time_ms}`);
-        console.log(`Max service time: ${latestSummary.max_service_time_ms}`);
-        console.log(`------------------------------------------`);
-        console.log('');
+            console.log('');
+            console.log(`------------------------------------------`);
+            console.log(`JobId: ${response.jobId} - ${response.meta.duration_sec} seconds at ${response.meta.tpm} tx/minute on vchain ${response.meta.vchain}`);
+            console.log(`Status #${tick}: ${response.status}`);
+            console.log(`Time: ${new Date().toISOString()}. Runtime so far: ${latestUpdate.runtime}ms`);
+            console.log(`Updates so far: ${response.updates.length}`);
+            console.log(`Total successful transactions: ${latestSummary.total_tx_count}`);
+            console.log(`Total erroneous transactions: ${latestSummary.err_tx_count}`);
+            console.log(`Average service time: ${latestSummary.avg_service_time_ms}`);
+            console.log(`P99 service time: ${latestSummary.p99_service_time_ms}`);
+            console.log(`Max service time: ${latestSummary.max_service_time_ms}`);
+            console.log(`------------------------------------------`);
+            console.log('');
 
-        if (response.status === 'DONE') {
-            returnValue = true;
-            break;
+            if (response.status === 'DONE') {
+                returnValue = true;
+                break;
+            }
         }
 
         await pSleep(90);
