fe4aa2b6eae559e4e514afb0203844a657b536c4
Kirill Maksimov <kirill@orbs.com>
2020-02-20 11:55:26 +0200

    Revert CI changes to enable tagged versions

--- .circleci/release.sh
+++ .circleci/release.sh
@@ -1,22 +1,27 @@
 #!/bin/bash -e
-VERSION=experimental
-VHASH=$(./docker/hash.sh)
+
+if [[ $CIRCLE_TAG == v* ]] ;
+then
+  VERSION=$CIRCLE_TAG
+else
+  VERSION=experimental
+fi
 
 $(aws ecr get-login --no-include-email --region us-west-2)
 docker login -u $DOCKER_HUB_LOGIN -p $DOCKER_HUB_PASSWORD
 
-docker pull $NODE_DOCKER_IMAGE:$VHASH
+docker pull $NODE_DOCKER_IMAGE:$(./docker/hash.sh)
 
-docker tag $NODE_DOCKER_IMAGE:$VHASH orbsnetwork/node:$VERSION
+docker tag $NODE_DOCKER_IMAGE:$(./docker/hash.sh) orbsnetwork/node:$VERSION
 docker push orbsnetwork/node:$VERSION
 
-docker tag $NODE_DOCKER_IMAGE:$VHASH orbsnetwork/node:$VHASH
-docker push orbsnetwork/node:$VHASH
+docker tag $NODE_DOCKER_IMAGE:$(./docker/hash.sh) orbsnetwork/node:$(./docker/hash.sh)
+docker push orbsnetwork/node:$(./docker/hash.sh)
 
-docker pull $GAMMA_DOCKER_IMAGE:$VHASH
-docker tag $GAMMA_DOCKER_IMAGE:$VHASH orbsnetwork/gamma:$VERSION
+docker pull $GAMMA_DOCKER_IMAGE:$(./docker/hash.sh)
+docker tag $GAMMA_DOCKER_IMAGE:$(./docker/hash.sh) orbsnetwork/gamma:$VERSION
 docker push orbsnetwork/gamma:$VERSION
 
-docker pull $SIGNER_DOCKER_IMAGE:$VHASH
-docker tag $SIGNER_DOCKER_IMAGE:$VHASH orbsnetwork/signer:$VERSION
+docker pull $SIGNER_DOCKER_IMAGE:$(./docker/hash.sh)
+docker tag $SIGNER_DOCKER_IMAGE:$(./docker/hash.sh) orbsnetwork/signer:$VERSION
 docker push orbsnetwork/signer:$VERSION
