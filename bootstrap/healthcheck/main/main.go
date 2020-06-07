ec3ddc88fa84a3714d2fa068adb6b67498fbcb7b
Kirill Maksimov <kirill@orbs.com>
2020-06-07 14:45:15 +0300

    Add healthcheck

---
+++ bootstrap/healthcheck/main/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/orbs-network/healthcheck"
+
+func main() {
+	healthcheck.Main()
+}
--- build-node.sh
+++ build-node.sh
@@ -8,3 +8,7 @@ time go build -o _bin/orbs-node -ldflags "-X $CONFIG_PKG.SemanticVersion=$SEMVER
 
 echo "Exporting artifacts go.mod"
 time go run ./bootstrap/build/artifacts_go_mod.go _bin/
+
+echo "Build healthckeck binary"
+export BUILD_FLAG="$BUILD_FLAG netgo osusergo" # allows static linking, further reading https://github.com/golang/go/issues/30419
+time go build -o _bin/healthcheck -ldflags "-w -extldflags '-static' -X $CONFIG_PKG.SemanticVersion=$SEMVER -X $CONFIG_PKG.CommitVersion=$GIT_COMMIT" -tags "$BUILD_FLAG" -a bootstrap/healthcheck/main/main.go
