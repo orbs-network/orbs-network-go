e124c11cb84d74087bc5b5435c5471735401e850
itamar Arjuan <itamar.arjuan@gmail.com>
2020-07-21 09:35:05 +0300

    adding pre checks for logging in entry point

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,14 @@
-#!/bin/bash +x
+#!/bin/bash
 
-/opt/orbs/orbs-node $@ | multilog s16777215 n3 '!tai64nlocal' /opt/orbs/logs 2>&1
+multilog_err=1
+multilog_cmd="multilog s16777215 n2 '!tai64nlocal' /opt/orbs/logs"
+
+while [[ "${multilog_err}" -ne "0" ]]; do
+    sleep 1
+    echo "orbs-network-go logging pre checks.." | $multilog_cmd
+    multilog_err=$?
+done
+
+echo "Running orbs-network-go.."
+
+/opt/orbs/orbs-node $@ | $multilog_cmd 2>&1
