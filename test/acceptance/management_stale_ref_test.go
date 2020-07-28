c86eea707e510e54ad3116d1e1a313012d7e792c
Aviram <aviram@orbs.com>
2020-07-28 20:24:00 +0300

    increase staleness timeout

--- test/acceptance/management_stale_ref_test.go
+++ test/acceptance/management_stale_ref_test.go
@@ -14,7 +14,7 @@ import (
 
 func TestStaleManagementRef(t *testing.T) {
 	NewHarness().
-		WithConfigOverride(config.NodeConfigKeyValue{Key: config.MANAGEMENT_NETWORK_LIVENESS_TIMEOUT, Value: config.NodeConfigValue{DurationValue: 1*time.Second}}).
+		WithConfigOverride(config.NodeConfigKeyValue{Key: config.MANAGEMENT_NETWORK_LIVENESS_TIMEOUT, Value: config.NodeConfigValue{DurationValue: 2 * time.Second}}).
 		WithNumNodes(6).
 		WithManagementPollingInterval(20*time.Millisecond).
 		WithLogFilters(log.DiscardAll()).
@@ -40,7 +40,7 @@ func TestStaleManagementRef(t *testing.T) {
 			require.NoError(t, err2)
 
 			// Wait for time to pass livness
-			waitForBlockTime(ctx, network, primitives.TimestampNano(now.UnixNano() + int64(3 * time.Second)), changedBlock)
+			waitForBlockTime(ctx, network, primitives.TimestampNano(now.UnixNano()+int64(3*time.Second)), changedBlock)
 
 			response, _ = token.Transfer(ctx, 0, 17, 5, 6)
 			require.Equal(t, response.TransactionStatus(), protocol.TRANSACTION_STATUS_REJECTED_GLOBAL_PRE_ORDER) // rejected because of liveness
@@ -61,7 +61,7 @@ func waitForBlockTime(ctx context.Context, network *Network, blockTime primitive
 		if bp.TransactionsBlock.Header.Timestamp() >= blockTime {
 			return bp.TransactionsBlock.Header.BlockHeight()
 		}
-		waitingBlock = bp.TransactionsBlock.Header.BlockHeight()+1
+		waitingBlock = bp.TransactionsBlock.Header.BlockHeight() + 1
 	}
 	return 0
 }
